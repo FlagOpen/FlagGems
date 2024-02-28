21ede1beeeed749813acfdfb2b86afa083b9d8ff
lizhixin <strongspoon@outlook.com>
2024-02-28 10:09:04 +0000

    addmm & linear

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from .layernorm import layer_norm
 from .selu import selu
 from .gelu import gelu
 from .dropout import dropout
+from .addmm import addmm
 
 aten_lib = torch.library.Library("aten", "IMPL")
 
@@ -16,27 +17,33 @@ aten_lib.impl('silu', selu, 'CUDA')
 aten_lib.impl('silu.out', selu, 'CUDA')
 aten_lib.impl('dropout', dropout, 'CUDA')
 aten_lib.impl('native_layer_norm', layer_norm, 'CUDA')
-aten_lib.impl('softmax.int', softmax, 'CompositeImplicitAutograd')
+# aten_lib.impl('softmax.int', softmax, 'CompositeImplicitAutograd')
+aten_lib.impl('addmm', addmm, 'CUDA')
 
-# test case
+## TODO
+# advanced softmax
+# sort
 
-test_x = torch.randn((16, 16), device='cuda', dtype=torch.float16)
-test_y = torch.randn((16, 16), device='cuda', dtype=torch.float16)
-test_w = torch.randn((16, 16), device='cuda', dtype=torch.float16)
-test_b = torch.randn((16, 16), device='cuda', dtype=torch.float16)
+# test case: copy the code below and run outside the FlagGems folder
 
-SiLU = torch.nn.SiLU()
-SiLU(test_x)
-GELU = torch.nn.GELU()
-GELU(test_x)
-DROPOUT = torch.nn.Dropout()
-DROPOUT(test_x, p=0.5)
-torch.mm(test_x, test_y)
-torch.softmax(test_x)
-torch.layer_norm(test_x, test_x.shape, test_w, test_b, 1e-5)
+# test_x = torch.randn((16, 16), device='cuda', dtype=torch.float16)
+# test_y = torch.randn((16, 16), device='cuda', dtype=torch.float16)
+# test_w = torch.randn((16, 16), device='cuda', dtype=torch.float16)
+# test_b = torch.randn((16, 16), device='cuda', dtype=torch.float16)
 
-test_x = torch.randn((16, 16, 16), device='cuda', dtype=torch.float16)
-test_y = torch.randn((16, 16, 16), device='cuda', dtype=torch.float16)
-test_z = torch.empty_like(test_x)
-torch.bmm(test_x, test_y)
-torch.bmm(test_x, test_y, out=test_z)
+# SiLU = torch.nn.SiLU()
+# SiLU(test_x)
+# GELU = torch.nn.GELU()
+# GELU(test_x)
+# DROPOUT = torch.nn.Dropout()
+# DROPOUT(test_x, p=0.5)
+# torch.mm(test_x, test_y)
+# torch.softmax(test_x)
+# torch.layer_norm(test_x, test_x.shape, test_w, test_b, 1e-5)
+
+
+# test_x = torch.randn((16, 16, 16), device='cuda', dtype=torch.float16)
+# test_y = torch.randn((16, 16, 16), device='cuda', dtype=torch.float16)
+# test_z = torch.empty_like(test_x)
+# torch.bmm(test_x, test_y)
+# torch.bmm(test_x, test_y, out=test_z)
---
+++ addmm.py
@@ -0,0 +1,95 @@
+import torch
+import triton
+import triton.language as tl
+from .libentry import libentry
+
+@libentry()
+@triton.autotune(
+    configs=[
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 64}, num_stages=3, num_warps=8),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_K': 32}, num_stages=4, num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32}, num_stages=4, num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32}, num_stages=4, num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_K': 32}, num_stages=4, num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32}, num_stages=4, num_warps=4),
+        triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32, 'BLOCK_SIZE_K': 32}, num_stages=5, num_warps=2),
+        triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64, 'BLOCK_SIZE_K': 32}, num_stages=5, num_warps=2),
+    ],
+    key=['M', 'N', 'K'],
+)
+@triton.jit
+def addmm_kernel(
+    a_ptr, b_ptr, bias_ptr, c_ptr,
+    alpha, beta,
+    M, N, K,
+    stride_am, stride_ak,
+    stride_bk, stride_bn,
+    stride_cm, stride_cn,
+    BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
+):
+    """Kernel for computing the addmm C = alpha * (A x B) + beta * Bias.
+    A has shape (M, K), B has shape (K, N) and C has shape (M, N)
+    """
+    pid_m = tl.program_id(0)
+    pid_n = tl.program_id(1)
+
+    offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    offs_k = tl.arange(0, BLOCK_SIZE_K)
+    a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+    b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+    bias_ptrs = bias_ptr + offs_bn
+
+    # -----------------------------------------------------------
+    # Iterate to compute a block of the C matrix.
+    # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
+    # of fp32 values for higher accuracy.
+    # `accumulator` will be converted back to fp16 after the loop.
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+        # Load the next block of A and B, generate a mask by checking the K dimension.
+        # If it is out of bounds, set it to 0.
+        a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
+        b = tl.load(b_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
+        # We accumulate along the K dimension.
+        accumulator += tl.dot(a, b)
+        # Advance the ptrs to the next K block.
+        a_ptrs += BLOCK_SIZE_K * stride_ak
+        b_ptrs += BLOCK_SIZE_K * stride_bk
+    # You can fuse arbitrary activation functions here
+    # while the accumulator is still in FP32!
+    c = accumulator.to(tl.float16)
+    bias = tl.load(bias_ptrs, mask=offs_bn < N, other=0.0)
+    c = c * alpha + bias * beta
+
+    # -----------------------------------------------------------
+    # Write back the block of the output matrix C with masks.
+    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    tl.store(c_ptrs, c, mask=c_mask)
+
+def addmm(bias, input, weight, *, beta=1, alpha=1, out=None):
+    print("FLAG ADDMM")
+    assert input.shape[1] == weight.shape[0], "Incompatible dimensions"
+    M, K = input.shape
+    _, N = weight.shape
+
+    if out is None:
+        out = torch.empty((M, N), device=input.device, dtype=input.dtype)
+    else:
+        assert out.shape == (M, N), "Output shape must match"
+        assert out.is_contiguous(), "Output must be contiguous"
+
+    grid = lambda META: (triton.cdiv(M, META['BLOCK_SIZE_M']), triton.cdiv(N, META['BLOCK_SIZE_N']), )
+    addmm_kernel[grid](
+        input, weight, bias, out,
+        alpha, beta,
+        M, N, K,
+        input.stride(0), input.stride(1),
+        weight.stride(0), weight.stride(1),
+        out.stride(0), out.stride(1),
+    )
+    return out
+
