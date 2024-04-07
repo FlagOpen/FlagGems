04e7f121074e6cc238e0290611d437d24db885f2
lizhixin <strongspoon@outlook.com>
2024-04-07 08:22:33 +0000

    runtime optimization

--- src/gems/__libentry__.py
+++ src/gems/__libentry__.py
@@ -11,26 +11,26 @@ class LibEntry(triton.KernelInterface):
         self.divisibility = 16
         self.config_cache = dict()
         self.kernel_cache = dict()
-
-    def get_key(self, all_args):
-        key = []
-        for name in self.arg_names:
-            if name in all_args:
-                arg = all_args[name]
-                if hasattr(arg, "data_ptr"):
-                    key.append(arg.dtype)
-                    key.append(arg.data_ptr() % self.divisibility == 0)
-                elif isinstance(arg, int):
-                    key.append(arg)
-        return tuple(key)
+        if isinstance(fn, triton.runtime.Autotuner):
+            self.rt = "Autotuner"
+        elif isinstance(fn, triton.runtime.Heuristics):
+            self.rt = "Heuristics"
+        else:
+            self.rt = "JitFunction"
 
     def run(self, *args, **kwargs):
-        nargs = dict(zip(self.arg_names, args))
-        all_args = {**nargs, **kwargs}
-        entry_key = self.get_key(all_args)
+        key = []
+        for arg in args:
+            if hasattr(arg, "data_ptr"):
+                key.append(arg.dtype)
+                key.append(arg.data_ptr() % self.divisibility == 0)
+            elif isinstance(arg, int):
+                key.append(arg)
+        entry_key = tuple(key)
+
         config = {}
         # Autotuner
-        if isinstance(self.fn, triton.runtime.Autotuner):
+        if self.rt == "Autotuner":
             if entry_key not in self.config_cache:
                 # tune
                 kernel = self.fn.run(*args, **kwargs)
@@ -43,7 +43,7 @@ class LibEntry(triton.KernelInterface):
                 config = self.config_cache[entry_key]
                 kernel = self.kernel_cache[entry_key]
         # Heuristics
-        elif isinstance(self.fn, triton.runtime.Heuristics):
+        elif self.rt == "Heuristics":
             if entry_key not in self.kernel_cache:
                 # compile
                 kernel = self.fn.run(*args, **kwargs)
@@ -70,20 +70,14 @@ class LibEntry(triton.KernelInterface):
                 ]
                 kernel = self.kernel_cache[entry_key]
         grid = kwargs["grid"]
-        if isinstance(grid, type(lambda: None)):
+        if callable(grid):
             # grid_fn
             current = dict(**kwargs, **config)
             meta = {**dict(zip(self.arg_names, args)), **current}
             grid = grid(meta)
+        grid = grid + (1, 1)
 
-        # allow grid len < 3
-        grid_size = len(grid)
-        grid_0 = grid[0]
-        grid_1 = grid[1] if grid_size > 1 else 1
-        grid_2 = grid[2] if grid_size > 2 else 1
-        grid = (grid_0, grid_1, grid_2)
-
-        kernel[grid](*args)
+        kernel[grid[0:3]](*args)
         return
 
 
