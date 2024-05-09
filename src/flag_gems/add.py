Fix bugs that order of making tensor contiguous and broadcasting between two tensors on OP:add, div, sub, mul, pow

src/flag_gems/add.py
@@ -105,7 +105,6 @@ def add(A, B, *, alpha=1):
     if __debug__:
         print("GEMS ADD")
     if isinstance(A, torch.Tensor) and isinstance(B, torch.Tensor):
-        A = A.contiguous()
         O = torch.empty_like(A)
         try:
             A, B = torch.broadcast_tensors(A, B)
@@ -113,6 +112,7 @@ def add(A, B, *, alpha=1):
             print(
                 f"Add: Tensor shape {A.shape} and tensor shape {B.shape} cannot broadcast to each other."
             )
+        A = A.contiguous()
         B = B.contiguous()
         M = A.numel()
         grid_fn = lambda meta: (triton.cdiv(M, meta["M_BLOCK_SIZE"]),)

src/flag_gems/div.py
@@ -212,7 +212,6 @@ def div(A, B):
     if __debug__:
         print("GEMS DIV")
     if isinstance(A, torch.Tensor) and isinstance(B, torch.Tensor):
-        A = A.contiguous()
         O = torch.empty_like(A)
         try:
             A, B = torch.broadcast_tensors(A, B)
@@ -220,6 +219,7 @@ def div(A, B):
             print(
                 f"Div: Tensor shape {A.shape} and tensor shape {B.shape} cannot broadcast to each other."
             )
+        A = A.contiguous()
         B = B.contiguous()
         M = A.numel()
         grid_fn = lambda meta: (triton.cdiv(M, meta["M_BLOCK_SIZE"]),)

src/flag_gems/pow_tensor_tensor.py
@@ -60,7 +60,6 @@ def pow_tensor_tensor_kernel(
 def pow_tensor_tensor(A, exponent):
     if __debug__:
         print("GEMS POW_TENSOR_TENSOR")
-    A = A.contiguous()
     O = torch.empty_like(A)
     try:
         A, exponent = torch.broadcast_tensors(A, exponent)
@@ -68,6 +67,7 @@ def pow_tensor_tensor(A, exponent):
         print(
             f"Pow: Tensor shape {A.shape} and tensor shape {exponent.shape} cannot broadcast to each other."
         )
+    A = A.contiguous()
     exponent = exponent.contiguous()
     M = A.numel()
     grid_fn = lambda meta: (triton.cdiv(M, meta["M_BLOCK_SIZE"]),)