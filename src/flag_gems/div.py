[bug fix] contiguous for binary pointwise operands after broadcasting

src/flag_gems/div.py
@@ -212,7 +212,6 @@ def div(A, B):
     if __debug__:
         print("GEMS DIV")
     if isinstance(A, torch.Tensor) and isinstance(B, torch.Tensor):
-        O = torch.empty_like(A)
         try:
             A, B = torch.broadcast_tensors(A, B)
         except RuntimeError as e:
@@ -221,6 +220,7 @@ def div(A, B):
             )
         A = A.contiguous()
         B = B.contiguous()
+        O = torch.empty_like(A)
         M = A.numel()
         grid_fn = lambda meta: (triton.cdiv(M, meta["M_BLOCK_SIZE"]),)
         div_kernel[grid_fn](A, B, O, M)

src/flag_gems/pow_tensor_tensor.py
@@ -60,7 +60,6 @@ def pow_tensor_tensor_kernel(
 def pow_tensor_tensor(A, exponent):
     if __debug__:
         print("GEMS POW_TENSOR_TENSOR")
-    O = torch.empty_like(A)
     try:
         A, exponent = torch.broadcast_tensors(A, exponent)
     except RuntimeError as e:
@@ -69,6 +68,7 @@ def pow_tensor_tensor(A, exponent):
         )
     A = A.contiguous()
     exponent = exponent.contiguous()
+    O = torch.empty_like(A)
     M = A.numel()
     grid_fn = lambda meta: (triton.cdiv(M, meta["M_BLOCK_SIZE"]),)
     pow_tensor_tensor_kernel[grid_fn](A, exponent, O, M)

src/flag_gems/sub.py
@@ -151,7 +151,6 @@ def sub(A, B, *, alpha=1):
     if __debug__:
         print("GEMS SUB")
     if isinstance(A, torch.Tensor) and isinstance(B, torch.Tensor):
-        O = torch.empty_like(A)
         try:
             A, B = torch.broadcast_tensors(A, B)
         except RuntimeError as e:
@@ -160,6 +159,7 @@ def sub(A, B, *, alpha=1):
             )
         A = A.contiguous()
         B = B.contiguous()
+        O = torch.empty_like(A)
         M = A.numel()
         grid_fn = lambda meta: (triton.cdiv(M, meta["M_BLOCK_SIZE"]),)
         sub_kernel[grid_fn](A, B, alpha, O, M)