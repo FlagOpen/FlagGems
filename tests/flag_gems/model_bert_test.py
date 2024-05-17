deeacf8e14b133c387d09aae3d1c2e5bf40d1fe6
jokmingwong <jokmingwong@gmail.com>
2024-05-17 09:13:33 +0000

    Support llama inference test

--- tests/flag_gems/model_bert_test.py
+++ tests/flag_gems/model_bert_test.py
@@ -2,7 +2,7 @@ import torch
 import pytest
 import copy
 import flag_gems
-from transformers import AutoTokenizer, BertConfig, BertModel
+from transformers import AutoTokenizer, AutoModelForCausalLM, BertConfig, BertModel
 
 
 @pytest.mark.parametrize("dtype", [torch.float16, torch.float32, torch.bfloat16])
@@ -46,3 +46,25 @@ def test_accuracy_bert(dtype):
     assert (
         succeed
     ), f"BERT_{dtype} FAIL with maxdiff {maxdiff} and score {score}\nREF: {ref_outputs}\nRES: {res_outputs}"
+
+
+@pytest.mark.parametrize("prompt", ["How are you today?", "What is your name?", "Who are you?", "Where are you from?"])
+def test_accuracy_llama(prompt):
+    tokenizer = AutoTokenizer.from_pretrained("sharpbai/Llama-2-7b-hf")
+    model = AutoModelForCausalLM.from_pretrained("sharpbai/Llama-2-7b-hf")
+
+    model.to("cuda").eval()
+    inputs = tokenizer(prompt, return_tensors='pt').to(device="cuda")
+    with torch.no_grad():
+        ref_output = model.generate(**inputs, max_length=100, num_beams=5)
+
+    with flag_gems.use_gems():
+        res_output = model.generate(**inputs, max_length=100, num_beams=5)
+    
+    maxdiff = torch.max(torch.abs(ref_output - res_output))
+    assert torch.allclose(
+        ref_output,
+        res_output,
+        atol=1e-3,
+        rtol=1e-3,
+    ), f"LLAMA FAIL with maxdiff {maxdiff} \nREF: {ref_outputs}\nRES: {res_outputs}"
