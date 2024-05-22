0a44e39a3ced0012cdc45eb44456b604c119a375
Tongxin Bai <waffle.bai@gmail.com>
2024-05-22 22:21:42 +0800

    [dropout] fix errors in reading cuda Philox state, cast seed and offset in uint32, save mask for backward due to the difficulty of recovering the exact offset value for each index.

--- src/flag_gems/ops/dropout.py
+++ src/flag_gems/ops/dropout.py
@@ -2,7 +2,7 @@ import triton
 import triton.language as tl
 import torch
 import logging
-from ..utils.random_utils import philox_cuda_seed_offset
+from ..utils.random_utils import philox_cuda_seed_offset, per_thread_offset
 from ..utils import libentry
 
 
@@ -26,35 +26,28 @@ from ..utils import libentry
 def dropout_forward_kernel(
     X,
     Y,
+    Mask,
     N,
     p,
     philox_seed,
     philox_offset,
     N_BLOCK_SIZE: tl.constexpr,
 ):
-    block_offset = tl.program_id(0) * N_BLOCK_SIZE
-    X_ptr = tl.make_block_ptr(
-        X,
-        shape=(N,),
-        strides=(1,),
-        offsets=(block_offset,),
-        block_shape=(N_BLOCK_SIZE,),
-        order=(0,),
-    )
-    Y_ptr = tl.make_block_ptr(
-        Y,
-        shape=(N,),
-        strides=(1,),
-        offsets=(block_offset,),
-        block_shape=(N_BLOCK_SIZE,),
-        order=(0,),
-    )
-    inp = tl.load(X_ptr)
-    offset = philox_offset + block_offset + tl.arange(0, N_BLOCK_SIZE)
-    pmask = tl.rand(philox_seed, offset, n_rounds=6) > p
+    pid = tl.program_id(0) * N_BLOCK_SIZE
+    offset = pid + tl.arange(0, N_BLOCK_SIZE)
+    mask = offset < N
+    X_ptr = X + offset
+    Y_ptr = Y + offset
+    Mask_ptr = Mask + offset
+    inp = tl.load(X_ptr, mask=mask, other=0.0)
+    philox_seed = philox_seed.to(tl.uint32)
+    philox_offset = philox_offset.to(tl.uint32)
+    philox_offset = philox_offset + offset
+    pmask = tl.rand(philox_seed, philox_offset, n_rounds=6) > p
     p = 1.0 / (1.0 - p)
     out = tl.where(pmask, inp * p, 0.0)
-    tl.store(Y_ptr, out.to(inp.dtype))
+    tl.store(Y_ptr, out.to(inp.dtype), mask=mask)
+    tl.store(Mask_ptr, pmask.to(tl.int8), mask=mask)
 
 
 @libentry()
@@ -76,36 +69,23 @@ def dropout_forward_kernel(
 @triton.jit
 def dropout_backward_kernel(
     DY,
+    MASK,
     DX,
     N,
-    p,
-    philox_seed,
-    philox_offset,
+    scale,
     N_BLOCK_SIZE: tl.constexpr,
 ):
-    block_offset = tl.program_id(0) * N_BLOCK_SIZE
-    DY_ptr = tl.make_block_ptr(
-        DY,
-        shape=(N,),
-        strides=(1,),
-        offsets=(block_offset,),
-        block_shape=(N_BLOCK_SIZE,),
-        order=(0,),
-    )
-    DX_ptr = tl.make_block_ptr(
-        DX,
-        shape=(N,),
-        strides=(1,),
-        offsets=(block_offset,),
-        block_shape=(N_BLOCK_SIZE,),
-        order=(0,),
-    )
-    dy = tl.load(DY_ptr)
-    offset = philox_offset + block_offset + tl.arange(0, N_BLOCK_SIZE)
-    pmask = tl.rand(philox_seed, offset, n_rounds=6) > p
-    p = 1.0 / (1.0 - p)
-    dx = tl.where(pmask, dy * p, 0.0)
-    tl.store(DX_ptr, dx.to(dy.dtype))
+    pid = tl.program_id(0) * N_BLOCK_SIZE
+    offset = pid + tl.arange(0, N_BLOCK_SIZE)
+    mask = offset < N
+    DY_ptr = DY + offset
+    MASK_ptr = MASK + offset
+    DX_ptr = DX + offset
+    dy = tl.load(DY_ptr, mask=mask, other=0.0)
+    Mask = tl.load(MASK_ptr, mask=mask, other=0.0)
+    output = dy * Mask
+    output = output * scale
+    tl.store(DX_ptr, output.to(dy.dtype), mask=mask)
 
 
 class NativeDropout(torch.autograd.Function):
@@ -115,10 +95,15 @@ class NativeDropout(torch.autograd.Function):
         assert p > 0.0 and p < 1.0, "p must be in (0, 1)"
         x = x.contiguous()
         O = torch.empty_like(x)
+        Mask = torch.empty(x.shape, dtype=torch.bool, device=x.device)
         N = x.numel()
         grid_fn = lambda meta: (triton.cdiv(N, meta["N_BLOCK_SIZE"]),)
-        philox_seed, philox_offset = philox_cuda_seed_offset(N)
-        dropout_forward_kernel[grid_fn](x, O, N, p, philox_seed, philox_offset)
+        # (TODO) Using Triton autotuner makes kernel parameters opaque to the caller,
+        # hence we cannot obtain the per thread offset as in Pytorch.
+        increment = N
+        philox_seed, philox_offset = philox_cuda_seed_offset(increment)
+        dropout_forward_kernel[grid_fn](x, O, Mask, N, p, philox_seed, philox_offset)
+        ctx.save_for_backward(Mask)
         ctx.p = p
         ctx.philox_seed = philox_seed
         ctx.philox_offset = philox_offset
@@ -127,11 +112,13 @@ class NativeDropout(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_outputs, kwargs):
         logging.debug("GEMS NATIVE DROPOUT BACKWARD")
+        (Mask,) = ctx.saved_tensors
+        scale = 1.0 / (1.0 - ctx.p)
         grad_outputs = grad_outputs.contiguous()
         grad_inputs = torch.empty_like(grad_outputs)
         N = grad_outputs.numel()
         grid_fn = lambda meta: (triton.cdiv(N, meta["N_BLOCK_SIZE"]),)
-        dropout_backward_kernel[grid_fn](grad_outputs, grad_inputs, N, ctx.p, ctx.philox_seed, ctx.philox_offset)
+        dropout_backward_kernel[grid_fn](grad_outputs, Mask, grad_inputs, N, scale)
         return grad_inputs, None, None
 
 
