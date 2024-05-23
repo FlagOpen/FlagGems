[fix] change sum_dim test cases to higher precision

tests/flag_gems/op_accu_test.py
@@ -1179,7 +1179,7 @@ def test_accuracy_min_dim(shape, dim, keepdim, dtype):
 def test_accuracy_sum(shape, dtype):
     inp = torch.randn(shape, dtype=dtype, device="cuda")
 
-    ref_out = torch.sum(inp, dtype=dtype)
+    ref_out = torch.sum(inp.to(torch.float64))
     with flag_gems.use_gems():
         res_out = torch.sum(inp)
 
@@ -1188,15 +1188,15 @@ def test_accuracy_sum(shape, dtype):
 
 @pytest.mark.parametrize(
     "shape",
-    [(4096, i * 64) for i in range(1, 2)],
+    [(4096, i * 64) for i in range(1, 20)],
 )
 @pytest.mark.parametrize("keepdim", [True, False])
 @pytest.mark.parametrize("dim", [[0, 1], 0, 1, None])
-@pytest.mark.parametrize("dtype", [torch.float16, torch.float32, torch.bfloat16])
+@pytest.mark.parametrize("dtype", [torch.float32])
 def test_accuracy_sum_dim(shape, dim, keepdim, dtype):
     inp = torch.randn(shape, dtype=dtype, device="cuda")
 
-    ref_out = torch.sum(inp, dim=dim, dtype=dtype, keepdim=keepdim)
+    ref_out = torch.sum(inp.to(torch.float64), dim=dim, keepdim=keepdim)
     with flag_gems.use_gems():
         res_out = torch.sum(inp, dim=dim, keepdim=keepdim)
 
@@ -1208,7 +1208,7 @@ def test_accuracy_sum_dim(shape, dim, keepdim, dtype):
     [(4096, i * 64) for i in range(1, 20)],
 )
 @pytest.mark.parametrize("keepdim", [True, False])
-@pytest.mark.parametrize("dim", [[0, 1], [1, 0]])
+@pytest.mark.parametrize("dim", [[0, 1], [1, 0], 0, 1, None])
 @pytest.mark.parametrize("dtype", [torch.float16, torch.float32, torch.bfloat16])
 def test_accuracy_amax(shape, dim, keepdim, dtype):
     inp = torch.randn(shape, dtype=dtype, device="cuda")
@@ -1332,7 +1332,7 @@ def test_accuracy_outer(shape, dtype):
     [(4096, i * 64) for i in range(1, 20)],
 )
 def test_accuracy_all(shape):
-    inp = torch.rand(shape) < 0.5
+    inp = torch.rand(shape, device="cuda") < 0.5
 
     ref_out = torch.all(inp)
     with flag_gems.use_gems():
@@ -1348,7 +1348,7 @@ def test_accuracy_all(shape):
 @pytest.mark.parametrize("keepdim", [True, False])
 @pytest.mark.parametrize("dim", [0, 1])
 def test_accuracy_all_dim(shape, dim, keepdim):
-    inp = torch.rand(shape) < 0.5
+    inp = torch.rand(shape, device="cuda") < 0.5
 
     ref_out = torch.all(inp, dim=dim, keepdim=keepdim)
     with flag_gems.use_gems():
@@ -1363,7 +1363,7 @@ def test_accuracy_all_dim(shape, dim, keepdim):
 @pytest.mark.parametrize("keepdim", [True, False])
 @pytest.mark.parametrize("dim", [[1, 0], [1, 2], 0, 1])
 def test_accuracy_all_dims(shape, dim, keepdim):
-    inp = torch.rand(shape) < 0.5
+    inp = torch.rand(shape, device="cuda") < 0.5
 
     ref_out = torch.all(inp, dim=dim, keepdim=keepdim)
     with flag_gems.use_gems():
@@ -1376,7 +1376,7 @@ def test_accuracy_all_dims(shape, dim, keepdim):
     [(4096, i * 64) for i in range(1, 20)],
 )
 def test_accuracy_any(shape):
-    inp = torch.rand(shape) < 0.5
+    inp = torch.rand(shape, device="cuda") < 0.5
 
     ref_out = torch.any(inp)
     with flag_gems.use_gems():
@@ -1392,7 +1392,7 @@ def test_accuracy_any(shape):
 @pytest.mark.parametrize("keepdim", [True, False])
 @pytest.mark.parametrize("dim", [0, 1])
 def test_accuracy_any_dim(shape, dim, keepdim):
-    inp = torch.rand(shape) < 0.5
+    inp = torch.rand(shape, device="cuda") < 0.5
 
     ref_out = torch.any(inp, dim=dim, keepdim=keepdim)
     with flag_gems.use_gems():
@@ -1407,7 +1407,7 @@ def test_accuracy_any_dim(shape, dim, keepdim):
 @pytest.mark.parametrize("keepdim", [True, False])
 @pytest.mark.parametrize("dim", [[1, 0], [1, 2], 0, 1])
 def test_accuracy_any_dims(shape, dim, keepdim):
-    inp = torch.rand(shape) < 0.5
+    inp = torch.rand(shape, device="cuda") < 0.5
 
     ref_out = torch.any(inp, dim=dim, keepdim=keepdim)
     with flag_gems.use_gems():