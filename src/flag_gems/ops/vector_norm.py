425c67398637fb5f79344b8c6aea4bcb2fcf13ab
GwokHiujin <guozi1625113688@163.com>
2024-05-24 15:25:38 +0800

    revert permutation

--- src/flag_gems/ops/vector_norm.py
+++ src/flag_gems/ops/vector_norm.py
@@ -137,12 +137,9 @@ def vector_norm(x, ord=2, dim=None, keepdim=False, dtype=None):
     if dim is None:
         dim = list(range(x.ndim))
     shape = list(x.shape)
-    dim = sorted([d % x.ndim for d in dim])
+    dim = [d % x.ndim for d in dim]
     order = [i for i in range(x.ndim) if i not in dim] + dim
-    if order == shape:
-        x = x.contiguous()
-    else:
-        x = x.permute(order).contiguous()
+    x = x.permute(order).contiguous()
     N = 1
     for i in dim:
         N *= shape[i]
