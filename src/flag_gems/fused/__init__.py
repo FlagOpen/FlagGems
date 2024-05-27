e7131e3204f9080f9157624be9a836acb441ef8c
Ping Zhu <58718936+pingzhuu@users.noreply.github.com>
2024-05-27 11:27:36 +0800

    Add fused apply_rotary_pos_emb (#25)
    * Add fused apply_rotary_pos_emb
    
    * Add contiguous check for cos/sin and refine shape check
    
    * refine by comment

--- src/flag_gems/fused/__init__.py
+++ src/flag_gems/fused/__init__.py
@@ -1,3 +1,4 @@
+from .rotary_embedding import apply_rotary_pos_emb
 from .skip_layernorm import skip_layer_norm
 from .skip_rms_norm import skip_rms_norm
 from .silu_and_mul import silu_and_mul
@@ -5,8 +6,9 @@ from .gelu_and_mul import gelu_and_mul
 
 
 __all__ = [
-    "skip_layer_norm", 
-    "skip_rms_norm", 
+    "apply_rotary_pos_emb",
+    "skip_layer_norm",
+    "skip_rms_norm",
     "silu_and_mul",
     "gelu_and_mul",
 ]
