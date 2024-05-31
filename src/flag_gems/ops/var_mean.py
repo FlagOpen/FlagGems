11e95703db9fea6f27965978715a3a40b8e9871f
cuichaowen <939136265@qq.com>
2024-05-31 16:16:54 +0800

    cambricon: fix bugs in var mean

--- src/flag_gems/ops/var_mean.py
+++ src/flag_gems/ops/var_mean.py
@@ -27,39 +27,45 @@ def var_mean_kernel(
     BLOCK_N: tl.constexpr,
 ):
     # Map the program id to the row of X it should compute.
-    pid = tl.program_id(0) * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
-    X = X + pid * N
-    Var = Var + pid
-    Mean = Mean + pid
-    row_mask = pid < M
+    num_prog = tl.num_programs(0)
+    task_num = tl.cdiv(M, BLOCK_M)
+    iter_num = tl.cdiv(task_num, num_prog)
+    if task_num % num_prog != 0:
+        iter_num = iter_num + 1
+    for i in range(0, iter_num):
+        pid = (i * num_prog + tl.program_id(0)) * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
+        X_ptr = X + pid * N
+        Var_ptr = Var + pid
+        Mean_ptr = Mean + pid
+        row_mask = pid < M
 
-    # Compute mean
-    _mean = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
-    for off in range(0, N, BLOCK_N):
-        cols = off + tl.arange(0, BLOCK_N)[None, :]
-        col_mask = cols < N
-        mask = row_mask and col_mask
+        # Compute mean
+        _mean = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+        for off in range(0, N, BLOCK_N):
+            cols = off + tl.arange(0, BLOCK_N)[None, :]
+            col_mask = cols < N
+            mask = row_mask and col_mask
 
-        a = tl.load(X + cols, mask, other=0.0).to(tl.float32)
-        _mean += a
-    mean = tl.sum(_mean, axis=1) / N
-    mean = mean[:, None]
+            a = tl.load(X_ptr + cols, mask, other=0.0).to(tl.float32)
+            _mean += a
+        mean = tl.sum(_mean, axis=1) / N
+        mean = mean[:, None]
 
-    # Compute variance
-    _var = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
-    for off in range(0, N, BLOCK_N):
-        cols = off + tl.arange(0, BLOCK_N)[None, :]
-        col_mask = cols < N
-        mask = row_mask and col_mask
+        # Compute variance
+        _var = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+        for off in range(0, N, BLOCK_N):
+            cols = off + tl.arange(0, BLOCK_N)[None, :]
+            col_mask = cols < N
+            mask = row_mask and col_mask
 
-        x = tl.load(X + cols, mask, other=0.0).to(tl.float32)
-        x = tl.where(mask, x - mean, 0.0)
-        _var += x * x
-    var = tl.sum(_var, axis=1) / (N - correction)
-    var = var[:, None]
-    # Write mean / var
-    tl.store(Mean, mean, row_mask)
-    tl.store(Var, var, row_mask)
+            x = tl.load(X_ptr + cols, mask, other=0.0).to(tl.float32)
+            x = tl.where(mask, x - mean, 0.0)
+            _var += x * x
+        var = tl.sum(_var, axis=1) / (N - correction)
+        var = var[:, None]
+        # Write mean / var
+        tl.store(Mean_ptr, mean, row_mask)
+        tl.store(Var_ptr, var, row_mask)
 
 
 def var_mean(x, dim=None, *, correction=None, keepdim=False):
@@ -81,7 +87,8 @@ def var_mean(x, dim=None, *, correction=None, keepdim=False):
     var = torch.empty(shape, dtype=x.dtype, device=x.device)
     mean = torch.empty(shape, dtype=x.dtype, device=x.device)
 
-    grid = lambda META: (triton.cdiv(M, META["BLOCK_M"]),)
+    #grid = lambda META: (triton.cdiv(M, META["BLOCK_M"]),)
+    grid = lambda META: (min(triton.cdiv(M, META["BLOCK_M"]), 65535),)
     var_mean_kernel[grid](x, var, mean, M, N, correction)
     if not keepdim:
         var = var.squeeze(dim=dim)
