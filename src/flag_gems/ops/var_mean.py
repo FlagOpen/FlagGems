60994349eb0b4c0047f61f3b8f02c6ffd6289dde
strongspoon <strongspoon@outlook.com>
2024-06-13 15:46:47 +0800

    [Operator] var_mean welford implementation

--- src/flag_gems/ops/var_mean.py
+++ src/flag_gems/ops/var_mean.py
@@ -64,6 +64,62 @@ def var_mean_kernel(
     tl.store(Var, var, row_mask)
 
 
+@triton.jit
+def welford_func(mean_x, count_x, M_x, mean_y, count_y, M_y):
+    count = count_x + count_y
+    _count = tl.maximum(count, 1)
+    mc_x = mean_x * count_x
+    mc_y = mean_y * count_y
+    mean = (mc_x + mc_y) / _count
+    M = M_x + mc_x * mean_x + M_y + mc_y * mean_y - count * mean * mean
+    return mean, count, M
+
+
+@triton.autotune(configs=cfggen(), key=["M", "N"])
+@triton.jit
+def var_mean_welford_kernel(
+    X,
+    Var,
+    Mean,
+    M,
+    N,
+    correction,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+):
+    # Map the program id to the row of X it should compute.
+    pid = tl.program_id(0) * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
+    X = X + pid * N
+    Var = Var + pid
+    Mean = Mean + pid
+    row_mask = pid < M
+
+    _mean = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+    _acc = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+    _count = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+    for off in range(0, N, BLOCK_N):
+        cols = off + tl.arange(0, BLOCK_N)[None, :]
+        col_mask = cols < N
+        mask = row_mask and col_mask
+
+        x = tl.load(X + cols, mask, other=0.0).to(tl.float32)
+
+        count = _count + mask
+        cnt = tl.maximum(count, 1)
+        cur_mean = (_mean * _count + x) / cnt
+        _acc += (x - cur_mean) * (x - _mean) * mask
+        _mean = cur_mean
+        _count = count
+
+    mean, count, acc = tl.reduce((_mean, _count, _acc), axis=1, combine_fn=welford_func)
+    var = acc / (N - correction)
+    mean = mean[:, None]
+    var = var[:, None]
+    # Write mean / var
+    tl.store(Mean, mean, row_mask)
+    tl.store(Var, var, row_mask)
+
+
 def var_mean(x, dim=None, *, correction=None, keepdim=False):
     logging.debug("GEMS VAR MEAN")
     if correction is None:
@@ -83,7 +139,8 @@ def var_mean(x, dim=None, *, correction=None, keepdim=False):
     mean = torch.empty(shape, dtype=x.dtype, device=x.device)
 
     grid = lambda META: (triton.cdiv(M, META["BLOCK_M"]),)
-    var_mean_kernel[grid](x, var, mean, M, N, correction)
+    # var_mean_kernel[grid](x, var, mean, M, N, correction)
+    var_mean_welford_kernel[grid](x, var, mean, M, N, correction)
     if not keepdim:
         var = var.squeeze(dim=dim)
         mean = mean.squeeze(dim=dim)
