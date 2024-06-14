144f296b7382af64a23502f58b7cccd4339100c2
strongspoon <strongspoon@outlook.com>
2024-06-14 14:34:29 +0800

    [Operator] remove low-perf var_mean_kernel

--- src/flag_gems/ops/var_mean.py
+++ src/flag_gems/ops/var_mean.py
@@ -20,55 +20,6 @@ def cfggen():
     return configs
 
 
-@libentry()
-@triton.autotune(configs=cfggen(), key=["M", "N"])
-@triton.jit
-def var_mean_kernel(
-    X,
-    Var,
-    Mean,
-    M,
-    N,
-    correction,
-    BLOCK_M: tl.constexpr,
-    BLOCK_N: tl.constexpr,
-):
-    # Map the program id to the row of X it should compute.
-    pid = tl.program_id(0) * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
-    X = X + pid * N
-    Var = Var + pid
-    Mean = Mean + pid
-    row_mask = pid < M
-
-    # Compute mean
-    _mean = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
-    for off in range(0, N, BLOCK_N):
-        cols = off + tl.arange(0, BLOCK_N)[None, :]
-        col_mask = cols < N
-        mask = row_mask and col_mask
-
-        a = tl.load(X + cols, mask, other=0.0).to(tl.float32)
-        _mean += a
-    mean = tl.sum(_mean, axis=1) / N
-    mean = mean[:, None]
-
-    # Compute variance
-    _var = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
-    for off in range(0, N, BLOCK_N):
-        cols = off + tl.arange(0, BLOCK_N)[None, :]
-        col_mask = cols < N
-        mask = row_mask and col_mask
-
-        x = tl.load(X + cols, mask, other=0.0).to(tl.float32)
-        x = tl.where(mask, x - mean, 0.0)
-        _var += x * x
-    var = tl.sum(_var, axis=1) / (N - correction)
-    var = var[:, None]
-    # Write mean / var
-    tl.store(Mean, mean, row_mask)
-    tl.store(Var, var, row_mask)
-
-
 @triton.jit
 def welford_func(mean_x, count_x, M_x, mean_y, count_y, M_y):
     count = count_x + count_y
@@ -223,7 +174,6 @@ def var_mean(x, dim=None, *, correction=None, keepdim=False):
         mean = torch.empty(shape, dtype=x.dtype, device=x.device)
 
         grid = lambda META: (triton.cdiv(M, META["BLOCK_M"]),)
-        # var_mean_kernel[grid](x, var, mean, M, N, correction)
         var_mean_welford_kernel[grid](x, var, mean, M, N, correction)
 
     if not keepdim:
