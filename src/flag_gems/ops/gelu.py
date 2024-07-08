e45b8fe38c8699ff16046a634b2323e65ddfa446
Clement Chan <iclementine@outlook.com>
2024-07-08 17:58:27 +0800

    try to import tl.math functions from different path at various versions of triton

--- src/flag_gems/ops/gelu.py
+++ src/flag_gems/ops/gelu.py
@@ -5,12 +5,20 @@ import triton.language as tl
 
 from ..utils import pointwise_dynamic
 
+try:
+    from triton.language.extra.cuda.libdevice import erf, pow, tanh
+except ImportError:
+    try:
+        from triton.language.math import erf, pow, tanh
+    except ImportError:
+        from triton.language.libdevice import erf, pow, tanh
+
 
 @pointwise_dynamic(promotion_methods=[(0, "DEFAULT")])
 @triton.jit
 def gelu_none(x):
     scale: tl.constexpr = 0.7071067811
-    output = 0.5 * x * (1 + tl.math.erf(x * scale))
+    output = 0.5 * x * (1 + erf(x * scale))
     return output
 
 
@@ -18,14 +26,7 @@ def gelu_none(x):
 @triton.jit
 def gelu_tanh(x):
     output = (
-        0.5
-        * x
-        * (
-            1
-            + tl.math.tanh(
-                x * 0.79788456 * (1 + 0.044715 * tl.math.pow(x.to(tl.float32), 2))
-            )
-        )
+        0.5 * x * (1 + tanh(x * 0.79788456 * (1 + 0.044715 * pow(x.to(tl.float32), 2))))
     )
     return output
 
