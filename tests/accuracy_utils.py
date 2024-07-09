6cc9ec587593e1a1d133e830de17dee2b1ad14bd
jialuo.bai <jialuo.bai@mthreads.com>
2024-07-09 06:26:24 +0000

    fix: vectornorm upcast to fp64

--- tests/accuracy_utils.py
+++ tests/accuracy_utils.py
@@ -137,6 +137,16 @@ def to_reference(inp, upcast=False):
             ref_inp = ref_inp.to(torch.float64)
     return ref_inp
 
+def to_reference_fp64(inp, upcast=False):
+    if inp is None:
+        return None
+    ref_inp = inp
+    if TO_CPU:
+        ref_inp = ref_inp.to("cpu")
+    if upcast:
+        ref_inp = ref_inp.to(torch.float64)
+    return ref_inp
+
 
 def to_cpu(res, ref):
     if TO_CPU:
