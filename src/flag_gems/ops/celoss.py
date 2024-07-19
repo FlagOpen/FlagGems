f4f8f6829f3f026ad1a720d67792c1b3fe8d429d
StrongSpoon <strongspoon@outlook.com>
2024-07-19 08:38:41 +0000

    [operator] impl celoss of probabilities

--- src/flag_gems/ops/celoss.py
+++ src/flag_gems/ops/celoss.py
@@ -8,29 +8,6 @@ from flag_gems import sum
 
 
 @triton.autotune(
-    configs=[triton.Config({"BLOCK_SIZE": n}, num_warps=4) for n in [512, 1024, 2048]],
-    key=["N"],
-)
-@triton.jit
-def weight_kernel(w_ptr, tgt_ptr, w_tgt_ptr, ignore_index, N, BLOCK_SIZE: tl.constexpr):
-    pid = tl.program_id(0)
-    offset = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
-    mask = offset < N
-
-    tgt_ptrs = tgt_ptr + offset
-
-    tgt = tl.load(tgt_ptrs, mask, other=0)
-    ignore_mask = tgt == ignore_index
-
-    w_ptrs = w_ptr + tgt
-    w = tl.load(w_ptrs, mask, other=0)
-    w_tgt = tl.where(ignore_mask, 0, w)
-
-    w_tgt_ptrs = w_tgt_ptr + offset
-    tl.store(w_tgt_ptrs, w_tgt, mask)
-
-
-@triton.autotune(
     configs=[
         triton.Config({"BLOCK_C": c, "BLOCK_D": d}, num_warps=4)
         for c in [256, 512, 1024]
@@ -39,11 +16,12 @@ def weight_kernel(w_ptr, tgt_ptr, w_tgt_ptr, ignore_index, N, BLOCK_SIZE: tl.con
     key=["C", "D"],
 )
 @triton.jit
-def celoss_kernel(
+def celoss_indice_kernel(
     inp_ptr,
     tgt_ptr,
     w_ptr,
     out_ptr,
+    w_tgt_ptr,
     ignore_index,
     N,
     C,
@@ -61,8 +39,10 @@ def celoss_kernel(
 
     ignore_mask = tgt == ignore_index
 
-    w_tgt_ptrs = w_ptr + pid_n * D + offset_d
-    w_tgt = tl.load(w_tgt_ptrs, mask=tgt_mask, other=0).to(tl.float32)
+    w_ptrs = w_ptr + tgt
+    w_tgt = tl.load(w_ptrs, mask=tgt_mask, other=0).to(tl.float32)
+    w_tgt_ptrs = w_tgt_ptr + pid_n * D + offset_d
+    tl.store(w_tgt_ptrs, w_tgt, mask=tgt_mask)
 
     tmp_max = tl.zeros([BLOCK_C, BLOCK_D], dtype=tl.float32)
     tmp_sum = tl.zeros([BLOCK_C, BLOCK_D], dtype=tl.float32)
@@ -88,6 +68,65 @@ def celoss_kernel(
     out = tl.where(ignore_mask, 0, out)
     out_ptrs = out_ptr + pid_n * D + offset_d
     tl.store(out_ptrs, out, mask=tgt_mask)
+
+
+@triton.autotune(
+    configs=[
+        triton.Config({"BLOCK_C": c, "BLOCK_D": d}, num_warps=4)
+        for c in [256, 512, 1024]
+        for d in [1, 4, 16]
+    ],
+    key=["C", "D"],
+)
+@triton.jit
+def celoss_probability_kernel(
+    inp_ptr,
+    tgt_ptr,
+    w_ptr,
+    out_ptr,
+    N,
+    C,
+    D,
+    BLOCK_C: tl.constexpr,
+    BLOCK_D: tl.constexpr,
+):
+    pid_n = tl.program_id(0)
+    pid_d = tl.program_id(1)
+    offset_d = pid_d * BLOCK_D + tl.arange(0, BLOCK_D)
+
+    tmp_max = tl.zeros([BLOCK_C, BLOCK_D], dtype=tl.float32)
+    tmp_sum = tl.zeros([BLOCK_C, BLOCK_D], dtype=tl.float32)
+
+    for off in range(0, C, BLOCK_C):
+        offset_c = off + tl.arange(0, BLOCK_C)
+        inp_ptrs = inp_ptr + pid_n * C * D + offset_c[:, None] * D + offset_d[None, :]
+        inp_mask = offset_c[:, None] < C and offset_d[None, :] < D
+        inp = tl.load(inp_ptrs, inp_mask, other=-float("inf")).to(tl.float32)
+        cur_max = tl.maximum(tmp_max, inp)
+        cur_exp = tl.exp(inp - cur_max)
+        tmp_sum = tmp_sum * tl.exp(tmp_max - cur_max) + cur_exp
+        tmp_max = cur_max
+    final_max = tl.max(tmp_max, axis=0)
+    tmp_sum = tmp_sum * tl.exp(tmp_max - final_max[None, :])
+    final_sum = tl.sum(tmp_sum, axis=0)
+
+    _sum = tl.zeros([BLOCK_C, BLOCK_D], dtype=tl.float32)
+    for off in range(0, C, BLOCK_C):
+        offset_c = off + tl.arange(0, BLOCK_C)
+        inp_ptrs = inp_ptr + pid_n * C * D + offset_c[:, None] * D + offset_d[None, :]
+        tgt_ptrs = tgt_ptr + pid_n * C * D + offset_c[:, None] * D + offset_d[None, :]
+        mask = offset_c[:, None] < C and offset_d[None, :] < D
+        w_ptrs = w_ptr + offset_c
+        w_mask = offset_c < C
+        inp = tl.load(inp_ptrs, mask, other=0).to(tl.float32)
+        tgt = tl.load(tgt_ptrs, mask, other=1).to(tl.float32)
+        w = tl.load(w_ptrs, w_mask, other=0)[:, None]
+        log = tl.log(tgt * tl.exp(inp - final_max[None, :]) / final_sum[None, :])
+        _sum -= w * log
+
+    out = tl.sum(_sum, axis=0)
+    out_ptrs = out_ptr + pid_n * D + offset_d
+    tl.store(out_ptrs, out, mask=offset_d < D)
 
 
 class CrossEntropyLoss(torch.autograd.Function):
@@ -116,24 +155,28 @@ class CrossEntropyLoss(torch.autograd.Function):
         inp = inp.contiguous()
         tgt = target.contiguous()
         weight = weight.contiguous()
-        w_tgt = torch.empty(shape, dtype=inp.dtype, device=inp.device)
         out = torch.empty(shape, dtype=inp.dtype, device=inp.device)
+        grid = lambda meta: (N, triton.cdiv(D, meta["BLOCK_D"]))
 
-        grid = lambda meta: (triton.cdiv(N * D, meta["BLOCK_SIZE"]),)
-        with torch.cuda.device(inp.device):
-            weight_kernel[grid](weight, tgt, w_tgt, ignore_index, N * D)
-
-        grid = lambda meta: (
-            N,
-            triton.cdiv(D, meta["BLOCK_D"]),
-        )
-        with torch.cuda.device(inp.device):
-            celoss_kernel[grid](inp, tgt, w_tgt, out, ignore_index, N, C, D)
+        if target.ndim == dim:
+            # target probabilities
+            with torch.cuda.device(inp.device):
+                celoss_probability_kernel[grid](inp, tgt, weight, out, N, C, D)
+        else:
+            # target indices
+            w_tgt = torch.empty(shape, dtype=inp.dtype, device=inp.device)
+            with torch.cuda.device(inp.device):
+                celoss_indice_kernel[grid](
+                    inp, tgt, weight, out, w_tgt, ignore_index, N, C, D
+                )
 
         if reduction == 0:  # NONE
             return out
         elif reduction == 1:  # MEAN
-            return sum(out) / sum(w_tgt)
+            if target.ndim == dim:
+                return sum(out) / (N * D)
+            else:
+                return sum(out) / sum(w_tgt)
         else:  # SUM
             return sum(out)
 
@@ -153,9 +196,10 @@ for size in SIZE:
     shape = [BATCH, size, 64]
     dtype = torch.float32
     up_limit = size
-    target_shape = [BATCH, 64]
+    # target_shape = [BATCH, 64]
+    target_shape = shape
 
-    weight = torch.randn(
+    weight = torch.ones(
         [
             size,
         ],
@@ -163,8 +207,9 @@ for size in SIZE:
         device="cuda",
     )
     loss = torch.nn.CrossEntropyLoss(weight=weight, reduction="sum", ignore_index=-100)
-    inp = torch.randn(shape, dtype=dtype, device="cuda")
-    tgt = torch.randint(0, 3, target_shape, device="cuda")
+    inp = torch.ones(shape, dtype=dtype, device="cuda")
+    # tgt = torch.randint(0, 3, target_shape, device="cuda")
+    tgt = torch.ones(shape, dtype=dtype, device="cuda")
     out_ = loss(inp, tgt)
     out = celoss(inp, tgt, weight=weight, ignore_index=-100, reduction=2)
     flag = torch.allclose(out_, out)
