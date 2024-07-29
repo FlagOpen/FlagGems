403cd1493d8ec8d8241ea3bda731e57f3b149aa7
hexi <hexi@cambricon.com>
2024-07-29 01:46:04 +0000

    cambricon: opt triu grid

--- src/flag_gems/ops/triu.py
+++ src/flag_gems/ops/triu.py
@@ -4,13 +4,16 @@ import torch
 import triton
 import triton.language as tl
 
-from ..utils import libentry
+from ..utils import libentry, TOTAL_CORE_NUM
 
 
 def cfggen():
     configs = [
-        triton.Config({"M_BLOCK_SIZE": 32, "N_BLOCK_SIZE": 512}, num_warps=1, num_stages=3),
-        triton.Config({"M_BLOCK_SIZE": 1, "N_BLOCK_SIZE": 2048}, num_warps=1, num_stages=0)
+        triton.Config({"M_BLOCK_SIZE": 1}, num_warps=1, num_stages=3),
+        triton.Config({"M_BLOCK_SIZE": 4}, num_warps=1, num_stages=3),
+        triton.Config({"M_BLOCK_SIZE": 8}, num_warps=1, num_stages=3),
+        triton.Config({"M_BLOCK_SIZE": 16}, num_warps=1, num_stages=3),
+        triton.Config({"M_BLOCK_SIZE": 32}, num_warps=1, num_stages=3),
     ]
     return configs
 
@@ -33,21 +36,40 @@ def triu_kernel(
     diagonal,
     M_BLOCK_SIZE: tl.constexpr,
     N_BLOCK_SIZE: tl.constexpr,
+    NEED_LOOP: tl.constexpr,
 ):
     pid = tl.program_id(0)
-    row = pid * M_BLOCK_SIZE + tl.arange(0, M_BLOCK_SIZE)[:, None]
-    m_mask = row < M
-    X += row * N
-    Y += row * N
+    num_jobs = tl.num_programs(0)
+    m_block_step = M_BLOCK_SIZE * num_jobs
 
-    for n_offset in range(0, N, N_BLOCK_SIZE):
-        cols = n_offset + tl.arange(0, N_BLOCK_SIZE)[None, :]
-        n_mask = cols < N
-        mask = m_mask and n_mask
+    for m_offset in range(pid * M_BLOCK_SIZE, M, m_block_step):
+        if NEED_LOOP:
+            row = m_offset + tl.arange(0, M_BLOCK_SIZE)[:, None]
+            m_mask = row < M
+            PX = X + row * N
+            PY = Y + row * N
+            for n_offset in range(0, N, N_BLOCK_SIZE):
+                cols = n_offset + tl.arange(0, N_BLOCK_SIZE)[None, :]
+                n_mask = cols < N
+                mask = m_mask and n_mask
 
-        x = tl.load(X + cols, mask, other=0.0)
-        y = tl.where(row + diagonal <= cols, x, 0.0)
-        tl.store(Y + cols, y, mask=mask)
+                x = tl.load(PX + cols, mask, other=0.0)
+                y = tl.where(row + diagonal <= cols, x, 0.0)
+                tl.store(PY + cols, y, mask=mask)
+        else:
+            write = tl.empty([M_BLOCK_SIZE, N_BLOCK_SIZE], X.dtype.element_ty)
+            cols = tl.arange(0, N_BLOCK_SIZE)
+            repeat_num = tl.minimum(M_BLOCK_SIZE, M - m_offset)
+            for i in tl.range(repeat_num, num_stages=0):
+                cur_row = m_offset + i
+                PX = X + cur_row * N
+                rmask = cols >= cur_row + diagonal
+                write[i, :] = tl.load(PX + cols, mask=rmask, other=0.0)
+
+            row = m_offset + tl.arange(0, M_BLOCK_SIZE)
+            offset = cols[None, :] + row[:, None] * N
+            n_mask = row[:, None] < M
+            tl.store(Y + offset, write, mask=n_mask)
 
 
 @libentry()
@@ -88,8 +110,14 @@ def triu(A, diagonal=0):
     M, N = A.shape[-2:]
     with torch.mlu.device(A.device):
         if len(A.shape) == 2:
-            grid = lambda meta: (triton.cdiv(M, meta["M_BLOCK_SIZE"]),)
-            triu_kernel[grid](A, out, M, N, diagonal)
+            grid = lambda meta: (min(triton.cdiv(M, meta["M_BLOCK_SIZE"]), TOTAL_CORE_NUM),)
+            # A large value for n_block_size can lead to insufficient MLU resources,
+            # causing the compilation to fail. Therefore, a conservative upper limit of 8192
+            # is currently set, but the actual maximum achievable value should be confirmed
+            # based on real-world conditions.
+            n_block = min(8192, N)
+            need_loop = n_block < N
+            triu_kernel[grid](A, out, M, N, diagonal, N_BLOCK_SIZE=n_block, NEED_LOOP=need_loop)
         else:
             batch = int(torch.numel(A) / M / N)
             B = A.view(batch, -1)
