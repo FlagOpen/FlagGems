090da6d502ba848b57f488a4f1db0e07c61d79ba
Hiujin Gwok <70586936+GwokHiujin@users.noreply.github.com>
2024-07-31 10:52:42 +0800

    [Operator] init slice&select scatter

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -98,6 +98,8 @@ def enable(lib=aten_lib):
     lib.impl("isclose", isclose, "CUDA")
     lib.impl("allclose", allclose, "CUDA")
     lib.impl("flip", flip, "CUDA")
+    lib.impl("slice_scatter", slice_scatter, "CUDA")
+    lib.impl("select_scatter", select_scatter, "CUDA")
 
 
 class use_gems:
