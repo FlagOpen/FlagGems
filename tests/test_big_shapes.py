[GENESIS-1475]cambricon: fix log softmax bug

tests/test_big_shapes.py
@@ -32,7 +32,8 @@ def test_accuracy_cumsum(shape, dtype):
 @pytest.mark.parametrize("shape", BIG_REDUCTION_SHAPES)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
 @pytest.mark.parametrize("dim", [0, 1])
-def test_accuracy_log_softmax(shape, dtype, dim):
+def test_accuracy_log_softmax_forward(shape, dtype, dim):
+    torch.manual_seed(2)
     inp = torch.randn(shape, dtype=dtype, device=DEVICE, requires_grad=True)
     ref_inp = to_reference(inp, True)
 
@@ -41,7 +42,21 @@ def test_accuracy_log_softmax(shape, dtype, dim):
         res_out = torch.nn.functional.log_softmax(inp, dim=dim)
     gems_assert_close(res_out, ref_out, dtype)
 
-    out_grad = torch.randn_like(res_out)
+@pytest.mark.parametrize("shape", BIG_REDUCTION_SHAPES)
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+@pytest.mark.parametrize("dim", [0, 1])
+def test_accuracy_log_softmax_backward(shape, dtype, dim):
+    # Here we set input all ones to avoid random effect.
+    inp = torch.ones(shape, dtype=dtype, device=DEVICE, requires_grad=True)
+    # UpCast is set to False to keep same precision data for backward.
+    ref_inp = to_reference(inp, False)
+
+    ref_out = torch.nn.functional.log_softmax(ref_inp, dim=dim)
+    with flag_gems.use_gems():
+        res_out = torch.nn.functional.log_softmax(inp, dim=dim)
+    gems_assert_close(res_out, ref_out, dtype)
+
+    out_grad = torch.ones_like(res_out)
     ref_grad = to_reference(out_grad, True)
 
     (ref_in_grad,) = torch.autograd.grad(ref_out, ref_inp, ref_grad)