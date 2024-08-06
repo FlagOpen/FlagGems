4bfa34cc1d8a51e7f9dc17686ab9817bdf661028
Yangrl <2535184404@qq.com>
2024-08-06 06:56:08 +0000

    codestyle

--- src/flag_gems/ops/minimum.py
+++ src/flag_gems/ops/minimum.py
@@ -8,12 +8,12 @@ from flag_gems.utils import pointwise_dynamic
 
 @pointwise_dynamic(is_tensor=[True, True], promotion_methods=[(0, 1, "DEFAULT")])
 @triton.jit
-def maximum_kernel(X, Y):
+def minimum_kernel(X, Y):
     return tl.minimum(X, Y)
 
 
-def maximum(X, Y):
+def minimum(X, Y):
     logging.debug("GEMS MINIMUM")
     assert X.is_cuda and Y.is_cuda
     assert X.ndim == 1 and Y.ndim == 1 and X.size(0) == Y.size(0)
-    return maximum_kernel(X, Y)
+    return minimum_kernel(X, Y)
