7edb38dc2ab27b49262a8ba6d8152d21de42b538
Yangrl <2535184404@qq.com>
2024-08-06 05:48:37 +0000

    add some ops

---
+++ src/flag_gems/ops/maximum.py
@@ -0,0 +1,20 @@
+import logging
+import torch
+import triton
+import triton.language as tl
+from flag_gems.utils import pointwise_dynamic
+
+
+@pointwise_dynamic(is_tensor=[True, True], promotion_methods=[(0, 1, "DEFAULT")])
+@triton.jit
+def maximum_kernel(X, Y):
+    return tl.maximum(X, Y)
+
+def maximum(X, Y):
+    logging.debug("GEMS MAXIMUM")
+    assert X.is_cuda and Y.is_cuda
+    assert X.ndim == 1 and Y.ndim == 1 and X.size(0) == Y.size(0)
+    return maximum_kernel(A, B)
+
+
+
