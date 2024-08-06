980c9d79992169fd835c58f7f1f025783f289ff8
Bowen <81504862+Bowen12992@users.noreply.github.com>
2024-08-06 10:08:14 +0800

    Update test_specific_ops.py

--- tests/test_specific_ops.py
+++ tests/test_specific_ops.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
             op_nums = op_nums + len(item)
             for op in item.keys():
                 op_list.append(op)
-        print(f"{op_nums} ops to be tests, and here is the sorted op list:")
+        print(f"{op_nums} ops to test, and here is the sorted op list:")
         op_list = sorted(op_list)
         print(op_list)
 
