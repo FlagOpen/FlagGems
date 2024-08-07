0be2081721899c998f1693cf416d65bd2d16ea99
Yangrl <2535184404@qq.com>
2024-08-07 04:55:46 +0000

    fix test

--- src/flag_gems/ops/maximum.py
+++ src/flag_gems/ops/maximum.py
@@ -1,9 +1,10 @@
 import logging
 
+import torch
 import triton
 import triton.language as tl
 
-from ..utils import pointwise_dynamic
+from flag_gems.utils import pointwise_dynamic
 
 
 @pointwise_dynamic(
@@ -14,7 +15,15 @@ def maximum_kernel(X, Y):
     return tl.maximum(X, Y)
 
 
+def convert_bf16_to_fp32(tensor):
+    if tensor.dtype == torch.bfloat16:
+        return tensor.to(torch.float32)
+    return tensor
+
+
 def maximum(X, Y):
     logging.debug("GEMS MAXIMUM")
     assert X.is_cuda and Y.is_cuda
+    X = convert_bf16_to_fp32(X)
+    Y = convert_bf16_to_fp32(Y)
     return maximum_kernel(X, Y)
