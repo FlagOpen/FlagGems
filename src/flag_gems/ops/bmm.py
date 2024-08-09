7addda7dbadbfade60edbab6420df69432ac67f0
Zhendong Fu <zhendong.fu@iluvatar.com>
2024-08-09 18:08:06 +0800

    Add prune configs for bmm

--- src/flag_gems/ops/bmm.py
+++ src/flag_gems/ops/bmm.py
@@ -3,32 +3,50 @@ import logging
 import torch
 import triton
 import triton.language as tl
+from triton.ops.matmul_perf_model import early_config_prune, estimate_matmul_time
 
 from ..utils import libentry
 
 
 def heur_divisible_m(args):
-    return args["M"] % args["TILE_M"] == 0
+    return args["M"] % args["BLOCK_M"] == 0
 
 
 def heur_divisible_n(args):
-    return args["N"] % args["TILE_N"] == 0
+    return args["N"] % args["BLOCK_N"] == 0
 
 
 def heur_divisible_k(args):
-    return args["K"] % args["TILE_K"] == 0
+    return args["K"] % args["BLOCK_K"] == 0
+
 
 def get_configs_io_bound():
     configs = []
     for num_stages in [1, 2]:
-      for block_m in [16, 32] if torch.version.hip is None and not hasattr(torch, "corex") else [32, 64]:
-        for block_k in [32, 64]:
-          for block_n in [32, 64, 128, 256]:
-            for group_m in [1, 2]:
-              num_warps = 4 if block_n <= 64 else 8
-              configs.append(triton.Config({'TILE_M': block_m, 'TILE_N': block_n, 'TILE_K': block_k, 'GROUP_M': group_m},
-                            num_stages=num_stages, num_warps=num_warps))
+        for block_m in (
+            [16, 32]
+            if torch.version.hip is None and not hasattr(torch, "corex")
+            else [32, 64]
+        ):
+            for block_k in [32, 64]:
+                for block_n in [32, 64, 128, 256]:
+                    for group_m in [1, 2]:
+                        num_warps = 4 if block_n <= 64 else 8
+                        configs.append(
+                            triton.Config(
+                                {
+                                    "BLOCK_M": block_m,
+                                    "BLOCK_N": block_n,
+                                    "BLOCK_K": block_k,
+                                    "GROUP_M": group_m,
+                                    "SPLIT_K": 1,
+                                },
+                                num_stages=num_stages,
+                                num_warps=num_warps,
+                            )
+                        )
     return configs
+
 
 def get_configs_compute_bound():
     configs = []
@@ -39,72 +57,83 @@ def get_configs_compute_bound():
                     for group_m in [1, 2]:
                         num_warps = 8 if block_n <= 64 else 16
                         configs.append(
-                                triton.Config({'TILE_M': block_m, 'TILE_N': block_n, 'TILE_K': block_k, 'GROUP_M': group_m},
-                                        num_stages=num_stages, num_warps=num_warps))
+                            triton.Config(
+                                {
+                                    "BLOCK_M": block_m,
+                                    "BLOCK_N": block_n,
+                                    "BLOCK_K": block_k,
+                                    "GROUP_M": group_m,
+                                    "SPLIT_K": 1,
+                                },
+                                num_stages=num_stages,
+                                num_warps=num_warps,
+                            )
+                        )
     return configs
+
 
 def get_nv_configs():
     configs = []
     if hasattr(torch, "corex"):
-      return configs
-    configs=[
+        return configs
+    configs = [
         triton.Config(
-            {"TILE_M": 32, "TILE_N": 32, "TILE_K": 32, "GROUP_M": 1},
+            {"BLOCK_M": 32, "BLOCK_N": 32, "BLOCK_K": 32, "GROUP_M": 1, "SPLIT_K": 1},
             num_warps=4,
             num_stages=2,
         ),
         triton.Config(
-            {"TILE_M": 64, "TILE_N": 32, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 64, "BLOCK_N": 32, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=2,
         ),
         triton.Config(
-            {"TILE_M": 64, "TILE_N": 64, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 64, "BLOCK_N": 64, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=2,
         ),
         triton.Config(
-            {"TILE_M": 128, "TILE_N": 32, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 128, "BLOCK_N": 32, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=2,
         ),
         triton.Config(
-            {"TILE_M": 128, "TILE_N": 64, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 128, "BLOCK_N": 64, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=2,
         ),
         triton.Config(
-            {"TILE_M": 128, "TILE_N": 128, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 128, "BLOCK_N": 128, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=2,
         ),
         triton.Config(
-            {"TILE_M": 32, "TILE_N": 32, "TILE_K": 32, "GROUP_M": 1},
+            {"BLOCK_M": 32, "BLOCK_N": 32, "BLOCK_K": 32, "GROUP_M": 1, "SPLIT_K": 1},
             num_warps=4,
             num_stages=3,
         ),
         triton.Config(
-            {"TILE_M": 64, "TILE_N": 32, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 64, "BLOCK_N": 32, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=3,
         ),
         triton.Config(
-            {"TILE_M": 64, "TILE_N": 64, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 64, "BLOCK_N": 64, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=3,
         ),
         triton.Config(
-            {"TILE_M": 128, "TILE_N": 32, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 128, "BLOCK_N": 32, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=3,
         ),
         triton.Config(
-            {"TILE_M": 128, "TILE_N": 64, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 128, "BLOCK_N": 64, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=3,
         ),
         triton.Config(
-            {"TILE_M": 128, "TILE_N": 128, "TILE_K": 32, "GROUP_M": 2},
+            {"BLOCK_M": 128, "BLOCK_N": 128, "BLOCK_K": 32, "GROUP_M": 2, "SPLIT_K": 1},
             num_warps=4,
             num_stages=3,
         ),
@@ -114,8 +143,15 @@ def get_nv_configs():
 
 @libentry()
 @triton.autotune(
-    get_nv_configs() + get_configs_compute_bound() + get_configs_io_bound(),
+    # get_nv_configs() + get_configs_compute_bound() + get_configs_io_bound(),
+    # get_configs_compute_bound(),
+    configs=get_configs_io_bound(),
     key=["M", "N", "K"],
+    prune_configs_by={
+        "early_config_prune": early_config_prune,
+        "perf_model": estimate_matmul_time,
+        "top_k": 10,
+    },
 )
 @triton.heuristics(
     {
@@ -128,14 +164,15 @@ def get_nv_configs():
 def bmm_kernel(
     A,
     B,
-    O,
+    C,
     M,
     N,
     K,
-    TILE_M: tl.constexpr,
-    TILE_N: tl.constexpr,
-    TILE_K: tl.constexpr,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    BLOCK_K: tl.constexpr,
     GROUP_M: tl.constexpr,
+    SPLIT_K: tl.constexpr,
     DIVISIBLE_M: tl.constexpr,
     DIVISIBLE_N: tl.constexpr,
     DIVISIBLE_K: tl.constexpr,
@@ -144,7 +181,7 @@ def bmm_kernel(
     pid_b = tl.program_id(2)
     A += pid_b * M * K
     B += pid_b * K * N
-    O += pid_b * M * N
+    C += pid_b * M * N
 
     pidx = tl.program_id(0)
     pidy = tl.program_id(1)
@@ -168,9 +205,9 @@ def bmm_kernel(
         pid_m = group_id * GROUP_M + inner_group_id % GROUP_SIZE
         pid_n = inner_group_id // GROUP_SIZE
 
-    offs_m = pid_m * TILE_M + tl.arange(0, TILE_M)
-    offs_n = pid_n * TILE_N + tl.arange(0, TILE_N)
-    offs_k = tl.arange(0, TILE_K)
+    offs_m = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_n = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
+    offs_k = tl.arange(0, BLOCK_K)
 
     if not DIVISIBLE_M:
         mask_m = offs_m < M
@@ -179,10 +216,10 @@ def bmm_kernel(
 
     a_ptrs = A + offs_m[:, None] * K + offs_k[None, :]
     b_ptrs = B + offs_k[:, None] * N + offs_n[None, :]
-    o_ptrs = O + offs_m[:, None] * N + offs_n[None, :]
+    o_ptrs = C + offs_m[:, None] * N + offs_n[None, :]
 
-    num_iters = tl.cdiv(K, TILE_K)
-    o = tl.zeros((TILE_M, TILE_N), dtype=tl.float32)
+    num_iters = tl.cdiv(K, BLOCK_K)
+    o = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.float32)
     for _ in range(num_iters):
         if DIVISIBLE_K:
             if DIVISIBLE_M:
@@ -207,9 +244,9 @@ def bmm_kernel(
         a = tl.load(a_ptrs, mask_a)
         b = tl.load(b_ptrs, mask_b)
 
-        offs_k += TILE_K
-        a_ptrs += TILE_K
-        b_ptrs += TILE_K * N
+        offs_k += BLOCK_K
+        a_ptrs += BLOCK_K
+        b_ptrs += BLOCK_K * N
 
         o += tl.dot(a, b, allow_tf32=False)
 
@@ -233,8 +270,8 @@ def bmm(A, B):
     out = torch.empty((batch, M, N), dtype=A.dtype, device=A.device)
 
     grid_fn = lambda meta: (
-        triton.cdiv(meta["M"], meta["TILE_M"]),
-        triton.cdiv(meta["N"], meta["TILE_N"]),
+        triton.cdiv(meta["M"], meta["BLOCK_M"]),
+        triton.cdiv(meta["N"], meta["BLOCK_N"]),
         batch,
     )
     with torch.cuda.device(A.device):
