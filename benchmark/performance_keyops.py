b41e86393621c4f72348aa63fb3c2d4ceccd1ef5
mx-flaggems-user <m01080@metax-tech.com>
2024-08-09 18:30:31 +0800

    1. weekly test files updated 2. modify exponential_.py to adapt to triton

--- benchmark/performance_keyops.py
+++ benchmark/performance_keyops.py
@@ -14,19 +14,9 @@ torch.backends.cuda.matmul.allow_tf32 = False
 
 class Benchmark:
     def __init__(
-        self,
-        op_name,
-        torch_op,
-        arg_func,
-        dtypes,
-        batch,
-        sizes,
-        is_backward=False,
-        kwargs_func=None,
+        self, op_name, torch_op, arg_func, dtypes, batch, sizes, kwargs_func=None
     ):
         self.op_name = op_name
-        if is_backward:
-            self.op_name += " backward"
         self.torch_op = torch_op
         self.arg_func = arg_func
         self.kwargs_func = kwargs_func
@@ -34,30 +24,24 @@ class Benchmark:
         self.batch = batch
         self.sizes = sizes
         self.gems_op = None
-        self.is_backward = is_backward
 
     def set_gems(self, gems_op):
         self.gems_op = gems_op
 
     def profile(self, op, *args, **kwargs):
-        fn = lambda: op(*args, **kwargs)
-        if self.is_backward:
-            out = fn()
-            dout = torch.randn_like(out)
-            fn = lambda: out.backward(dout, retain_graph=True)
         if CPU_MODE:
             for i in range(WARMUP):
-                fn()
+                op(*args, **kwargs)
             torch.cuda.synchronize()
             start = time.time()
             for i in range(REPETITION):
-                fn()
+                op(*args, **kwargs)
             torch.cuda.synchronize()
             end = time.time()
             latency = (end - start) / REPETITION * 1000
         else:
             latency = triton.testing.do_bench(
-                fn,
+                lambda: op(*args, **kwargs),
                 warmup=WARMUP,
                 rep=REPETITION,
                 return_mode="median",
@@ -66,20 +50,12 @@ class Benchmark:
         return latency
 
     def run(self):
-        print(f"{self.op_name}")
         for size in self.sizes:
-            print(f"{size}", end="")
+            print(f"{BATCH}x{size}", end="")
             for dtype in self.dtypes:
                 args = ()
                 if self.arg_func is not None:
                     args = self.arg_func(dtype, self.batch, size)
-                if self.is_backward:
-                    args = tuple(
-                        a.clone().requires_grad_()
-                        if torch.is_tensor(a) and torch.is_floating_point(a)
-                        else a
-                        for a in args
-                    )
 
                 kwargs = {}
                 if self.kwargs_func is not None:
@@ -91,19 +67,14 @@ class Benchmark:
                 else:
                     with flag_gems.use_gems():
                         gems_perf = self.profile(self.torch_op, *args, **kwargs)
-                print(f", {torch_perf}, {gems_perf}", end="")
+                speedup = torch_perf / gems_perf
+                print(f", {speedup}", end="")
             print()
 
 
 FLOAT_DTYPES = [torch.float16, torch.float32, torch.bfloat16]
-INT_DTYPES = [torch.int16, torch.int32]
-
-
-DEFAULT_BATCH = 1
-POINTWISE_BATCH = 1024
-REDUCTION_BATCH = 1024
-BLAS_BATCH = 16
-SIZES = [i * 64 for i in range(1, 22, 5)]
+BATCH = 1024
+SIZES = [32, 96, 8192, 20480, 32768]
 
 
 def unary_arg(dtype, batch, size):
@@ -111,31 +82,74 @@ def unary_arg(dtype, batch, size):
     return (inp,)
 
 
-def unary_int_arg(dtype, batch, size):
-    inp = torch.randint(
-        low=0, high=0x7FFF, size=[batch, size], dtype=dtype, device="cuda"
+def test_perf_gelu():
+    bench = Benchmark(
+        op_name="gelu",
+        torch_op=torch.nn.functional.gelu,
+        arg_func=unary_arg,
+        dtypes=FLOAT_DTYPES,
+        batch=BATCH,
+        sizes=SIZES,
     )
-    return (inp,)
+    bench.run()
 
 
-def binary_args(dtype, batch, size):
-    inp1 = torch.randn([batch, size], dtype=dtype, device="cuda")
-    inp2 = torch.randn([batch, size], dtype=dtype, device="cuda")
-    return inp1, inp2
-
-
-def binary_int_args(dtype, batch, size):
-    inp1 = torch.randint(
-        low=0, high=0x7FFF, size=[batch, size], dtype=dtype, device="cuda"
+def test_perf_sigmoid():
+    bench = Benchmark(
+        op_name="sigmoid",
+        torch_op=torch.sigmoid,
+        arg_func=unary_arg,
+        dtypes=FLOAT_DTYPES,
+        batch=BATCH,
+        sizes=SIZES,
     )
-    inp2 = torch.randint(
-        low=0, high=0x7FFF, size=[batch, size], dtype=dtype, device="cuda"
+    bench.run()
+
+
+def test_perf_layernorm():
+    def layer_norm_args(dtype, batch, size):
+        inp = torch.randn([batch, size], dtype=dtype, device="cuda")
+        weight = torch.randn(
+            [
+                size,
+            ],
+            dtype=dtype,
+            device="cuda",
+        )
+        bias = torch.randn(
+            [
+                size,
+            ],
+            dtype=dtype,
+            device="cuda",
+        )
+        return (
+            inp,
+            [
+                size,
+            ],
+            weight,
+            bias,
+        )
+
+    bench = Benchmark(
+        op_name="layernorm",
+        torch_op=torch.layer_norm,
+        arg_func=layer_norm_args,
+        dtypes=FLOAT_DTYPES,
+        batch=BATCH,
+        sizes=SIZES,
     )
-    return inp1, inp2
+    bench.run()
 
 
-def ternary_args(dtype, batch, size):
-    inp1 = torch.randn([batch, size], dtype=dtype, device="cuda")
-    inp2 = torch.randn([batch, size], dtype=dtype, device="cuda")
-    inp3 = torch.randn([batch, size], dtype=dtype, device="cuda")
-    return inp1, inp2, inp3
+def test_perf_softmax():
+    bench = Benchmark(
+        op_name="softmax",
+        torch_op=torch.nn.functional.softmax,
+        arg_func=unary_arg,
+        dtypes=FLOAT_DTYPES,
+        batch=BATCH,
+        sizes=SIZES,
+    )
+    bench.run()
