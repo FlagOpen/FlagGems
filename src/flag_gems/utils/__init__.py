d3c046113cbc523945df27e731aef54a8fd808b3
GwokHiujin <guozi1625113688@163.com>
2024-08-12 17:37:15 +0800

    [chore] remove the previous offsets calculator in utils package's init code

--- src/flag_gems/utils/__init__.py
+++ src/flag_gems/utils/__init__.py
@@ -1,12 +1,11 @@
 from .libentry import libentry
 from .pointwise_dynamic import pointwise_dynamic
-from .shape_utils import broadcastable_to, dim_compress, offset_calculator, offsetCalculator, restride_dim
+from .shape_utils import broadcastable_to, dim_compress, offset_calculator, restride_dim
 
 __all__ = [
     "libentry",
     "pointwise_dynamic",
     "dim_compress",
-    "offsetCalculator",
     "restride_dim",
     "offset_calculator",
     "broadcastable_to",
