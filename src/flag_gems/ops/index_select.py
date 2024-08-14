e9d785718bfc17ab2fe64d142352150389a9ded8
GwokHiujin <guozi1625113688@163.com>
2024-08-14 16:18:05 +0800

    [fix] keep kernel-version of index_select

--- src/flag_gems/ops/index_select.py
+++ src/flag_gems/ops/index_select.py
@@ -8,9 +8,12 @@ from ..utils import dim_compress, libentry
 
 
 def cfggen():
-    block_m = [1, 2, 4, 8]
+    block_m = [1, 2, 4]
+    block_n = [1024, 2048, 4096]
     configs = [
-        triton.Config({"BLOCK_M": m, "BLOCK_N": 1024}, num_warps=4) for m in block_m
+        triton.Config({"BLOCK_M": m, "BLOCK_N": n}, num_warps=4)
+        for m in block_m
+        for n in block_n
     ]
     return configs
 
@@ -21,23 +24,22 @@ def cfggen():
 def index_select_kernel(
     inp, out, M, N, index, index_len, BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr
 ):
-    pid = tl.program_id(0)
-    rows_offsets = pid * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
+    pid_x = tl.program_id(axis=0)
+    pid_y = tl.program_id(axis=1)
+    rows_offsets = pid_x * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
     rows_mask = rows_offsets < M
+    cols_offsets = pid_y + tl.arange(0, BLOCK_N)
+    cols_mask = cols_offsets < N
 
-    for off in range(0, index_len, BLOCK_N):
-        cols_offsets = off + tl.arange(0, BLOCK_N)
-        block_mask = rows_mask and (cols_offsets < N)
-        out_mask = rows_mask and (cols_offsets < index_len)
+    block_mask = rows_mask and cols_mask
+    out_mask = rows_mask and (cols_offsets < index_len)
 
-        indices = tl.load(
-            index + cols_offsets, mask=(cols_offsets < index_len), other=0
-        )
-        inp_off = rows_offsets * N + indices[None, :]
-        out_off = rows_offsets * index_len + cols_offsets[None, :]
+    indices = tl.load(index + cols_offsets, mask=(cols_offsets < index_len), other=0)
+    inp_off = rows_offsets * N + indices[None, :]
+    out_off = rows_offsets * index_len + cols_offsets[None, :]
 
-        selected = tl.load(inp + inp_off, mask=block_mask, other=0.0)
-        tl.store(out + out_off, selected, mask=out_mask)
+    selected = tl.load(inp + inp_off, mask=block_mask, other=0.0)
+    tl.store(out + out_off, selected, mask=out_mask)
 
 
 def index_select(inp, dim, index):
@@ -50,22 +52,25 @@ def index_select(inp, dim, index):
         index = index.unsqueeze(0)
     dim = dim % inp.ndim
     inp_shape = list(inp.shape)
+    index_len = index.numel()
 
     # with dim_compress
     inp = dim_compress(inp, dim)
     N = inp_shape[dim]
     M = inp.numel() // N
-    index_len = index.numel()
     out_shape = list(inp.shape)
-    out_shape[inp.ndim - 1] = index.numel()
+    out_shape[inp.ndim - 1] = index_len
     out = torch.empty(out_shape, dtype=inp.dtype, device=inp.device)
 
-    grid = lambda meta: (triton.cdiv(M, meta["BLOCK_M"]),)
+    grid = lambda meta: (
+        triton.cdiv(M, meta["BLOCK_M"]),
+        triton.cdiv(index_len, meta["BLOCK_N"]),
+    )
     index_select_kernel[grid](inp, out, M, N, index, index_len)
     if dim != out.ndim - 1:
         order = [i for i in range(out.ndim - 1)]
         order.insert(dim, out.ndim - 1)
-        return out.permute(order).contiguous()
+        return out.permute(order)
     else:
         return out
     """
