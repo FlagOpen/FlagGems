641429d70da48b594456f6f9d6f27b96b724cd74
Bowen12992 <zhangbo.sjtu@qq.com>
2024-08-22 09:37:32 +0000

    try

--- tools/code_coverage/coverage.sh
+++ tools/code_coverage/coverage.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-echo "  Usage:   $0  pull_request_id "
+set -ex
 
+echo "  Usage:   $0  pull_request_id "
 echo "PR_ID: $1"
 
 PR_ID=$1
+
+PYTHON_BIN=/usr/bin/python3.11
 
 FlagGemsROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")/../../" && pwd )"
 echo ${FlagGemsROOT}
 
 cmds=(
    "CUDA_VISIBLE_DEVICES=0 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_unary_pointwise_ops.py &"
-   "CUDA_VISIBLE_DEVICES=0 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_pointwise_type_promotion.py &"
-   "CUDA_VISIBLE_DEVICES=1 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_binary_pointwise_ops.py &"
-   "CUDA_VISIBLE_DEVICES=1 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_tensor_constructor_ops.py &"
-   "CUDA_VISIBLE_DEVICES=1 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_distribution_ops.py &"
-   "CUDA_VISIBLE_DEVICES=2 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_blas_ops.py &"
-   "CUDA_VISIBLE_DEVICES=3 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_reduction_ops.py &"
-   "CUDA_VISIBLE_DEVICES=4 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_special_ops.py &"
-   "CUDA_VISIBLE_DEVICES=5 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_libentry.py &"
-   "CUDA_VISIBLE_DEVICES=5 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s examples/model_bert_test.py &"
+#    "CUDA_VISIBLE_DEVICES=0 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_pointwise_type_promotion.py &"
+#    "CUDA_VISIBLE_DEVICES=1 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_binary_pointwise_ops.py &"
+#    "CUDA_VISIBLE_DEVICES=1 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_tensor_constructor_ops.py &"
+#    "CUDA_VISIBLE_DEVICES=1 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_distribution_ops.py &"
+#    "CUDA_VISIBLE_DEVICES=2 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_blas_ops.py &"
+#    "CUDA_VISIBLE_DEVICES=3 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_reduction_ops.py &"
+#    "CUDA_VISIBLE_DEVICES=4 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_special_ops.py &"
+#    "CUDA_VISIBLE_DEVICES=5 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s tests/test_libentry.py &"
+#    "CUDA_VISIBLE_DEVICES=5 coverage run --parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests -m pytest -s examples/model_bert_test.py &"
 )
 
 declare -a exit_statuses
@@ -40,7 +43,7 @@ coverage combine
 coverage report -m
 coverage xml -i -o python-coverage.xml
 
-python3.11 ${FlagGemsROOT}/tools/code_coverage/python_coverage.py > python-coverage.info
+${PYTHON_BIN} ${FlagGemsROOT}/tools/code_coverage/python_coverage.py > python-coverage.info
 
 lcov --extract python-coverage.info \
     '*FlagGems*' \
@@ -55,9 +58,9 @@ genhtml -o python-coverage-full \
     python-coverage-full.info
 
 # git
-COVERAGE_DIFF_PATTERN="`python3.11 ${FlagGemsROOT}/tools/code_coverage/pull_request.py files ${PR_ID}`"
+COVERAGE_DIFF_PATTERN="`${PYTHON_BIN} ${FlagGemsROOT}/tools/code_coverage/pull_request.py files ${PR_ID}`"
 echo ${COVERAGE_DIFF_PATTERN}
-python3.11 ${FlagGemsROOT}/tools/code_coverage/pull_request.py diff ${PR_ID} > python-git-diff.out
+${PYTHON_BIN} ${FlagGemsROOT}/tools/code_coverage/pull_request.py diff ${PR_ID} > python-git-diff.out
 
 # bash
 lcov --extract python-coverage-full.info \
@@ -65,7 +68,7 @@ lcov --extract python-coverage-full.info \
     -o python-coverage-diff.info \
     --rc lcov_branch_coverage=0
 
-python3.11 ${FlagGemsROOT}/tools/code_coverage/coverage_diff.py python-coverage-diff.info python-git-diff.out > python-coverage-diff.tmp
+${PYTHON_BIN} ${FlagGemsROOT}/tools/code_coverage/coverage_diff.py python-coverage-diff.info python-git-diff.out > python-coverage-diff.tmp
 mv -f python-coverage-diff.tmp python-coverage-diff.info
 
 genhtml -o python-coverage-diff \
@@ -75,8 +78,8 @@ genhtml -o python-coverage-diff \
     --ignore-errors source \
     python-coverage-diff.info
 
-python3.11 ${FlagGemsROOT}/tools/code_coverage/jit_func_position.py ${COVERAGE_DIFF_PATTERN} > python-triton-jit-position.info
-python3.11 ${FlagGemsROOT}/tools/code_coverage/coverage_diff_discard.py python-coverage-diff.info python-triton-jit-position.info > python-coverage-discard-diff.info
+${PYTHON_BIN} ${FlagGemsROOT}/tools/code_coverage/jit_func_position.py ${COVERAGE_DIFF_PATTERN} > python-triton-jit-position.info
+${PYTHON_BIN} ${FlagGemsROOT}/tools/code_coverage/coverage_diff_discard.py python-coverage-diff.info python-triton-jit-position.info > python-coverage-discard-diff.info
 
 genhtml -o python-coverage-diff-discard \
     -t 'Python Diff Coverage Discard JIT' \
@@ -85,4 +88,4 @@ genhtml -o python-coverage-diff-discard \
     --ignore-errors source \
     python-coverage-discard-diff.info
 
-python3.11 ${FlagGemsROOT}/tools/code_coverage/coverage_lines.py  python-coverage-discard-diff.info 0.9
+${PYTHON_BIN} ${FlagGemsROOT}/tools/code_coverage/coverage_lines.py  python-coverage-discard-diff.info 0.9
