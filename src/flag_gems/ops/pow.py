b8491c2fe5a6718b4c894a2b4327a24cf381518e
mx-flaggems-user <m01080@metax-tech.com>
2024-08-26 17:02:02 +0800

    fix pow

--- src/flag_gems/ops/pow.py
+++ src/flag_gems/ops/pow.py
@@ -17,7 +17,7 @@ def pow_func(x, exponent):
 
 def pow_tensor_tensor(A, exponent):
     logging.debug("GEMS POW_TENSOR_TENSOR")
-    if exponent == 2:
+    if isinstance(exponent, (int, float)) and exponent == 2:
         return A * A
     return pow_func(A, exponent)
 
