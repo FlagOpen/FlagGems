91f52dd45c5671f16175008357060d784f70a3f1
GwokHiujin <guozi1625113688@163.com>
2024-09-03 03:42:16 +0000

    [fix] Del slice&select scatter's perf tests

--- benchmark/test_reduction_perf.py
+++ benchmark/test_reduction_perf.py
@@ -310,58 +310,6 @@ def test_perf_vector_norm():
     bench.run()
 
 
-def test_perf_slice_scatter():
-    def slice_scatter_args(dtype, batch, size):
-        shape = [batch, size]
-        inp = torch.randn(shape, dtype=dtype, device="cuda")
-
-        dim = 1
-        start = 32
-        end = 128
-        step = 3
-        range = end - start
-
-        valid_shape = shape
-        valid_shape[dim] = (range + (step - 1)) // step
-        src = torch.randn(valid_shape, dtype=dtype, device="cuda")
-
-        return (inp, src, dim, start, end, step)
-
-    bench = Benchmark(
-        op_name="slice_scatter",
-        torch_op=torch.slice_scatter,
-        arg_func=slice_scatter_args,
-        dtypes=FLOAT_DTYPES,
-        batch=REDUCTION_BATCH,
-        sizes=SIZES,
-    )
-    bench.run()
-
-
-def test_perf_select_scatter():
-    def select_scatter_args(dtype, batch, size):
-        shape = [batch, size]
-        inp = torch.randn(shape, dtype=dtype, device="cuda")
-
-        dim = 1
-        index = 32
-        src_shape = shape
-        del src_shape[dim]
-        src = torch.randn(src_shape, dtype=dtype, device="cuda")
-
-        return (inp, src, dim, index)
-
-    bench = Benchmark(
-        op_name="slice_scatter",
-        torch_op=torch.select_scatter,
-        arg_func=select_scatter_args,
-        dtypes=FLOAT_DTYPES,
-        batch=REDUCTION_BATCH,
-        sizes=SIZES,
-    )
-    bench.run()
-
-
 def test_perf_index_select():
     def index_select_args(dtype, batch, size):
         inp = torch.randn([batch, size], dtype=dtype, device="cuda")
