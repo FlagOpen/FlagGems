73c6d75f9253b40aa9f02a4ff02647384bc87ab2
StrongSpoon <strongspoon@outlook.com>
2024-09-04 07:49:37 +0000

    [bugfix] skip test of scatter & gather

--- tests/test_reduction_ops.py
+++ tests/test_reduction_ops.py
@@ -704,6 +704,209 @@ def test_accuracy_vectornorm(shape, ord, dim, keepdim, dtype):
     gems_assert_close(res_out, ref_out, dtype)
 
 
+@pytest.mark.skipif(True, reason="operator undone")
+@pytest.mark.parametrize("src_shape", [(128, 16 * i, 32 * i) for i in range(1, 10, 4)])
+@pytest.mark.parametrize("inp_shape", [(512, 32 * i, 64 * i) for i in range(1, 10, 4)])
+@pytest.mark.parametrize("dim", [0, 1, 2])
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_scatter_src(src_shape, inp_shape, dim, dtype):
+    inp = torch.randn(inp_shape, dtype=dtype, device="cuda")
+    src = torch.randn(src_shape, dtype=dtype, device="cuda")
+    size_dim = min(src_shape[dim], inp_shape[dim])
+
+    import random
+
+    index_shape = [
+        random.randint(1, min(src_shape[0], inp_shape[0])),
+        random.randint(1, min(src_shape[1], inp_shape[1])),
+        random.randint(1, min(src_shape[2], inp_shape[2])),
+    ]
+    index = torch.empty(tuple(index_shape), dtype=torch.long, device="cuda")
+
+    m, n, o = index_shape
+
+    index_size_dim = index_shape[dim]
+    # make unique indices
+    for i in range(1 if dim == 0 else m):
+        for j in range(1 if dim == 1 else n):
+            for k in range(1 if dim == 2 else o):
+                ii = [i, j, k]
+                ii[dim] = slice(0, index.size(dim) + 1)
+                index[tuple(ii)] = torch.randperm(size_dim)[0:index_size_dim]
+
+    ref_inp = to_reference(inp)
+    ref_index = to_reference(index)
+    ref_src = to_reference(src)
+    ref_out = torch.scatter(ref_inp, dim, ref_index, ref_src)
+    from src.flag_gems.ops import scatter_src
+
+    res_out = scatter_src(inp, dim, index, src)
+
+    gems_assert_equal(res_out, ref_out)
+
+
+@pytest.mark.skipif(True, reason="operator undone")
+@pytest.mark.parametrize("src_shape", [(2, 2, 2)])
+@pytest.mark.parametrize("inp_shape", [(3, 3, 3)])
+@pytest.mark.parametrize("dim", [0, 1, 2])
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_scatter_add(src_shape, inp_shape, dim, dtype):
+    inp = torch.randn(inp_shape, dtype=dtype, device="cuda")
+    src = torch.randn(src_shape, dtype=dtype, device="cuda")
+    size_dim = min(src_shape[dim], inp_shape[dim])
+
+    import random
+
+    index_shape = [
+        random.randint(1, min(src_shape[0], inp_shape[0])),
+        random.randint(1, min(src_shape[1], inp_shape[1])),
+        random.randint(1, min(src_shape[2], inp_shape[2])),
+    ]
+    index = torch.empty(tuple(index_shape), dtype=torch.long, device="cuda")
+
+    m, n, o = index_shape
+
+    index_size_dim = index_shape[dim]
+    # make unique indices
+    for i in range(1 if dim == 0 else m):
+        for j in range(1 if dim == 1 else n):
+            for k in range(1 if dim == 2 else o):
+                ii = [i, j, k]
+                ii[dim] = slice(0, index.size(dim) + 1)
+                index[tuple(ii)] = torch.randperm(size_dim)[0:index_size_dim]
+
+    ref_inp = to_reference(inp)
+    ref_index = to_reference(index)
+    ref_src = to_reference(src)
+    ref_out = torch.scatter(ref_inp, dim, ref_index, ref_src, reduce="add")
+    from src.flag_gems.ops import scatter_reduce
+
+    res_out = scatter_reduce(inp, dim, index, src, reduce="add")
+
+    gems_assert_close(res_out, ref_out, dtype)
+
+
+@pytest.mark.skipif(True, reason="operator undone")
+@pytest.mark.parametrize("src_shape", [(128, 16 * i, 32 * i) for i in range(1, 10, 4)])
+@pytest.mark.parametrize("inp_shape", [(512, 32 * i, 64 * i) for i in range(1, 10, 4)])
+@pytest.mark.parametrize("dim", [0, 1, 2])
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_scatter_mul(src_shape, inp_shape, dim, dtype):
+    inp = torch.randn(inp_shape, dtype=dtype, device="cuda")
+    src = torch.randn(src_shape, dtype=dtype, device="cuda")
+    size_dim = min(src_shape[dim], inp_shape[dim])
+
+    import random
+
+    index_shape = [
+        random.randint(1, min(src_shape[0], inp_shape[0])),
+        random.randint(1, min(src_shape[1], inp_shape[1])),
+        random.randint(1, min(src_shape[2], inp_shape[2])),
+    ]
+    index = torch.empty(tuple(index_shape), dtype=torch.long, device="cuda")
+
+    m, n, o = index_shape
+
+    index_size_dim = index_shape[dim]
+    # make unique indices
+    for i in range(1 if dim == 0 else m):
+        for j in range(1 if dim == 1 else n):
+            for k in range(1 if dim == 2 else o):
+                ii = [i, j, k]
+                ii[dim] = slice(0, index.size(dim) + 1)
+                index[tuple(ii)] = torch.randperm(size_dim)[0:index_size_dim]
+
+    ref_inp = to_reference(inp)
+    ref_index = to_reference(index)
+    ref_src = to_reference(src)
+    ref_out = torch.scatter(ref_inp, dim, ref_index, ref_src, reduce="multiply")
+    from src.flag_gems.ops import scatter_reduce
+
+    res_out = scatter_reduce(inp, dim, index, src, reduce="multiply")
+
+    gems_assert_close(res_out, ref_out, dtype)
+
+
+@pytest.mark.skipif(True, reason="operator undone")
+@pytest.mark.parametrize("inp_shape", [(512, 32 * i, 64 * i) for i in range(1, 10, 4)])
+@pytest.mark.parametrize("dim", [0, 1, 2])
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_gather(inp_shape, dim, dtype):
+    inp = torch.randn(inp_shape, dtype=dtype, device="cuda")
+    size_dim = inp_shape[dim]
+
+    import random
+
+    index_shape = [
+        random.randint(1, inp_shape[0]),
+        random.randint(1, inp_shape[1]),
+        random.randint(1, inp_shape[2]),
+    ]
+    index = torch.empty(tuple(index_shape), dtype=torch.long, device="cuda")
+
+    m, n, o = index_shape
+
+    index_size_dim = index_shape[dim]
+    # make unique indices
+    for i in range(1 if dim == 0 else m):
+        for j in range(1 if dim == 1 else n):
+            for k in range(1 if dim == 2 else o):
+                ii = [i, j, k]
+                ii[dim] = slice(0, index.size(dim) + 1)
+                index[tuple(ii)] = torch.randperm(size_dim)[0:index_size_dim]
+
+    ref_inp = to_reference(inp)
+    ref_index = to_reference(index)
+    ref_out = torch.gather(ref_inp, dim, ref_index)
+
+    from src.flag_gems.ops import gather
+
+    res_out = gather(inp, dim, index)
+
+    gems_assert_equal(res_out, ref_out)
+
+
+@pytest.mark.skipif(True, reason="operator undone")
+@pytest.mark.parametrize("out_shape", [(128, 16 * i, 32 * i) for i in range(1, 10, 4)])
+@pytest.mark.parametrize("inp_shape", [(512, 32 * i, 64 * i) for i in range(1, 10, 4)])
+@pytest.mark.parametrize("dim", [0, 1, 2])
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_gather_out(out_shape, inp_shape, dim, dtype):
+    inp = torch.randn(inp_shape, dtype=dtype, device="cuda")
+    size_dim = min(out_shape[dim], inp_shape[dim])
+
+    import random
+
+    index_shape = [
+        random.randint(1, min(out_shape[0], inp_shape[0])),
+        random.randint(1, min(out_shape[1], inp_shape[1])),
+        random.randint(1, min(out_shape[2], inp_shape[2])),
+    ]
+    index = torch.empty(tuple(index_shape), dtype=torch.long, device="cuda")
+    out = torch.randn(tuple(index_shape), dtype=dtype, device="cuda")
+
+    m, n, o = index_shape
+
+    index_size_dim = index_shape[dim]
+    # make unique indices
+    for i in range(1 if dim == 0 else m):
+        for j in range(1 if dim == 1 else n):
+            for k in range(1 if dim == 2 else o):
+                ii = [i, j, k]
+                ii[dim] = slice(0, index.size(dim) + 1)
+                index[tuple(ii)] = torch.randperm(size_dim)[0:index_size_dim]
+
+    ref_inp = to_reference(inp)
+    ref_index = to_reference(index)
+    ref_out = torch.gather(ref_inp, dim, ref_index, sparse_grad=False, out=out)
+
+    from src.flag_gems.ops import gather_out
+
+    res_out = gather_out(inp, dim, index, sparse_grad=False, out=out)
+
+    gems_assert_equal(res_out, ref_out)
+
+
 @pytest.mark.parametrize("shape", REDUCTION_SHAPES)
 @pytest.mark.parametrize("dim", DIM_LIST)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
