4a2fb57211fc9eeff172ad349d90f198a050afda
Clement Chan <iclementine@outlook.com>
2024-09-09 17:57:20 +0800

    add test for shape_utils

--- tools/op-unit-test.sh
+++ tools/op-unit-test.sh
@@ -28,6 +28,7 @@ cmds=(
    "CUDA_VISIBLE_DEVICES=5 coverage run ${COVERAGE_ARGS} -m pytest -s tests/test_special_ops.py &"
    "CUDA_VISIBLE_DEVICES=6 coverage run ${COVERAGE_ARGS} -m pytest -s tests/test_libentry.py &"
    "CUDA_VISIBLE_DEVICES=6 coverage run ${COVERAGE_ARGS} -m pytest -s tests/test_pointwise_dynamic.py &"
+   "CUDA_VISIBLE_DEVICES=6 coverage run ${COVERAGE_ARGS} -m pytest -s tests/test_shape_utils.py &"
 )
 
 declare -a exit_statuses
