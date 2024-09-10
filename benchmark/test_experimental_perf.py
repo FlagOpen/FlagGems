1bc253011703aa94b99cf8729cf5539f2e325821
Bowen12992 <zhangbo.sjtu@qq.com>
2024-09-10 02:00:43 +0000

    fix test and perf

--- benchmark/test_experimental_perf.py
+++ benchmark/test_experimental_perf.py
@@ -1,5 +1,7 @@
 import torch
 
+import flag_gems.experimental
+
 from .performance_utils import Benchmark
 
 
@@ -7,6 +9,8 @@ def test_perf_rad2_fft():
     def rad2_fft_kwargs(dtype, batch, size):
         x = torch.randn((size,), device="cuda", dtype=dtype)
         return (x,)
+
+    gems_op = flag_gems.experimental.fft.rad2_fft
 
     bench = Benchmark(
         op_name="fft.fft",
@@ -16,4 +20,5 @@ def test_perf_rad2_fft():
         batch=[1],
         sizes=[i * 1024 for i in range(1, 100, 10)],
     )
+    bench.set_gems(gems_op)
     bench.run()
--- tests/test_experimental_ops.py
+++ tests/test_experimental_ops.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 
 import flag_gems
+import flag_gems.experimental
 
 from .accuracy_utils import gems_assert_close
 
@@ -11,6 +12,5 @@ from .accuracy_utils import gems_assert_close
 def test_accuracy_fft(shape, dtype):
     x = torch.randn(size=shape, dtype=dtype, device="cuda")
     y_ref = torch.fft.fft(x)
-    with flag_gems.use_gems():
-        y_res = torch.fft.fft(x)
+    y_res = flag_gems.experimental.fft.rad2_fft(x)
     gems_assert_close(y_ref, y_res, dtype)
