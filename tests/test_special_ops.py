7fa501231fcf6aa4bd6c11179746669519472388
zfu82 <fuzhendong97@gmail.com>
2024-09-13 08:04:09 +0000

    Add exception test cases

--- tests/test_special_ops.py
+++ tests/test_special_ops.py
@@ -9,7 +9,6 @@ import flag_gems
 from .accuracy_utils import (
     ALL_INT_DTYPES,
     FLOAT_DTYPES,
-    HSTACK_SHAPES,
     INT_DTYPES,
     RESOLUTION,
     SPECIAL_SHAPES,
@@ -535,6 +534,13 @@ def test_accuracy_stack(shape, dim, dtype):
     gems_assert_equal(res_out, ref_out)
 
 
+HSTACK_SHAPES = [
+    [(8,), (16,)],
+    [(16, 256), (16, 128)],
+    [(20, 320, 15), (20, 160, 15), (20, 80, 15)],
+]
+
+
 @pytest.mark.parametrize("shape", HSTACK_SHAPES)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES + INT_DTYPES)
 def test_accuracy_hstack(shape, dtype):
@@ -547,9 +553,33 @@ def test_accuracy_hstack(shape, dtype):
             )
             for s in shape
         ]
-    ref_inp = [to_reference(_, True) for _ in inp]
+    ref_inp = [to_reference(_) for _ in inp]
     ref_out = torch.hstack(ref_inp)
 
     with flag_gems.use_gems():
         res_out = torch.hstack(inp)
     gems_assert_equal(res_out, ref_out)
+
+
+HSTACK_EXCEPTION_SHAPES = [
+    [(16, 256), (16,)],
+    [(16, 256), (8, 128)],
+]
+
+
+@pytest.mark.parametrize("shape", HSTACK_EXCEPTION_SHAPES)
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES + INT_DTYPES)
+def test_exception_hstack(shape, dtype):
+    if dtype in FLOAT_DTYPES:
+        inp = [torch.randn(s, dtype=dtype, device="cuda") for s in shape]
+    else:
+        inp = [
+            torch.randint(low=0, high=0x7FFF, size=s, dtype=dtype, device="cuda").to(
+                dtype
+            )
+            for s in shape
+        ]
+
+    with pytest.raises(RuntimeError):
+        with flag_gems.use_gems():
+            _ = torch.hstack(inp)
