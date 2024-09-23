930d4099506c1da87dcb7cdb65fe5852708d9ce2
Jian Li <jian.li@mthreads.com>
2024-09-23 13:16:09 +0000

    rebase on master commit 204f3d4c
    Signed-off-by: Jian Li <jian.li@mthreads.com>

--- tests/test_binary_pointwise_ops.py
+++ tests/test_binary_pointwise_ops.py
@@ -303,7 +303,7 @@ def test_accuracy_div_tensor_tensor(shape, dtype):
 def test_accuracy_div_tensor_scalar(shape, scalar, dtype):
     inp1 = torch.randn(shape, dtype=dtype, device=flag_gems.device)
     inp2 = scalar
-    ref_inp1 = to_reference(inp1, True)
+    ref_inp1 = to_reference(inp1, False)
 
     ref_out = torch.div(ref_inp1, inp2)
     with flag_gems.use_gems():
@@ -482,8 +482,6 @@ def test_accuracy_remainder(shape, dtype):
         dtype=dtype,
         device=flag_gems.device,
     )
-    # avoid torch complains RuntimeError: ZeroDivisionError
-    inp1 = torch.where(inp1 != 0, inp1, 1)
     inp2 = torch.randint(
         torch.iinfo(dtype).min,
         torch.iinfo(dtype).max,
@@ -1093,8 +1091,9 @@ def test_accuracy_isclose(shape, dtype, zero_tol, equal_nan, gen_nan):
                 dtype=dtype,
                 device=flag_gems.device,
             )
-            inp1.view(-1)[0] = -nan_num if gen_nan == 3 else nan_num
-            inp2.view(-1)[0] = -nan_num if gen_nan >= 3 else nan_num
+            # FIXME: Neg doesn't support double on torch_musa, so workaround temporarily.
+            inp1.view(-1)[0] = (-nan_num.cpu()).to("musa") if gen_nan == 3 else nan_num
+            inp2.view(-1)[0] = (-nan_num.cpu()).to("musa") if gen_nan >= 3 else nan_num
         atol = (
             torch.finfo(dtype).tiny
             * torch.randint(0, 4, (1,), device=flag_gems.device).item()
@@ -1194,8 +1193,9 @@ def test_accuracy_allclose(shape, dtype, equal_nan, gen_nan):
                 dtype=dtype,
                 device=flag_gems.device,
             )
-            inp1.view(-1)[0] = -nan_num if gen_nan == 3 else nan_num
-            inp2.view(-1)[0] = -nan_num if gen_nan >= 3 else nan_num
+            # FIXME: Neg doesn't support double on torch_musa, so workaround temporarily.
+            inp1.view(-1)[0] = (-nan_num.cpu()).to("musa") if gen_nan == 3 else nan_num
+            inp2.view(-1)[0] = (-nan_num.cpu()).to("musa") if gen_nan >= 3 else nan_num
     else:
         atol = (
             torch.finfo(torch.float16).eps
