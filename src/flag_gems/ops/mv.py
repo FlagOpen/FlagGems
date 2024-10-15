2daffb966b9904705e87df1531fe55cbfa69df2b
junjian.zhan <junjian.zhan@iluvatar.com>
2024-10-15 11:23:05 +0800

    [TEST] fix outer UT failure when M=1, dtype=float16

--- src/flag_gems/ops/mv.py
+++ src/flag_gems/ops/mv.py
@@ -12,7 +12,7 @@ from ..utils import triton_lang_extension as tle
 @triton.autotune(
     configs=[
         triton.Config({"BLOCK_M": m, "BLOCK_N": n}, num_stages=s, num_warps=w)
-        for m in [32, 64, 128]
+        for m in [1, 32, 64, 128]
         for n in [1, 2, 4, 8]
         for s in [3, 4]
         for w in [4, 8]
