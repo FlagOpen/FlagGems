4f7e7924cd47f8132ce98a67fe7601b09b7371e0
Hiujin Gwok <70586936+GwokHiujin@users.noreply.github.com>
2024-10-28 12:27:02 +0800

    [Operator] slice&select scatter (#143)
    * add Ops & UT & Bench
    
    * add full zero ones Ops & UT & Bench
    
    * split normal op
    
    * [Operator] init slice&select scatter
    
    * code format
    
    * PR comment
    
    * split test_special_ops
    
    * add K-S test
    
    * split special perf
    
    * Exponential added. (#138)
    
    * exponential added.
    * Added K-S tests to exponential_, fp64 corrected.
    * aligned with aten prototype
    * Exponential_ uses uint64 offsets in Triton kernel.
    * Update pyproject config for new test dependencies.
    
    * resolve conflict
    
    * Use int64 indexing when needed & fix argmax (#146)
    
     1. fix amax, armax and triu, use int64 indexing when the largest tensor's size_in_bytes exceed int32's max;
    2. change the tiling scheme for argmax to loop in the reduction dimension, instead of data-size-dependent-tile-size
    
    * test for op
    
    * test for op
    
    * Making libentry thread safe (#136)
    
    * libentry now is lock protected.
    
    * Add multithreading tests for libentry.
    
    * polish code.
    
    * add argparse
    
    * fix desc
    
    * fix num
    
    * Update test_specific_ops.py
    
    * split UT files
    
    * fix
    
    * fix
    
    * [Operator] Optimize CrossEntropyLoss (#131)
    
    reimplement cross_entropy_loss forward and backward
    support; indices/probabilities/weight/reduction/ignore_index/label_smoothing; perform better than torch eager on large scale tensors
    
    * Exponential added. (#138)
    
    * exponential added.
    * Added K-S tests to exponential_, fp64 corrected.
    * aligned with aten prototype
    * Exponential_ uses uint64 offsets in Triton kernel.
    * Update pyproject config for new test dependencies.
    
    * Use int64 indexing when needed & fix argmax (#146)
    
     1. fix amax, armax and triu, use int64 indexing when the largest tensor's size_in_bytes exceed int32's max;
    2. change the tiling scheme for argmax to loop in the reduction dimension, instead of data-size-dependent-tile-size
    
    * Making libentry thread safe (#136)
    
    * libentry now is lock protected.
    
    * Add multithreading tests for libentry.
    
    * polish code.
    
    * [Test] Test for op (#151)
    
    * [chore] solve slice&select scatter's test cases
    
    * [fix] fix slice&select scatter's test cases
    
    * [chore] remove out-of-range indices in select_scatter's test cases
    
    * [chore] simplify slice_scatter's test cases
    
    * [fix] Added range that is deleted by mistake
    
    * Merge branch 'master' into slice&select_scatter
    
    * [chore] reformat
    
    * [fix] typo
    
    * [chore] Considering perf, pause the replacement of some aTen operators
    * slice_scatter
    * select_scatter
    * index_select
    
    * [fix] Add libentry in op.cumsum
    
    * [fix] Del slice&select scatter's perf tests
    
    * [Chore] Add pytest mark for slice&select scatter's test
    
    * [Fix] Correct slice_scatter test
    
    * [Fix] Replace CPU Tensor
    
    ---------
    
    Co-authored-by: Bowen12992 <zhangbluestars@gmail.com>
    Co-authored-by: Tongxin Bai <waffle.bai@gmail.com>
    Co-authored-by: Clement Chan <iclementine@outlook.com>
    Co-authored-by: Bowen <81504862+Bowen12992@users.noreply.github.com>
    Co-authored-by: StrongSpoon <35829812+StrongSpoon@users.noreply.github.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -80,9 +80,11 @@ from .resolve_neg import resolve_neg
 from .rms_norm import rms_norm
 from .rsqrt import rsqrt
 from .scatter import scatter
+from .select_scatter import select_scatter
 from .sigmoid import sigmoid
 from .silu import silu
 from .sin import sin
+from .slice_scatter import slice_scatter
 from .softmax import softmax
 from .stack import stack
 from .sub import sub
@@ -226,6 +228,8 @@ __all__ = [
     "where_self",
     "where_scalar_self",
     "where_scalar_other",
+    "select_scatter",
+    "slice_scatter",
     "masked_fill",
     "_unique2",
     "_upsample_bicubic2d_aa",
