eb59d333709a9d6c937743074cddff52b759cfe0
Hiujin Gwok <70586936+GwokHiujin@users.noreply.github.com>
2024-10-28 12:27:02 +0800

    [Operator] slice&select scatter (#143)
    * add Ops & UT & Bench
    
    * add full zero ones Ops & UT & Bench
    
    * split normal op
    
    * [Operator] init slice&select scatter
    
    * code format
    
    * PR comment
    
    * split test_special_ops
    
    * add K-S test
    
    * split special perf
    
    * Exponential added. (#138)
    
    * exponential added.
    * Added K-S tests to exponential_, fp64 corrected.
    * aligned with aten prototype
    * Exponential_ uses uint64 offsets in Triton kernel.
    * Update pyproject config for new test dependencies.
    
    * resolve conflict
    
    * Use int64 indexing when needed & fix argmax (#146)
    
     1. fix amax, armax and triu, use int64 indexing when the largest tensor's size_in_bytes exceed int32's max;
    2. change the tiling scheme for argmax to loop in the reduction dimension, instead of data-size-dependent-tile-size
    
    * test for op
    
    * test for op
    
    * Making libentry thread safe (#136)
    
    * libentry now is lock protected.
    
    * Add multithreading tests for libentry.
    
    * polish code.
    
    * add argparse
    
    * fix desc
    
    * fix num
    
    * Update test_specific_ops.py
    
    * split UT files
    
    * fix
    
    * fix
    
    * [Operator] Optimize CrossEntropyLoss (#131)
    
    reimplement cross_entropy_loss forward and backward
    support; indices/probabilities/weight/reduction/ignore_index/label_smoothing; perform better than torch eager on large scale tensors
    
    * Exponential added. (#138)
    
    * exponential added.
    * Added K-S tests to exponential_, fp64 corrected.
    * aligned with aten prototype
    * Exponential_ uses uint64 offsets in Triton kernel.
    * Update pyproject config for new test dependencies.
    
    * Use int64 indexing when needed & fix argmax (#146)
    
     1. fix amax, armax and triu, use int64 indexing when the largest tensor's size_in_bytes exceed int32's max;
    2. change the tiling scheme for argmax to loop in the reduction dimension, instead of data-size-dependent-tile-size
    
    * Making libentry thread safe (#136)
    
    * libentry now is lock protected.
    
    * Add multithreading tests for libentry.
    
    * polish code.
    
    * [Test] Test for op (#151)
    
    * [chore] solve slice&select scatter's test cases
    
    * [fix] fix slice&select scatter's test cases
    
    * [chore] remove out-of-range indices in select_scatter's test cases
    
    * [chore] simplify slice_scatter's test cases
    
    * [fix] Added range that is deleted by mistake
    
    * Merge branch 'master' into slice&select_scatter
    
    * [chore] reformat
    
    * [fix] typo
    
    * [chore] Considering perf, pause the replacement of some aTen operators
    * slice_scatter
    * select_scatter
    * index_select
    
    * [fix] Add libentry in op.cumsum
    
    * [fix] Del slice&select scatter's perf tests
    
    * [Chore] Add pytest mark for slice&select scatter's test
    
    * [Fix] Correct slice_scatter test
    
    * [Fix] Replace CPU Tensor
    
    ---------
    
    Co-authored-by: Bowen12992 <zhangbluestars@gmail.com>
    Co-authored-by: Tongxin Bai <waffle.bai@gmail.com>
    Co-authored-by: Clement Chan <iclementine@outlook.com>
    Co-authored-by: Bowen <81504862+Bowen12992@users.noreply.github.com>
    Co-authored-by: StrongSpoon <35829812+StrongSpoon@users.noreply.github.com>

--- src/flag_gems/ops/select_scatter.py
+++ src/flag_gems/ops/select_scatter.py
@@ -4,6 +4,54 @@ import torch
 
 from ..ops.copy import copy
 from ..utils.shape_utils import has_internal_overlapping
+=======
+import triton
+import triton.language as tl
+
+from ..utils import libentry, offsetCalculator, restride_dim
+
+
+def cfggen():
+    block_m = [1, 2, 4, 8]
+    configs = [
+        triton.Config({"BLOCK_M": m, "BLOCK_N": 1024}, num_warps=4) for m in block_m
+    ]
+    return configs
+
+
+@libentry()
+@triton.autotune(configs=cfggen(), key=["M", "N"])
+@triton.jit
+def select_scatter_kernel(
+    inp,
+    inp_indices,
+    src,
+    src_offsets,
+    M,
+    N,
+    index,
+    stride_dim,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+):
+    pid = tl.program_id(0)
+    rows_offsets = pid * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
+    rows_mask = rows_offsets < M
+
+    for off in range(0, N, BLOCK_N):
+        cols_offsets = off + tl.arange(0, BLOCK_N)[None, :]
+        cols_mask = cols_offsets < N
+
+        offsets = rows_offsets * N + cols_offsets
+        mask = rows_mask and cols_mask
+
+        indices = tl.load(inp_indices + offsets, mask=mask, other=0)
+        src_indices = tl.load(src_offsets + offsets, mask=mask, other=0)
+        cur_src = tl.load(src + src_indices, mask=mask, other=0)
+
+        indices += index * stride_dim
+        tl.store(inp + indices, cur_src, mask=mask)
+>>>>>>> 89c65c7 ([Operator] slice&select scatter (#143))
 
 
 def select_scatter(inp, src, dim, index):
