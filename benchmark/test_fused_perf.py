4e6cb3b2efeca8ae34b171226aa333d526636f82
kiddyjinjin <54064850+kiddyjinjin@users.noreply.github.com>
2024-10-30 15:15:20 +0800

    benchmark fix (#229)
    * benchmark fix
    
    *  add seven new testing parameters
    
    * move shapes info to yaml file
    
    * Added the BenchmarkMetrics & BenchmarkResult  abstraction

--- benchmark/test_fused_perf.py
+++ benchmark/test_fused_perf.py
@@ -1,118 +1,82 @@
+import pytest
 import torch
 
 import flag_gems
 
+from .attri_util import FLOAT_DTYPES
 from .performance_utils import (
-    FLOAT_DTYPES,
-    POINTWISE_BATCH,
-    REDUCTION_BATCH,
-    SIZES,
-    Benchmark,
-    binary_args,
+    GenericBenchmark,
+    GenericBenchmarkExcluse1D,
+    binary_input_fn,
 )
 
 
+@pytest.mark.gelu_and_mul
 def test_perf_gelu_and_mul():
     def torch_op(x, y):
         return torch.mul(torch.nn.functional.gelu(x), y)
 
     gems_op = flag_gems.gelu_and_mul
-
-    bench = Benchmark(
+    bench = GenericBenchmark(
+        input_fn=binary_input_fn,
         op_name="gelu_and_mul",
         torch_op=torch_op,
-        arg_func=binary_args,
         dtypes=FLOAT_DTYPES,
-        batch=POINTWISE_BATCH,
-        sizes=SIZES,
     )
     bench.set_gems(gems_op)
     bench.run()
 
 
+@pytest.mark.silu_and_mul
 def test_perf_silu_and_mul():
     def torch_op(x, y):
         return torch.mul(torch.nn.functional.silu(x), y)
 
     gems_op = flag_gems.silu_and_mul
 
-    bench = Benchmark(
+    bench = GenericBenchmark(
+        input_fn=binary_input_fn,
         op_name="silu_and_mul",
         torch_op=torch_op,
-        arg_func=binary_args,
         dtypes=FLOAT_DTYPES,
-        batch=POINTWISE_BATCH,
-        sizes=SIZES,
     )
     bench.set_gems(gems_op)
     bench.run()
 
 
+@pytest.mark.skip_layernorm
 def test_perf_skip_layernorm():
-    def skip_layernorm_args(dtype, batch, size):
-        inp = torch.randn([batch, size], dtype=dtype, device="cuda")
-        residual = torch.randn([batch, size], dtype=dtype, device="cuda")
-        weight = torch.randn(
-            [
-                size,
-            ],
-            dtype=dtype,
-            device="cuda",
-        )
-        bias = torch.randn(
-            [
-                size,
-            ],
-            dtype=dtype,
-            device="cuda",
-        )
-        return (
-            inp,
-            residual,
-            [
-                size,
-            ],
-            weight,
-            bias,
-        )
+    def skip_layernorm_input_fn(shape, dtype, device):
+        inp = torch.randn(shape, dtype=dtype, device=device)
+        residual = torch.randn(shape, dtype=dtype, device=device)
+        layer_shape = (shape[-1],)
+        weight = torch.randn(layer_shape, dtype=dtype, device=device)
+        bias = torch.randn(layer_shape, dtype=dtype, device=device)
+        yield inp, residual, layer_shape, weight, bias
 
     def torch_op(inp, residual, layer_shape, weight, bias):
         return torch.layer_norm(inp + residual, layer_shape, weight, bias)
 
     gems_op = flag_gems.skip_layer_norm
 
-    bench = Benchmark(
+    bench = GenericBenchmarkExcluse1D(
+        input_fn=skip_layernorm_input_fn,
         op_name="skip_layernorm",
         torch_op=torch_op,
-        arg_func=skip_layernorm_args,
         dtypes=FLOAT_DTYPES,
-        batch=REDUCTION_BATCH,
-        sizes=SIZES,
     )
     bench.set_gems(gems_op)
     bench.run()
 
 
+@pytest.mark.skip_rmsnorm
 def test_perf_skip_rmsnorm():
-    def skip_rmsnorm_args(dtype, batch, size):
-        inp = torch.randn([batch, size], dtype=dtype, device="cuda")
-        residual = torch.randn([batch, size], dtype=dtype, device="cuda")
-        weight = torch.randn(
-            [
-                size,
-            ],
-            dtype=dtype,
-            device="cuda",
-        )
-        return (
-            inp,
-            residual,
-            [
-                size,
-            ],
-            weight,
-            1e-5,
-        )
+    def skip_rmsnorm_input_fn(shape, dtype, device):
+        inp = torch.randn(shape, dtype=dtype, device=device)
+        residual = torch.randn(shape, dtype=dtype, device=device)
+        layer_shape = (shape[-1],)
+        weight = torch.randn(layer_shape, dtype=dtype, device=device)
+        yield inp, residual, layer_shape, weight, 1e-5
 
     def torch_op(x, residual, layer_shape, weight, eps):
         x = x + residual
@@ -122,13 +86,14 @@ def test_perf_skip_rmsnorm():
 
     gems_op = flag_gems.skip_rms_norm
 
-    bench = Benchmark(
+    bench = GenericBenchmarkExcluse1D(
+        input_fn=skip_rmsnorm_input_fn,
         op_name="skip_rmsnorm",
         torch_op=torch_op,
-        arg_func=skip_rmsnorm_args,
         dtypes=FLOAT_DTYPES,
-        batch=REDUCTION_BATCH,
-        sizes=SIZES,
     )
     bench.set_gems(gems_op)
     bench.run()
+
+
+# TODO: apply_rotary_pos_emb
