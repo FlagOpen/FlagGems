9da92ff639fd4a111ed07f881065e15107391705
kiddyjinjin <kiddyjinjin@gmail.com>
2024-11-04 07:38:24 +0000

    fix repeat_interleave benchmark bug

--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -177,10 +177,11 @@ def setup_once(request):
         print("\nThis is query mode; all benchmark functions will be skipped.")
     else:
         note_info = (
-        f"\n\nNote: The 'size' field below is for backward compatibility with previous versions of the benchmark. "
-        f"\nThis field will be removed in a future release."
+            f"\n\nNote: The 'size' field below is for backward compatibility with previous versions of the benchmark. "
+            f"\nThis field will be removed in a future release."
         )
         print(note_info)
+
 
 @pytest.fixture()
 def extract_and_log_op_attributes(request):
