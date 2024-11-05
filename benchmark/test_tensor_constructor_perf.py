93446477908906ab20c29aaf14bc52668731ac18
yjl0101 <jialingyou001@gmail.com>
2024-11-05 15:24:49 +0800

    [Operator] Add randperm op (#185)

--- benchmark/test_tensor_constructor_perf.py
+++ benchmark/test_tensor_constructor_perf.py
@@ -12,45 +12,12 @@ from .performance_utils import (
 )
 
 
-<<<<<<< HEAD
-def test_perf_rand():
-    def rand_kwargs(dtype, batch, size):
-        return {"size": (batch, size), "dtype": dtype, "device": "musa"}
-
-    bench = Benchmark(
-        op_name="rand",
-        torch_op=torch.rand,
-        arg_func=None,
-        dtypes=FLOAT_DTYPES,
-        batch=POINTWISE_BATCH,
-        sizes=SIZES,
-        kwargs_func=rand_kwargs,
-    )
-    bench.run()
-
-
-def test_perf_randn():
-    def randn_kwargs(dtype, batch, size):
-        return {"size": (batch, size), "dtype": dtype, "device": "musa"}
-
-    bench = Benchmark(
-        op_name="randn",
-        torch_op=torch.randn,
-        arg_func=None,
-        dtypes=FLOAT_DTYPES,
-        batch=POINTWISE_BATCH,
-        sizes=SIZES,
-        kwargs_func=randn_kwargs,
-    )
-    bench.run()
-=======
 def generic_constructor_input_fn(shape, dtype, device):
     yield {"size": shape, "dtype": dtype, "device": device},
 
 
 def full_input_fn(shape, dtype, device):
     yield {"size": shape, "fill_value": 3.1415926, "dtype": dtype, "device": device},
->>>>>>> 4e6cb3b (benchmark fix (#229))
 
 
 def masked_fill_input_fn(shape, dtype, device):
@@ -65,48 +32,6 @@ def full_like_input_fn(shape, dtype, device):
     yield {"input": inp, "fill_value": 3.1415926},
 
 
-<<<<<<< HEAD
-def test_perf_ones():
-    def ones_kwargs(dtype, batch, size):
-        return {"size": (batch, size), "dtype": dtype, "device": "musa"}
-
-    bench = Benchmark(
-        op_name="ones",
-        torch_op=torch.ones,
-        arg_func=None,
-        dtypes=FLOAT_DTYPES,
-        batch=POINTWISE_BATCH,
-        sizes=SIZES,
-        kwargs_func=ones_kwargs,
-    )
-    bench.run()
-
-
-def test_perf_zeros():
-    def zeros_kwargs(dtype, batch, size):
-        return {"size": (batch, size), "dtype": dtype, "device": "musa"}
-
-    bench = Benchmark(
-        op_name="zeros",
-        torch_op=torch.zeros,
-        arg_func=None,
-        dtypes=FLOAT_DTYPES,
-        batch=POINTWISE_BATCH,
-        sizes=SIZES,
-        kwargs_func=zeros_kwargs,
-    )
-    bench.run()
-
-
-def test_perf_full():
-    def full_kwargs(dtype, batch, size):
-        return {
-            "size": (batch, size),
-            "fill_value": 3.1415926,
-            "dtype": dtype,
-            "device": "musa",
-        }
-=======
 def fill_input_fn(shape, dtype, device):
     input = torch.empty(shape, dtype=dtype, device=device)
     yield input, 3.14159,
@@ -126,7 +51,6 @@ def arange_input_fn(shape, dtype, device):
             "device": device,
             "dtype": dtype,
         },
->>>>>>> 4e6cb3b (benchmark fix (#229))
 
 
 # Define operations and their corresponding input functions
@@ -163,48 +87,15 @@ def test_tensor_constructor_benchmark(op_name, torch_op, input_fn):
     bench.run()
 
 
-# @pytest.mark.randperm
-# def test_perf_randperm():
-#     def randperm_input_fn(shape, dtype, device):
-#         yield {"n": shape[0], "dtype": dtype, "device": device},
+@pytest.mark.randperm
+def test_perf_randperm():
+    def randperm_input_fn(shape, dtype, device):
+        yield {"n": shape[0], "dtype": dtype, "device": device},
 
-<<<<<<< HEAD
-
-def test_perf_zeros_like():
-    bench = Benchmark(
-        op_name="zeros_like",
-        torch_op=torch.zeros_like,
-        arg_func=unary_arg,
-        dtypes=FLOAT_DTYPES,
-        batch=POINTWISE_BATCH,
-        sizes=SIZES,
+    bench = GenericBenchmark(
+        input_fn=randperm_input_fn,
+        op_name="randperm",
+        torch_op=torch.randperm,
+        dtypes=[torch.int32, torch.int64],
     )
     bench.run()
-
-
-def test_perf_full_like():
-    def full_kwargs(dtype, batch, size):
-        return {
-            "input": torch.randn([batch, size], dtype=dtype, device="musa"),
-            "fill_value": 3.1415926,
-        }
-
-    bench = Benchmark(
-        op_name="full_like",
-        torch_op=torch.full_like,
-        arg_func=None,
-        dtypes=FLOAT_DTYPES,
-        batch=POINTWISE_BATCH,
-        sizes=SIZES,
-        kwargs_func=full_kwargs,
-    )
-    bench.run()
-=======
-#     bench = GenericBenchmark(
-#         input_fn=randperm_input_fn,
-#         op_name="randperm",
-#         torch_op=torch.randperm,
-#         dtypes=[torch.int32, torch.int64],
-#     )
-#     bench.run()
->>>>>>> 4e6cb3b (benchmark fix (#229))
