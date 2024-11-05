189f64591b8b1e8f5fb529bc1595fd93a9e3b936
Tongxin Bai <waffle.bai@gmail.com>
2024-11-05 09:12:31 +0800

    Specializing slice_scatter. (#270)
    * specializing slice_scatter. WIP.
    
    * polish and refine 2d_inner cases.
    
    * fix slice_scatter error on 1d inputs.
    
    * test slice_scatter fallback

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -142,7 +142,7 @@ def enable(lib=aten_lib):
     lib.impl("fill.Scalar", fill_scalar, "CUDA")
     lib.impl("fill.Tensor", fill_tensor, "CUDA")
     lib.impl("flip", flip, "CUDA")
-    lib.impl("slice_scatter", slice_scatter, "CUDA")
+    lib.impl("slice_scatter", slice_scatter_v2, "CUDA")
     lib.impl("select_scatter", select_scatter, "CUDA")
     lib.impl("index_select", index_select, "CUDA")
     lib.impl("tile", tile, "CUDA")
