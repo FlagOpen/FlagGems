ab3610974f11e6f72dc0ef4692507a9d10cf39b8
Jiang Bin <jiangbin@xcoresigma.com>
2024-11-11 08:45:07 +0000

    add backward of conv2d

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -159,7 +159,7 @@ def enable(lib=aten_lib):
     lib.impl("repeat_interleave.self_int", repeat_interleave_self_int, "CUDA")
     lib.impl("vstack", vstack, "CUDA")
     lib.impl("repeat_interleave.Tensor", repeat_interleave_tensor, "CUDA")
-    lib.impl("conv2d", conv2d, "CUDA")
+    lib.impl("conv2d", conv2d, "AutogradCUDA")
     lib.impl("conv1d", conv1d, "CUDA")
     lib.impl("_conv_depthwise2d", _conv_depthwise2d, "CUDA")
     lib.impl("repeat_interleave.self_Tensor", repeat_interleave_self_tensor, "CUDA")
