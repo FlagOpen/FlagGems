2c4de710fb40fcbd8073791a634e6fc0364d4a5d
kiddyjinjin <54064850+kiddyjinjin@users.noreply.github.com>
2024-11-15 18:44:49 +0800

    Narrow Benchmark Size for UnaryReductionBenchmark, Handle OOM Exception (#289)
    * modify benchmark size for UnaryReductionBenchmark & handle OOM exception
    
    * set special shapes for backward function

--- benchmark/test_special_perf.py
+++ benchmark/test_special_perf.py
@@ -44,7 +44,7 @@ def resolve_conj_input_fn(shape, dtype, device):
 special_operations = [
     # Sorting Operations
     ("topk", torch.topk, FLOAT_DTYPES, topk_input_fn),
-    ("sort", torch.sort, FLOAT_DTYPES, sort_input_fn),
+    # ("sort", torch.sort, FLOAT_DTYPES, sort_input_fn),
     # Complex Operations
     ("resolve_neg", torch.resolve_neg, [torch.cfloat], resolve_neg_input_fn),
     ("resolve_conj", torch.resolve_conj, [torch.cfloat], resolve_conj_input_fn),
