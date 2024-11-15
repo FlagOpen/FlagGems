b24fe49a123e3b4a9851e52ae1dffcfb2bb67b42
chuanjiang.ma <chuanjiang.ma@mthreads.com>
2024-11-15 13:46:37 +0800

    SW-47833: skip one case for half type op not supported by torch_musa
    Signed-off-by: chuanjiang.ma <chuanjiang.ma@mthreads.com>

--- tests/test_special_ops.py
+++ tests/test_special_ops.py
@@ -445,6 +445,7 @@ def test_pad(shape, dtype, pad_mode, contiguous):
 
 
 @pytest.mark.skipif(flag_gems.vendor_name == "cambricon", reason="fix")
+@pytest.mark.skipif(flag_gems.device == "musa", reason="torch not supports half yet")
 @pytest.mark.upsample_bicubic2d_aa
 @pytest.mark.parametrize("align_corners", [False, True])
 @pytest.mark.parametrize("scale", [(2, 2), (2.1, 3.7), (1.3, 5.1), (0.3, 0.7)])
