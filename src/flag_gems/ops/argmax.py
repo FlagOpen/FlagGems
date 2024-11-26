06bfd2a92b261b9ba587ad5df5aacc3f40eaadf9
strongspoon <strongspoon@outlook.com>
2024-11-26 13:35:38 +0800

    [operator] turn autotuner to heuristics

--- src/flag_gems/ops/argmax.py
+++ src/flag_gems/ops/argmax.py
@@ -46,20 +46,18 @@ def argmax_kernel_2(mid_value, mid_index, out, mid_size, BLOCK_MID: tl.constexpr
     tl.store(out, out_val)
 
 
+def heur_block_m(args):
+    return 4 if args["M"] < 4096 else 8
+
+
 def heur_block_n(args):
     return min(4096, triton.next_power_of_2(args["N"]))
 
 
 @libentry()
-@triton.autotune(
-    configs=runtime.get_triton_config("argmax"),
-    key=[
-        "M",
-        "N",
-    ],
-)
 @triton.heuristics(
     {
+        "BLOCK_M": heur_block_m,
         "BLOCK_N": heur_block_n,
     }
 )
--- src/flag_gems/ops/log_softmax.py
+++ src/flag_gems/ops/log_softmax.py
@@ -10,8 +10,31 @@ from ..utils import libentry
 from ..utils import triton_lang_extension as tle
 
 
+def heur_block_m(args):
+    return triton.next_power_of_2(triton.cdiv(256, args["N"]))
+
+
+def heur_block_n(args):
+    return triton.next_power_of_2(args["N"])
+
+
+def heur_num_warps(args):
+    if args["N"] <= 1024:
+        return 4
+    elif args["N"] <= 2048:
+        return 8
+    else:
+        return 16
+
+
 @libentry()
-@triton.autotune(configs=runtime.get_triton_config("log_softmax"), key=["M", "N"])
+@triton.heuristics(
+    {
+        "BLOCK_M": heur_block_m,
+        "BLOCK_N": heur_block_n,
+        "num_warps": heur_num_warps,
+    }
+)
 @triton.jit
 def log_softmax_kernel(
     output_ptr,
