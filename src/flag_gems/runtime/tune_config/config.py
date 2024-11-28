dbb90d7bd4ef02126803d020dfa236a0ed339c1b
Your Name <2535184404@qq.com>
2024-11-28 16:06:56 +0800

    Modify gen_impl with loops instead of recursion

--- src/flag_gems/runtime/tune_config/config.py
+++ src/flag_gems/runtime/tune_config/config.py
@@ -1,3 +1,5 @@
+import copy
+
 import triton
 
 from .. import backend
@@ -24,42 +26,50 @@ class Config:
         gen_config,
         param_config,
         iteration_keys,
-        current_config,
-        current_iteration,
+        std_config,
     ):
-        if current_iteration == len(iteration_keys):
-            return [
-                triton.Config(
-                    current_config["META"],
-                    num_warps=current_config["num_warps"],
-                    num_stages=current_config["num_stages"],
-                    num_ctas=current_config["num_ctas"],
+        all_configs = []
+        final_step = len(iteration_keys)
+        stack = [{"cur_config": std_config, "current_step": 0}]
+
+        while stack:
+            cur_state = stack[-1]
+            stack.pop()
+            cur_config = cur_state.get("cur_config")
+            current_step = cur_state.get("current_step")
+
+            if current_step == final_step:
+                all_configs.append(
+                    triton.Config(
+                        cur_config["META"],
+                        num_warps=cur_config["num_warps"],
+                        num_stages=cur_config["num_stages"],
+                        num_ctas=cur_config["num_ctas"],
+                    )
                 )
-            ]
-        current_config_list = []
-        param_key = iteration_keys[current_iteration]
-        if param_key in param_config["META"]:
-            config_var_key = param_config["META"][param_key]
-        else:
-            config_var_key = param_config[param_key]
-        if isinstance(config_var_key, int):
-            key_config = [config_var_key]
-        else:
-            key_config = gen_config[config_var_key]
-        for single_value in key_config:
-            if param_key in param_config["META"]:
-                current_config["META"][param_key] = single_value
             else:
-                current_config[param_key] = single_value
-            config_item = self._gen_impl(
-                gen_config,
-                param_config,
-                iteration_keys,
-                current_config,
-                current_iteration + 1,
-            )
-            current_config_list += config_item
-        return current_config_list
+                cur_key = iteration_keys[current_step]
+                if cur_key in param_config["META"]:
+                    config_var_key = param_config["META"][cur_key]
+                else:
+                    config_var_key = param_config[cur_key]
+                if isinstance(config_var_key, int):
+                    key_config = [config_var_key]
+                else:
+                    key_config = gen_config[config_var_key]
+                for single_value in key_config:
+                    new_config = copy.deepcopy(cur_config)
+                    if cur_key in param_config["META"]:
+                        new_config["META"][cur_key] = single_value
+                    else:
+                        new_config[cur_key] = single_value
+                    stack.append(
+                        {
+                            "cur_config": new_config,
+                            "current_step": current_step + 1,
+                        }
+                    )
+        return all_configs
 
     def to_gen_config(self, gen_config):
         param_config = gen_config["param_map"]
@@ -68,13 +78,11 @@ class Config:
         iteration_keys.remove("META")
         current_config = {"META": {}}
         current_config.update(self.triton_config_default)
-        iteration_count_init = 0
         return self._gen_impl(
             gen_config,
             param_config,
             iteration_keys,
             current_config,
-            iteration_count_init,
         )
 
     def get_op_tune_config(self, op_name):
