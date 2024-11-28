3ada4daa6a170ec29ee902df87f8532c96b0d0ad
Bowen12992 <zhangbluestars@gmail.com>
2024-11-28 13:26:40 +0000

    fix

--- tools/op-utils-test.sh
+++ tools/op-utils-test.sh
@@ -12,6 +12,7 @@ fi
 ID_SHA="${PR_ID}-${GITHUB_SHA}"
 echo ID_SHA $ID_SHA
 
+echo $PWD
 source ./run_command.sh
 COVERAGE_ARGS="--parallel-mode --omit "*/.flaggems/*","*/usr/lib/*" --source=./src,./tests --data-file=${ID_SHA}-op"
 run_command coverage run ${COVERAGE_ARGS} -m pytest -s tests/test_libentry.py && \
