9de501e38a545aa0107ef89c72a1bb3c846992a3
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2024-12-12 20:10:31 +0800

    [Muti_backend]part_2_device (#344)
    * new feature, muti_backend
    
    * update auto_tune_module
    
    * update auto_tune_module
    
    * update auto_tune_module
    
    * update __init__
    
    * rebase
    
    * fix bug
    
    * modifiy auto_tune_config
    
    * fix bug
    
    * fix bug
    
    * update
    
    * update
    
    * update scatter&gather
    
    * fix auto_tune
    
    * add gen_torch_device_fn
    
    * fix codestyle
    
    * fix codestyle
    
    * Modify code based on comments
    
    * Modify gen_impl with loops instead of recursion
    
    * Update code structure
    
    * Polish code
    
    * update
    
    * Polish code
    
    * Modify code based on comments
    
    * modify based on comment
    
    * Modify code based on comments
    
    * update
    
    * final fix
    
    * modify_device_unify
    
    * fix bug
    
    * update
    
    * remove '-> list'
    
    * modify
    
    * modify
    
    * modify
    
    * fix bug

--- src/flag_gems/ops/tile.py
+++ src/flag_gems/ops/tile.py
@@ -55,6 +55,7 @@ def generate_imports(code: IndentedBuffer) -> IndentedBuffer:
     code.writeline("import triton")
     code.writeline("from triton import language as tl")
     code.newline()
+    code.writeline("from flag_gems.runtime import torch_device_fn")
     code.writeline("from flag_gems.utils.shape_utils import volume")
     code.writeline("from flag_gems.utils.libentry import libentry")
     code.writeline("from flag_gems.utils.type_utils import type_promotion")
@@ -170,7 +171,7 @@ def generate_destination_passing_tile_wrapper(
         code.writeline("# kernel launch")
 
         # launch kernel
-        code.writeline("with torch.cuda.device(in0.device.index):")
+        code.writeline("with torch_device_fn.device(in0.device.index):")
         with code.indent():
             kernel_launch: str = f"{kernel_name}[grid]("
             code.writeline(kernel_launch)
