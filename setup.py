0613a33f6ad41faffc33318cd821279ef16c68c3
Clement Chan <iclementine@outlook.com>
2024-12-17 16:50:47 +0800

    merge upstream changes

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ from setuptools import find_packages, setup
 
 
 class PackageConflictError(Exception):
+    """Exception that there are conflicts in installed packages."""
+
     def __init__(self, message):
         super().__init__(message)
 
@@ -26,6 +28,7 @@ class PackageConflictError(Exception):
 def detect_installed_package_from_group(
     conflicting_package_names: Sequence[str],
 ) -> Optional[str]:
+    """Detect the installed packages in a group of mutually conflicting packages."""
     names = set(conflicting_package_names)
     if len(names) < len(conflicting_package_names):
         raise ValueError(
@@ -58,7 +61,6 @@ def detect_installed_package_from_group(
 triton_package_name = (
     detect_installed_package_from_group(("triton", "pytorch-triton")) or "triton"
 )
-print(f"TRION_PACKAGE_NAME: {triton_package_name}")
 
 # ----------------------------- Setup -----------------------------
 setup(
@@ -73,9 +75,7 @@ setup(
     description="FlagGems is a function library written in Triton.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    # triton requires python>=3.10.10 for builtin packages inspect to correctly
-    # get source from triton jit functions.
-    python_requires=">=3.10.10",
+    python_requires=">=3.8.0",
     license="Apache Software License",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -83,8 +83,8 @@ setup(
         "License :: OSI Approved :: Apache Software License",
     ],
     install_requires=[
-        f"{triton_package_name}>=3.0.0",
-        "torch>=2.4.0",  # To use custom op
+        f"{triton_package_name}>=2.2.0",
+        "torch>=2.2.0",  # To use custom op
         "transformers>=4.40.2",
     ],
     extras_require={
@@ -101,5 +101,5 @@ setup(
     package_data={
         "flag_gems.runtime": ["**/*.yaml"],
     },
-    setup_requires=["setuptools", "cython"],
+    setup_requires=["setuptools"],
 )
