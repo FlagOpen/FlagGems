00ea9596122b72e85441b8f18267c2f623a05621
Tongxin Bai <waffle.bai@gmail.com>
2024-12-18 17:46:38 +0800

    Reimplement slice_scatter/select_scatter using pointwise_dynamic (#358)
    * merged with origin.
    * updated
    * renames copy_ to copy, fixes benchmark code.
    * fixes benchmark.
    * handle self-overlapping input in select_scatter and slice_scatter
    
    ---------
    
    Co-authored-by: Clement Chan <iclementine@outlook.com>

--- tests/test_reduction_ops.py
+++ tests/test_reduction_ops.py
@@ -745,40 +745,18 @@ def test_accuracy_select_scatter(shape, dim, dtype):
     gems_assert_equal(res_out, ref_out)
 
 
-@pytest.mark.slice_scatter
-@pytest.mark.parametrize(("dim", "shape", "stride"), DIM_SHAPE_STRIDES)
-@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
-@pytest.mark.parametrize("start", [16, 64])
-@pytest.mark.parametrize("end", [1024, 256])
-@pytest.mark.parametrize("step", [1, 2])
-def test_accuracy_slice_scatter_v2(shape, stride, dim, dtype, start, end, step):
-    inp = torch.empty_strided(shape, stride, dtype=dtype, device=flag_gems.device)
-    inp.copy_(1)
-
-    valid_shape = list(inp.shape)
-    size = valid_shape[dim]
-
-    start = start % size
-    end = end % (size + 1)
-
-    if end < start:
-        end, start = start, end
-    elif end == start:
-        end = size
-
-    valid_shape[dim] = (end - start + step - 1) // step
-
-    src = torch.rand(valid_shape, dtype=dtype, device=flag_gems.device)
+@pytest.mark.select_scatter
+def test_accuracy_select_scatter_with_self_overlapping_input():
+    dim = 0
+    index = 1
+    inp = torch.randn((1, 4), device=flag_gems.device).broadcast_to((3, 4))
+    src = torch.randn((4,), device=flag_gems.device)
 
     ref_inp = to_reference(inp)
     ref_src = to_reference(src)
-    ref_out = torch.slice_scatter(
-        ref_inp, dim=dim, src=ref_src, start=start, end=end, step=step
-    )
+    ref_out = torch.select_scatter(ref_inp, dim=dim, index=index, src=ref_src)
     with flag_gems.use_gems():
-        res_out = torch.slice_scatter(
-            inp, dim=dim, src=src, start=start, end=end, step=step
-        )
+        res_out = torch.select_scatter(inp, dim=dim, index=index, src=src)
 
     gems_assert_equal(res_out, ref_out)
 
@@ -789,7 +767,7 @@ def test_accuracy_slice_scatter_v2(shape, stride, dim, dtype, start, end, step):
 @pytest.mark.parametrize("start", [16, 64])
 @pytest.mark.parametrize("end", [1024, 256])
 @pytest.mark.parametrize("step", [1, 2])
-def test_accuracy_slice_scatter_fallback(shape, stride, dim, dtype, start, end, step):
+def test_accuracy_slice_scatter(shape, stride, dim, dtype, start, end, step):
     inp = torch.empty_strided(shape, stride, dtype=dtype, device=flag_gems.device)
     inp.copy_(1)
 
@@ -814,6 +792,27 @@ def test_accuracy_slice_scatter_fallback(shape, stride, dim, dtype, start, end, 
         ref_inp, dim=dim, src=ref_src, start=start, end=end, step=step
     )
 
+    res_out = flag_gems.ops.slice_scatter(
+        inp, dim=dim, src=src, start=start, end=end, step=step
+    )
+
+    gems_assert_equal(res_out, ref_out)
+
+
+@pytest.mark.slice_scatter
+def test_accuracy_slice_scatter_with_self_overlapping_input():
+    inp = torch.randn((3, 1), device=flag_gems.device).broadcast_to((3, 8))
+    src = torch.rand((3, 4), device=flag_gems.device)
+
+    start = 0
+    end = 8
+    step = 2
+    dim = 1
+    ref_inp = to_reference(inp)
+    ref_src = to_reference(src)
+    ref_out = torch.slice_scatter(
+        ref_inp, dim=dim, src=ref_src, start=start, end=end, step=step
+    )
     res_out = flag_gems.ops.slice_scatter(
         inp, dim=dim, src=src, start=start, end=end, step=step
     )
