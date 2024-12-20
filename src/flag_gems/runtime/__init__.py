644b4305ae89d0bc3af8e4d8aa9220619ec47716
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2024-12-20 16:23:04 +0800

    [Polish code] Adjust some directory structures and variable names (#373)
    * modify code
    
    * fix bug
    
    * fix bug
    
    * modify
    
    * fix
    
    * fix
    
    * polish code
    
    * polish code
    
    * polish code

--- src/flag_gems/runtime/__init__.py
+++ src/flag_gems/runtime/__init__.py
@@ -6,7 +6,6 @@ config_loader = ConfigLoader()
 device = DeviceDetector()
 # torch_device_fn is like 'torch.cuda' object
 torch_device_fn = backend.gen_torch_device_object()
-tl_extra_module = moduel_tool.tl_extra_module
 # torch_backend_device is like 'torch.backend.cuda' object
 torch_backend_device = backend.get_torch_backend_device_fn()
 
