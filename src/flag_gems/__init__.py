43ffb1713d0fb977576b35a5485317fd2a295c4b
ph0375 <wwwph410@vip.qq.com>
2024-12-23 20:52:06 +0800

    _init_

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -202,6 +202,7 @@ def enable(lib=aten_lib, unused=None, registrar=registrar):
             ("logical_and", logical_and, Autograd.disable),
             ("logical_xor", logical_xor, Autograd.disable),
             ("logical_not", logical_not, Autograd.disable),
+            ("kron", kron, Autograd.disable),
         ),
         user_unused_ops_list=[] if unused is None else unused,
         lib=lib,
