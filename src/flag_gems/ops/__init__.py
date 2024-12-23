a3cc724ee377e74b3408384bd38dcefdf84e285d
ph0375 <wwwph410@vip.qq.com>
2024-12-23 20:51:27 +0800

    _init

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -124,6 +124,7 @@ from .weightnorm import weight_norm, weight_norm_interface
 from .where import where_scalar_other, where_scalar_self, where_self, where_self_out
 from .zeros import zeros
 from .zeros_like import zeros_like
+from .kron import kron
 
 __all__ = [
     "all",
@@ -283,4 +284,5 @@ __all__ = [
     "logical_xor",
     "logical_not",
     "sort",
+    "kron",
 ]
