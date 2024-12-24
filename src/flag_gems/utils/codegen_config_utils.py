81a8becd86911a028658778a2c109c2a057d1560
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2024-12-24 13:50:44 +0800

    muti_backend (#376)

---
+++ src/flag_gems/utils/codegen_config_utils.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+from flag_gems.runtime import device
+from flag_gems.runtime.commom_utils import vendors
+
+# import triton
+
+
+@dataclass
+class CodeGenConfig:
+    max_tile_size: int
+    max_grid_size: Tuple[int, int, int]
+    max_num_warps_per_cta: int
+
+    prefer_block_pointer: bool
+    prefer_1d_tile: bool
+    # gen_configs: -> configs
+    # prune_config: (as jit function, ) cofigs -> configs
+
+    def __post_init__(self):
+        if self.prefer_1d_tile:
+            self.prefer_block_pointer = False
+
+
+CODEGEN_COFIGS = {
+    vendors.NVIDIA: CodeGenConfig(
+        512,
+        (65536, 65536, 65536),
+        32,
+        True,
+        prefer_1d_tile=True,
+    )
+}
+
+
+def get_codegen_config():
+    if device.vendor not in CODEGEN_COFIGS:
+        return CODEGEN_COFIGS.get(vendors.NVIDIA)
+    return CODEGEN_COFIGS.get(device.vendor)
