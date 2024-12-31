aafc2d2d45612278a0cdb04c2ecd89886aa3f623
jiaqi.wang <jiaqi.wang@mthreads.com>
2024-12-31 18:02:35 +0800

    SW-49945: adjust merged code
    Signed-off-by: jiaqi.wang <jiaqi.wang@mthreads.com>

--- src/flag_gems/runtime/backend/__init__.py
+++ src/flag_gems/runtime/backend/__init__.py
@@ -74,7 +74,8 @@ def set_torch_backend_device_fn(vendor_name=None):
     global device_name, torch_device_fn_device
     device_name = device_name or get_vendor_info(vendor_name).device_name
     module_str = f"torch.backends.{device_name}"
-    torch_device_fn_device = importlib.import_module(module_str)
+    # torch_device_fn_device = importlib.import_module(module_str)
+    torch_device_fn_device = None
 
 
 def get_torch_backend_device_fn():
