9ab11595df2c9f19b1fa9673633e193a59a2e15b
jiaqi.wang <jiaqi.wang@mthreads.com>
2025-01-03 18:07:38 +0800

    SW-49945: update configuration for mthreads
    Signed-off-by: jiaqi.wang <jiaqi.wang@mthreads.com>

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -1,3 +1,14 @@
-from .add import add
+from backend_utils import Autograd
 
-__all__ = ["add"]
+from . import add, gelu
+
+
+def get_specific_ops():
+    return ()
+
+
+def get_unused_ops():
+    return ()
+
+
+__all__ = ["get_specific_ops", "get_unused_ops"]
