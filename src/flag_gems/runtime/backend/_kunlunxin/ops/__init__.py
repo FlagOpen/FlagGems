5665a5c4464fd17a01251be1b088da85fcf16663
duanyaqi <duanyaqiduanyaqi@gmail.com>
2025-01-06 17:50:03 +0800

    [kunlunxin] add support

--- src/flag_gems/runtime/backend/_kunlunxin/ops/__init__.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/__init__.py
@@ -1,3 +1,17 @@
-from .add import add
+from backend_utils import Autograd
 
-__all__ = ["add"]
+from . import add, gelu
+
+
+def get_specific_ops():
+    return (
+        ("add.Tensor", add.add, Autograd.disable),
+        ("gelu", gelu.gelu, Autograd.enable),
+    )
+
+
+def get_unused_ops():
+    return ("cumsum", "cos")
+
+
+__all__ = ["get_specific_ops", "get_unused_ops"]
