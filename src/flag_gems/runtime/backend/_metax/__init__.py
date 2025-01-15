df457c8eb12d86b9624ecb08f063fbeb41c212b5
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2025-01-15 10:45:04 +0800

    [New Feature] Update muti_backend_framework (#407)
    * update muti_backend_register_actions
    
    * update framework
    
    * update
    
    * update
    
    * fix

--- src/flag_gems/runtime/backend/_metax/__init__.py
+++ src/flag_gems/runtime/backend/_metax/__init__.py
@@ -1,20 +1,7 @@
 from backend_utils import VendorInfoBase  # noqa: E402
 
-from .heuristics_config_utils import HEURISTICS_CONFIGS
-
-global specific_ops, unused_ops
 vendor_info = VendorInfoBase(
     vendor_name="metax", device_name="cuda", device_query_cmd="mx-smi"
 )
 
-
-def OpLoader():
-    global specific_ops, unused_ops
-    # if specific_ops is None:
-    from . import ops  # noqa: F403
-
-    specific_ops = ops.get_specific_ops()
-    unused_ops = ops.get_unused_ops()
-
-
-__all__ = ["HEURISTICS_CONFIGS", "vendor_info", "OpLoader"]
+__all__ = ["vendor_info"]
