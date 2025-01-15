2c77d4e2f531179f085a45d6ae295413f28248a2
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2025-01-15 10:45:04 +0800

    [New Feature] Update muti_backend_framework (#407)
    * update muti_backend_register_actions
    
    * update framework
    
    * update
    
    * update
    
    * fix

--- src/flag_gems/runtime/backend/_iluvatar/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/__init__.py
@@ -1,22 +1,9 @@
 from backend_utils import VendorInfoBase  # noqa: E402
 
-from .heuristics_config_utils import HEURISTICS_CONFIGS
-
-global specific_ops, unused_ops
-specific_ops = None
-unused_ops = None
 vendor_info = VendorInfoBase(
     vendor_name="iluvatar", device_name="cuda", device_query_cmd="ixsmi"
 )
 
+CUSTOMIZED_UNUSED_OPS = ("randperm", "topk", "sort", "multinomial")
 
-def OpLoader():
-    global specific_ops, unused_ops
-    if specific_ops is None:
-        from . import ops  # noqa: F403
-
-        specific_ops = ops.get_specific_ops()
-        unused_ops = ops.get_unused_ops()
-
-
-__all__ = ["HEURISTICS_CONFIGS", "vendor_info", "OpLoader"]
+__all__ = ["*"]
