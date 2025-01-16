26f0f12f906d97891d486278d8ddfa78e5774c1b
shuailong <80105174+shuailong616@users.noreply.github.com>
2025-01-16 18:33:57 +0800

    Update log_sigmoid.py

--- src/flag_gems/ops/log_sigmoid.py
+++ src/flag_gems/ops/log_sigmoid.py
@@ -9,7 +9,7 @@ from ..utils import pointwise_dynamic
 @pointwise_dynamic(is_tensor=[True], promotion_methods=[(0, "DEFAULT")])
 @triton.jit
 def log_sigmoid_forward(x):
-    return tl.minimum(x, 0.0) - tl.log(1.0 + tl.exp(-tl.abs(x)))    
+    return tl.minimum(x, 0.0) - tl.log(1.0 + tl.exp(-tl.abs(x)))
 
 
 def log_sigmoid(x):
