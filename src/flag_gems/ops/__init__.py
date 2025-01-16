0bbf7ea545c768bad7f050afa06bbe15e52d2167
strongspoon <strongspoon@outlook.com>
2025-01-16 16:44:31 +0800

    [Operator] implement threshold fwd and bwd, as bwd of relu at the same time

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -120,6 +120,7 @@ from .stack import stack
 from .sub import sub
 from .sum import sum, sum_dim
 from .tanh import tanh, tanh_backward
+from .threshold import threshold, threshold_backward
 from .tile import tile
 from .topk import topk
 from .triu import triu
@@ -250,6 +251,8 @@ __all__ = [
     "sub",
     "tanh",
     "tanh_backward",
+    "threshold",
+    "threshold_backward",
     "tile",
     "triu",
     "topk",
