a2fb09b2f47857cc384b6c48ba5942bd015f3f88
strongspoon <strongspoon@outlook.com>
2025-01-16 16:56:03 +0800

    [Operator] register silu backward independently

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -142,7 +142,7 @@ from .rsqrt import rsqrt, rsqrt_
 from .scatter import scatter
 from .select_scatter import select_scatter
 from .sigmoid import sigmoid, sigmoid_, sigmoid_backward
-from .silu import silu, silu_
+from .silu import silu, silu_, silu_backward
 from .sin import sin, sin_
 from .slice_scatter import slice_scatter
 from .softmax import softmax
@@ -310,6 +310,7 @@ __all__ = [
     "sigmoid_backward",
     "silu",
     "silu_",
+    "silu_backward",
     "sin",
     "sin_",
     "softmax",
