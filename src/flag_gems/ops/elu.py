3dae648b94ae1a343ea55048b56a27a75f382343
shuailong616 <452509829@qq.com>
2025-02-11 14:57:31 +0800

    code new fix

--- src/flag_gems/ops/elu.py
+++ src/flag_gems/ops/elu.py
@@ -9,13 +9,9 @@ from ..utils import pointwise_dynamic
 @pointwise_dynamic(is_tensor=[True, False], promotion_methods=[(0, "DEFAULT")])
 @triton.jit
 def elu_forward_kernel(x, alpha):
-    alpha_tensor = tl.broadcast_to(alpha, x.shape)
-    return tl.where(x > 0, x, alpha_tensor * (tl.exp(x.to(tl.float32)) - 1))
+    return tl.where(x > 0, x, alpha * (tl.exp(x.to(tl.float32)) - 1))
 
 
-def elu(A, alpha):
+def elu(A, alpha=1.0):
     logging.debug("GEMS ELU")
-    if alpha is None:
-        return elu_forward_kernel(A, 1.0)
-    else:
-        return elu_forward_kernel(A, alpha)
+    return elu_forward_kernel(A, alpha)
