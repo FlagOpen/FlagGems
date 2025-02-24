d51175e1c1bfb36a6c9e648e202816a0496a8229
shuailong616 <452509829@qq.com>
2025-02-24 15:33:08 +0800

    new code fixed

--- src/flag_gems/ops/elu.py
+++ src/flag_gems/ops/elu.py
@@ -6,16 +6,18 @@ import triton.language as tl
 from ..utils import pointwise_dynamic
 
 
-@pointwise_dynamic(is_tensor=[True, False], promotion_methods=[(0, "DEFAULT")])
+@pointwise_dynamic(
+    is_tensor=[True, False, False, False], promotion_methods=[(0, "DEFAULT")]
+)
 @triton.jit
 def elu_forward_kernel(x, alpha, scale, input_scale):
     return tl.where(
-        x > 0, 
-        scale * input_scale * x, 
+        x > 0,
+        scale * input_scale * x,
         scale * alpha * (tl.exp(x.to(tl.float32)) * input_scale - 1),
     )
 
 
-def elu(A, alpha=1.0, scale=1, input_scale=1):
+def elu(A, alpha=1.0, scale=1.0, input_scale=1.0):
     logging.debug("GEMS ELU")
-    return elu_forward_kernel(A, alpha)
+    return elu_forward_kernel(A, alpha, scale, input_scale)
