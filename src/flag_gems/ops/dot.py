e329cd13d3ebe7323e9786f6b0f836baa248670e
wlxjhyf <xjh1178@gmail.com>
2025-02-24 12:50:54 +0800

    dot kernel

--- src/flag_gems/ops/dot.py
+++ src/flag_gems/ops/dot.py
@@ -11,6 +11,27 @@ from ..utils import libentry
 from ..utils import triton_lang_extension as tle
 
 
+@libentry()
+@triton.jit
+def dot_kernel(
+    x_ptr,
+    y_ptr,
+    out_ptr,
+    N,
+    BLOCK_SIZE: tl.constexpr
+):
+    pid = tle.program_id(0)
+    block_start = pid * BLOCK_SIZE
+
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    
+    mask = offsets < N
+    x = tl.load(x_ptr + offsets, mask=mask, other=0.0).to(tl.float32)
+    y = tl.load(y_ptr + offsets, mask=mask, other=0.0).to(tl.float32)
+
+    partial_sum = tl.sum(x * y)
+    
+    tl.atomic_add(out_ptr, partial_sum)
 
 
 @libentry()
@@ -57,9 +78,10 @@ def dot(x, y):
     assert x.shape == y.shape, "Input vectors must have the same shape"
     assert x.dim() == 1, "Input must be 1D tensors"
 
-    N = x.shape[0]
-    
+    N = x.shape[0] 
     block_size = triton.next_power_of_2(math.ceil(math.sqrt(N)))
+    
+    # if N <= 2560000:
     mid_size = triton.cdiv(N, block_size)
     block_mid = triton.next_power_of_2(mid_size)
     
@@ -72,6 +94,19 @@ def dot(x, y):
     with torch_device_fn.device(x.device):
         dot_kernel_1[grid_1](x, y, mid, N, block_size)
         dot_kernel_2[grid_2](mid, out, mid_size, block_mid)
+
+    # else:
+    #     grid_size = triton.cdiv(N, block_size)
+    #     grid = (grid_size,1,1)
+
+    #     with torch_device_fn.device(x.device):
+    #         if x.dtype != torch.float32:
+    #             out = torch.zeros([], dtype=torch.float32, device=x.device)
+    #             dot_kernel[grid](x, y, out, N, block_size)
+    #             out = out.to(x.dtype)
+    #         else:
+    #             out = torch.zeros([], dtype=x.dtype, device=x.device)
+    #             dot_kernel[grid](x, y, out, N, block_size)
 
     return out
 
