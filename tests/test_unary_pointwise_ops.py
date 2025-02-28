cd0636ab79ab6b6a076d6eaa403b9f57aecf594d
DuanYaQi <30496905+DuanYaQi@users.noreply.github.com>
2025-02-28 17:29:03 +0800

    Pytest Mark Repeat Miss Fixed (#471)

--- tests/test_unary_pointwise_ops.py
+++ tests/test_unary_pointwise_ops.py
@@ -474,7 +474,7 @@ def test_accuracy_tile(shape, dims, dtype):
 REPEAT_SIZES = [(2, 3, 4, 5), (5, 0, 4)]
 
 
-# @pytest.mark.repeat # confilt with pytest_repeat
+@pytest.mark.repeat
 @pytest.mark.parametrize("shape", POINTWISE_SHAPES)
 @pytest.mark.parametrize("sizes", REPEAT_SIZES)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
