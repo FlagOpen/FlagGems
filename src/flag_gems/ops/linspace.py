df506ee709bff73f2ed730183b4ac9cd83aa3c82
0x45f <wangzhen@baai.ac.cn>
2025-03-10 10:18:09 +0800

    Fix steps == 1

--- src/flag_gems/ops/linspace.py
+++ src/flag_gems/ops/linspace.py
@@ -12,6 +12,7 @@ from ..utils import triton_lang_extension as tle
 @triton.jit
 def linspace_kernel(
     out_ptr,
+    out_stride0,
     start,
     mid,
     end,
@@ -27,7 +28,7 @@ def linspace_kernel(
     bd_values = end - step_size * (steps - idx - 1)
 
     out_val = tl.where(fw_mask, fw_values, bd_values)
-    tl.store(out_ptr + idx, out_val, mask=mask)
+    tl.store(out_ptr + idx * out_stride0, out_val, mask=mask)
 
 
 def linspace(
@@ -45,12 +46,6 @@ def linspace(
     logging.debug("GEMS LINSPACE")
     assert steps >= 1, "steps must be >= 1"
 
-    mid = steps // 2
-    if steps == 1:
-        step_size = 0.0
-    else:
-        step_size = (float(end) - float(start)) / (steps - 1)
-
     if out is None:
         out = torch.empty(
             steps,
@@ -60,7 +55,14 @@ def linspace(
             pin_memory=pin_memory,
             requires_grad=requires_grad,
         )
-    BLOCK_SIZE = 128
-    grid = (triton.cdiv(steps, BLOCK_SIZE),)
-    linspace_kernel[grid](out, start, mid, end, step_size, steps, BLOCK_SIZE=BLOCK_SIZE)
-    return out
+    if steps == 1:
+        return torch.fill(out, start)
+    else:
+        mid = steps // 2
+        step_size = (float(end) - float(start)) / (steps - 1)
+        BLOCK_SIZE = 128
+        grid = (triton.cdiv(steps, BLOCK_SIZE),)
+        linspace_kernel[grid](
+            out, out.stride(0), start, mid, end, step_size, steps, BLOCK_SIZE=BLOCK_SIZE
+        )
+        return out
