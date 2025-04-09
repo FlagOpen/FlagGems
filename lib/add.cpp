64aa1d44b7f7fd3bed28de59cbd5644a95862080
Clement Chan <iclementine@outlook.com>
2025-04-10 00:39:36 +0800

    add device guard for operators with cpp runtime, refactor build system to use scikit-build-core

--- lib/add.cpp
+++ lib/add.cpp
@@ -9,6 +9,7 @@ namespace flag_gems {
 using namespace triton_jit;
 
 at::Tensor add_tensor(const at::Tensor &a_, const at::Tensor &b_) {
+
   auto res = torch::broadcast_tensors({a_, b_});
   res[0] = res[0].contiguous();
   res[1] = res[1].contiguous();
@@ -30,7 +31,9 @@ at::Tensor add_tensor(const at::Tensor &a_, const at::Tensor &b_) {
   int64_t n = out.numel();
   const unsigned int num_blocks = (n + tile_size - 1) / tile_size;
 
+  // getCurrentCUDAStream ensures that the stream is initialized, a default stream for each device
   c10::cuda::CUDAStream stream = c10::cuda::getCurrentCUDAStream();
+  c10::DeviceGuard guard(out.device());
   CUstream raw_stream = static_cast<CUstream>(stream.stream());
   f(stream, num_blocks, 1, 1, num_warps, num_stages, a, b, out, n, tile_size);
   return out;
--- src/flag_gems/csrc/cstub.cpp
+++ src/flag_gems/csrc/cstub.cpp
@@ -7,12 +7,12 @@ PYBIND11_MODULE(c_operators, m) {
 }
 
 namespace flag_gems {
-TORCH_LIBRARY(flaggems, m) {
+TORCH_LIBRARY(flag_gems, m) {
   m.def("sum.dim_IntList(Tensor self, int[1]? dim, bool keepdim=False, *, ScalarType? dtype=None) -> Tensor");
   m.def("add_tensor(Tensor self, Tensor other) -> Tensor");
 }
 
-TORCH_LIBRARY_IMPL(flaggems, CUDA, m) {
+TORCH_LIBRARY_IMPL(flag_gems, CUDA, m) {
   m.impl("sum.dim_IntList", TORCH_FN(sum_dim));
   m.impl("add_tensor", TORCH_FN(add_tensor));
 }
