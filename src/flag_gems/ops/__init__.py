42d6e80bd1f022fa76722a9ae98c37ee01fc6514
henghengxiedaima <1149963331@qq.com>
2025-04-15 12:21:42 +0800

    add_index_put_

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -67,8 +67,7 @@ from .groupnorm import group_norm
 from .gt import gt, gt_scalar
 from .hstack import hstack
 from .index_add import index_add
-from .index_put import index_put
-from .index_put_ import index_put_
+from .index_put import index_put, index_put_
 from .index_select import index_select
 from .instancenorm import instance_norm
 from .isclose import allclose, isclose
