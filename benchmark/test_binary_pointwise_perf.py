fca4da5c7cd77c57d7541df84e4400e35be414aa
zwhangang123 <1527764861@qq.com>
2025-04-16 15:11:00 +0800

    de flag_gems.ops.polar

--- benchmark/test_binary_pointwise_perf.py
+++ benchmark/test_binary_pointwise_perf.py
@@ -7,7 +7,6 @@ import flag_gems
 
 from .attri_util import BOOL_DTYPES, DEFAULT_METRICS, FLOAT_DTYPES, INT_DTYPES
 from .performance_utils import Benchmark, generate_tensor_input
-from flag_gems.ops.polar import polar
 
 
 class BinaryPointwiseBenchmark(Benchmark):
@@ -45,41 +44,41 @@ class BinaryPointwiseBenchmark(Benchmark):
         )
         for name, op, dtype in [
             # Arithmetic operations
-            ("add", torch.add, FLOAT_DTYPES),
-            ("div", torch.div, FLOAT_DTYPES),
-            ("mul", torch.mul, FLOAT_DTYPES),
-            ("pow", torch.pow, FLOAT_DTYPES),
-            ("sub", torch.sub, FLOAT_DTYPES),
+            #("add", torch.add, FLOAT_DTYPES),
+            #("div", torch.div, FLOAT_DTYPES),
+            #("mul", torch.mul, FLOAT_DTYPES),
+            #("pow", torch.pow, FLOAT_DTYPES),
+            #("sub", torch.sub, FLOAT_DTYPES),
             ("polar", torch.polar, [torch.float32]),
-            *(
-                [
-                    ("floor_divide", torch.floor_divide, INT_DTYPES),
-                    ("remainder", torch.remainder, INT_DTYPES),
-                ]
-                if flag_gems.device != "musa"
-                else []
-            ),
-            ("rsub", torch.rsub, FLOAT_DTYPES),
-            ("logical_or", torch.logical_or, INT_DTYPES + BOOL_DTYPES),
-            ("logical_and", torch.logical_and, INT_DTYPES + BOOL_DTYPES),
-            ("logical_xor", torch.logical_xor, INT_DTYPES + BOOL_DTYPES),
+            #*(
+            #    [
+            #        ("floor_divide", torch.floor_divide, INT_DTYPES),
+            #        ("remainder", torch.remainder, INT_DTYPES),
+            #    ]
+            #    if flag_gems.device != "musa"
+            #    else []
+            #),
+            #("rsub", torch.rsub, FLOAT_DTYPES),
+            #("logical_or", torch.logical_or, INT_DTYPES + BOOL_DTYPES),
+            #("logical_and", torch.logical_and, INT_DTYPES + BOOL_DTYPES),
+            #("logical_xor", torch.logical_xor, INT_DTYPES + BOOL_DTYPES),
             # Comparison operations
-            ("eq", torch.eq, FLOAT_DTYPES),
-            ("ge", torch.ge, FLOAT_DTYPES),
-            ("gt", torch.gt, FLOAT_DTYPES),
-            ("le", torch.le, FLOAT_DTYPES),
-            ("lt", torch.lt, FLOAT_DTYPES),
-            ("ne", torch.ne, FLOAT_DTYPES),
+            #("eq", torch.eq, FLOAT_DTYPES),
+            #("ge", torch.ge, FLOAT_DTYPES),
+            #("gt", torch.gt, FLOAT_DTYPES),
+            #("le", torch.le, FLOAT_DTYPES),
+            #("lt", torch.lt, FLOAT_DTYPES),
+            #("ne", torch.ne, FLOAT_DTYPES),
             # Minimum and maximum operations
-            ("maximum", torch.maximum, FLOAT_DTYPES),
-            ("minimum", torch.minimum, FLOAT_DTYPES),
+            #("maximum", torch.maximum, FLOAT_DTYPES),
+            #("minimum", torch.minimum, FLOAT_DTYPES),
             # Bitwise operations
-            ("bitwise_and", torch.bitwise_and, INT_DTYPES + BOOL_DTYPES),
-            ("bitwise_or", torch.bitwise_or, INT_DTYPES + BOOL_DTYPES),
-            ("or_", torch.bitwise_or, INT_DTYPES + BOOL_DTYPES),
+            #("bitwise_and", torch.bitwise_and, INT_DTYPES + BOOL_DTYPES),
+            #("bitwise_or", torch.bitwise_or, INT_DTYPES + BOOL_DTYPES),
+            #("or_", torch.bitwise_or, INT_DTYPES + BOOL_DTYPES),
             # Numerical Checks
-            ("isclose", torch.isclose, FLOAT_DTYPES + INT_DTYPES),
-            ("allclose", torch.allclose, FLOAT_DTYPES + INT_DTYPES),
+            #("isclose", torch.isclose, FLOAT_DTYPES + INT_DTYPES),
+            #("allclose", torch.allclose, FLOAT_DTYPES + INT_DTYPES),
         ]
     ],
 )
