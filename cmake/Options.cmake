80b0389e09c46750eafa4b779b74322a05f021ac
Clement Chan <iclementine@outlook.com>
2025-04-17 10:05:23 +0800

    prefer installed pybind11

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,2 +1,2 @@
 option(FLAGGEMS_USE_EXTERNAL_TRITON_JIT "whether to use external triton jit library" OFF)
-option(FLAGGEMS_USE_EXTERNAL_PYBIND11 "whether to use external pybind11 library" OFF)
+option(FLAGGEMS_USE_EXTERNAL_PYBIND11 "whether to use external pybind11 library" ON)
