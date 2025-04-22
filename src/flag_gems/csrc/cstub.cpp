112e3cd12d8004c02c1e98665c9b1e2b9bf926f9
kiddyjinjin <kiddyjinjin@gmail.com>
2025-04-22 09:06:03 +0000

    [C++ wrapper] rms_norm operator

--- src/flag_gems/csrc/cstub.cpp
+++ src/flag_gems/csrc/cstub.cpp
@@ -10,10 +10,17 @@ namespace flag_gems {
 TORCH_LIBRARY(flag_gems, m) {
   m.def("sum.dim_IntList(Tensor self, int[1]? dim, bool keepdim=False, *, ScalarType? dtype=None) -> Tensor");
   m.def("add_tensor(Tensor self, Tensor other) -> Tensor", {at::Tag::pt2_compliant_tag});
+  // Norm
+  m.def("rms_norm(Tensor input, Tensor weight, float epsilon) -> Tensor");
+  //m.def("fused_add_rms_norm(Tensor input, Tensor residual, Tensor weight, float epsilon) -> Tensor")  //TODO: maybe inplace is better
+
 }
 
 TORCH_LIBRARY_IMPL(flag_gems, CUDA, m) {
   m.impl("sum.dim_IntList", TORCH_FN(sum_dim));
   m.impl("add_tensor", TORCH_FN(add_tensor));
+  // Norm
+  m.impl("rms_norm", TORCH_FN(rms_norm));
+  //m.impl("fused_add_rms_norm", TORCH_FN(fused_add_rms_norm));
 }
 }  // namespace flag_gems
