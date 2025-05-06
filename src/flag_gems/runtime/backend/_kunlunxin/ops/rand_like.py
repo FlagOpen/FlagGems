571d20d23a8cb07fb5732556d3346acbe304faae
wyc202008 <1446773657@qq.com>
2025-05-07 02:26:27 +0800

    fix-speedup:rand

--- src/flag_gems/runtime/backend/_kunlunxin/ops/rand_like.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/rand_like.py
@@ -4,10 +4,11 @@ import torch
 import triton
 
 from flag_gems.ops.rand import rand_kernel
+from .rand import rand_kernel_1, rand_kernel_2, choose_unroll
 from flag_gems.runtime import torch_device_fn
 from flag_gems.utils.random_utils import philox_backend_seed_offset
 
-UNROLL = 4
+# UNROLL = 4
 
 
 def rand_like(
@@ -20,11 +21,18 @@ def rand_like(
         dtype = x.dtype
     out = torch.empty_like(x, device=device, dtype=dtype)
     N = x.numel()
-    grid_fn = lambda meta: (triton.cdiv(N, meta["BLOCK"] * UNROLL),)
+    # grid_fn = lambda meta: (triton.cdiv(N, meta["BLOCK"] * UNROLL),)
+    cluster_num = 12
+    UNROLL = choose_unroll(N)
+    BLOCK_SIZE = min(triton.next_power_of_2(triton.cdiv(N, cluster_num * UNROLL)), 1024)
+    grid_fn = triton.cdiv(N, BLOCK_SIZE * UNROLL)
     # (TODO) Using Triton autotuner makes kernel parameters opaque to the caller,
     # hence we cannot obtain the per thread offset as in Pytorch.
     increment = triton.cdiv(N, UNROLL)
     philox_seed, philox_offset = philox_backend_seed_offset(increment)
     with torch_device_fn.device(x.device):
-        rand_kernel[grid_fn](out, N, philox_seed, philox_offset)
+        if UNROLL <= 4:
+            rand_kernel_1[(grid_fn,)](out, N, philox_seed, philox_offset, BLOCK_SIZE, UNROLL)
+        else:
+            rand_kernel_2[(grid_fn,)](out, N, philox_seed, philox_offset, BLOCK_SIZE, UNROLL)
     return out
