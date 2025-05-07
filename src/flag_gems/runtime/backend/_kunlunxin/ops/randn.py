dc909ee8974cfc185a42d72570e0e9855a0958d5
wyc202008 <1446773657@qq.com>
2025-05-07 10:50:27 +0800

    fix-randn: change unroll to 8

--- src/flag_gems/runtime/backend/_kunlunxin/ops/randn.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/randn.py
@@ -60,7 +60,7 @@ def randn_kernel(
     tl.store(out_ptr + off_3, n3, mask=off_3 < N, eviction_policy="evict_first")
 
 def choose_unroll(N, core=64, clusters=12):
-    for u in (16, 4):
+    for u in (8, 4):
         if triton.cdiv(N, clusters * u) >= core:
             return u
     return 4
@@ -120,9 +120,9 @@ def randn_kernel_2(
     c0 += i4
     _O = c0 * 0
     r0, r1, r2, r3 = tl.philox(philox_seed, c0, c1, _O, _O)
-    r4, r5, r6, r7 = tl.philox(philox_seed, c0 + 1, c1, _O, _O)
-    r8, r9, r10, r11 = tl.philox(philox_seed, c0 + 2, c1, _O, _O)
-    r12, r13, r14, r15 = tl.philox(philox_seed, c0 + 3, c1, _O, _O)
+    r4, r5, r6, r7 = tl.philox(philox_seed, c0, c1, _O, _O)
+    # r8, r9, r10, r11 = tl.philox(philox_seed, c0, c1, _O, _O)
+    # r12, r13, r14, r15 = tl.philox(philox_seed, c0, c1, _O, _O)
     # r4, r5, r6, r7 = tl.philox(philox_seed, c0 + 100 * BLOCK, c1, _O, _O)
 
     r0 = uint_to_uniform_float(r0)
@@ -133,23 +133,23 @@ def randn_kernel_2(
     r5 = uint_to_uniform_float(r5)
     r6 = uint_to_uniform_float(r6)
     r7 = uint_to_uniform_float(r7)
-    r8 = uint_to_uniform_float(r8)
-    r9 = uint_to_uniform_float(r9)
-    r10 = uint_to_uniform_float(r10)
-    r11 = uint_to_uniform_float(r11)
-    r12 = uint_to_uniform_float(r12)
-    r13 = uint_to_uniform_float(r13)
-    r14 = uint_to_uniform_float(r14)
-    r15 = uint_to_uniform_float(r15)
+    # r8 = uint_to_uniform_float(r8)
+    # r9 = uint_to_uniform_float(r9)
+    # r10 = uint_to_uniform_float(r10)
+    # r11 = uint_to_uniform_float(r11)
+    # r12 = uint_to_uniform_float(r12)
+    # r13 = uint_to_uniform_float(r13)
+    # r14 = uint_to_uniform_float(r14)
+    # r15 = uint_to_uniform_float(r15)
     n0, n1 = pair_uniform_to_normal(r0, r1)
     n2, n3 = pair_uniform_to_normal(r2, r3)
     n4, n5 = pair_uniform_to_normal(r4, r5)
     n6, n7 = pair_uniform_to_normal(r6, r7)
-    n8, n9 = pair_uniform_to_normal(r8, r9)
-    n10, n11 = pair_uniform_to_normal(r10, r11)
-    n12, n13 = pair_uniform_to_normal(r12, r13)
-    n14, n15 = pair_uniform_to_normal(r14, r15)
-    off_0 = tl.program_id(0) * BLOCK * 16 + tl.arange(0, BLOCK)
+    # n8, n9 = pair_uniform_to_normal(r8, r9)
+    # n10, n11 = pair_uniform_to_normal(r10, r11)
+    # n12, n13 = pair_uniform_to_normal(r12, r13)
+    # n14, n15 = pair_uniform_to_normal(r14, r15)
+    off_0 = tl.program_id(0) * BLOCK * 8 + tl.arange(0, BLOCK)
     off_1 = off_0 + BLOCK
     off_2 = off_1 + BLOCK
     off_3 = off_2 + BLOCK
@@ -157,14 +157,14 @@ def randn_kernel_2(
     off_5 = off_4 + BLOCK
     off_6 = off_5 + BLOCK
     off_7 = off_6 + BLOCK
-    off_8 = off_7 + BLOCK
-    off_9 = off_8 + BLOCK
-    off_10 = off_9 + BLOCK
-    off_11 = off_10 + BLOCK
-    off_12 = off_11 + BLOCK
-    off_13 = off_12 + BLOCK
-    off_14 = off_13 + BLOCK
-    off_15 = off_14 + BLOCK
+    # off_8 = off_7 + BLOCK
+    # off_9 = off_8 + BLOCK
+    # off_10 = off_9 + BLOCK
+    # off_11 = off_10 + BLOCK
+    # off_12 = off_11 + BLOCK
+    # off_13 = off_12 + BLOCK
+    # off_14 = off_13 + BLOCK
+    # off_15 = off_14 + BLOCK
     tl.store(out_ptr + off_0, n0, mask=off_0 < N, eviction_policy="evict_first")
     tl.store(out_ptr + off_1, n1, mask=off_1 < N, eviction_policy="evict_first")
     tl.store(out_ptr + off_2, n2, mask=off_2 < N, eviction_policy="evict_first")
@@ -173,14 +173,14 @@ def randn_kernel_2(
     tl.store(out_ptr + off_5, n5, mask=off_5 < N, eviction_policy="evict_first")
     tl.store(out_ptr + off_6, n6, mask=off_6 < N, eviction_policy="evict_first")
     tl.store(out_ptr + off_7, n7, mask=off_7 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_8, n8, mask=off_8 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_9, n9, mask=off_9 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_10, n10, mask=off_10 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_11, n11, mask=off_11 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_12, n12, mask=off_12 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_13, n13, mask=off_13 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_14, n14, mask=off_14 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_15, n15, mask=off_15 < N, eviction_policy="evict_first")
+    # tl.store(out_ptr + off_8, n8, mask=off_8 < N, eviction_policy="evict_first")
+    # tl.store(out_ptr + off_9, n9, mask=off_9 < N, eviction_policy="evict_first")
+    # tl.store(out_ptr + off_10, n10, mask=off_10 < N, eviction_policy="evict_first")
+    # tl.store(out_ptr + off_11, n11, mask=off_11 < N, eviction_policy="evict_first")
+    # tl.store(out_ptr + off_12, n12, mask=off_12 < N, eviction_policy="evict_first")
+    # tl.store(out_ptr + off_13, n13, mask=off_13 < N, eviction_policy="evict_first")
+    # tl.store(out_ptr + off_14, n14, mask=off_14 < N, eviction_policy="evict_first")
+    # tl.store(out_ptr + off_15, n15, mask=off_15 < N, eviction_policy="evict_first")
 
 
 def randn(size, *, dtype=None, layout=None, device=None, pin_memory=None):
@@ -201,7 +201,7 @@ def randn(size, *, dtype=None, layout=None, device=None, pin_memory=None):
     increment = triton.cdiv(N, UNROLL)
     philox_seed, philox_offset = philox_backend_seed_offset(increment)
     with torch_device_fn.device(device):
-        if UNROLL == 4:
+        if UNROLL <= 4:
             randn_kernel_1[(grid_fn,)](out, N, philox_seed, philox_offset, BLOCK_SIZE, UNROLL)
         else:
             randn_kernel_2[(grid_fn,)](out, N, philox_seed, philox_offset, BLOCK_SIZE, UNROLL)
