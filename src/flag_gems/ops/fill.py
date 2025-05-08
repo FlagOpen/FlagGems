ae897e4cc5c374181a3dc4a7dd940af2cc16f5b8
nmpress1 <1935298275@qq.com>
2025-05-08 17:17:34 +0800

    “xchange fill&fill_ use out

--- src/flag_gems/ops/fill.py
+++ src/flag_gems/ops/fill.py
@@ -1,5 +1,6 @@
 import logging
 
+import torch
 import triton
 
 from ..runtime import torch_device_fn
@@ -10,7 +11,7 @@ from ..utils import pointwise_dynamic
     is_tensor=[True, False], promotion_methods=[(0, "DEFAULT")], num_outputs=1
 )
 @triton.jit
-def fill_scalar_kernel(out, value_scalar):
+def fill_scalar_func(inp, value_scalar):
     return value_scalar
 
 
@@ -18,14 +19,15 @@ def fill_scalar_kernel(out, value_scalar):
     is_tensor=[True, True], promotion_methods=[(0, "DEFAULT")], num_outputs=1
 )
 @triton.jit
-def fill_tensor_kernel(out, value):
+def fill_tensor_func(inp, value):
     return value
 
 
 def fill_scalar(input, value):
     logging.debug("GEMS FILL (Dynamic)")
+    out = torch.empty_like(input)
     with torch_device_fn.device(input.device):
-        return fill_scalar_kernel(input, value)
+        return fill_scalar_func(input, value, out0=out)
 
 
 def fill_tensor(input, value):
@@ -34,8 +36,9 @@ def fill_tensor(input, value):
         raise RuntimeError(
             f"fill_ only supports 0-dimension value tensor but got tensor with {value.ndim} dimensions."
         )
+    out = torch.empty_like(input)
     with torch_device_fn.device(input.device):
-        return fill_tensor_kernel(input, value)
+        return fill_tensor_func(input, value, out0=out)
 
 
 def fill_tensor_(self, value):
@@ -45,12 +48,12 @@ def fill_tensor_(self, value):
             f"fill_ only supports 0-dimension value tensor but got tensor with {value.ndim} dimensions."
         )
     with torch_device_fn.device(self.device):
-        fill_tensor_kernel(self, value, out0=self)
+        fill_tensor_func(self, value, out0=self)
     return self
 
 
 def fill_scalar_(self, value):
     logging.debug("GEMS FILL_SCALAR_")
     with torch_device_fn.device(self.device):
-        fill_scalar_kernel(self, value, out0=self)
+        fill_scalar_func(self, value, out0=self)
     return self
