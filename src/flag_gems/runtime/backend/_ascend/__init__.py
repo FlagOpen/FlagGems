d1dab80b25ad0faeaeea6491ce00e374b42aade9
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-08 15:23:51 +0800

    Update __init__.py

--- src/flag_gems/runtime/backend/_ascend/__init__.py
+++ src/flag_gems/runtime/backend/_ascend/__init__.py
@@ -1,7 +1,10 @@
 from backend_utils import VendorInfoBase  # noqa: E402
 
 vendor_info = VendorInfoBase(
-    vendor_name="ascend", device_name="npu", device_query_cmd="npu-smi info"
+    vendor_name="ascend",
+    device_name="npu",
+    device_query_cmd="npu-smi info"，
+    dispatch_key="PrivateUse1",
 )
 
 CUSTOMIZED_UNUSED_OPS = ("cumsum")
