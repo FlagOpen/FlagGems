b155ae4d2a08e44c3e24e39f0723e71a150bc8a3
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-08 15:45:33 +0800

    Update test_binary_pointwise_perf.py

--- benchmark/test_binary_pointwise_perf.py
+++ benchmark/test_binary_pointwise_perf.py
@@ -47,21 +47,30 @@ class BinaryPointwiseBenchmark(Benchmark):
             ("add", torch.add, FLOAT_DTYPES),
             ("div", torch.div, FLOAT_DTYPES),
             ("mul", torch.mul, FLOAT_DTYPES),
-            ("pow", torch.pow, FLOAT_DTYPES),
             ("sub", torch.sub, FLOAT_DTYPES),
-            ("polar", torch.polar, [torch.float32]),
             *(
                 [
                     ("floor_divide", torch.floor_divide, INT_DTYPES),
                     ("remainder", torch.remainder, INT_DTYPES),
                 ]
-                if flag_gems.device != "musa"
+                if flag_gems.device not in ["musa", "npu"]
                 else []
             ),
-            ("rsub", torch.rsub, FLOAT_DTYPES),
-            ("logical_or", torch.logical_or, INT_DTYPES + BOOL_DTYPES),
-            ("logical_and", torch.logical_and, INT_DTYPES + BOOL_DTYPES),
-            ("logical_xor", torch.logical_xor, INT_DTYPES + BOOL_DTYPES),
+            *(
+                [
+                    ("pow", torch.pow, FLOAT_DTYPES),
+                    ("polar", torch.polar, [torch.float32]),
+                    ("rsub", torch.rsub, FLOAT_DTYPES),
+                    ("logical_or", torch.logical_or, INT_DTYPES + BOOL_DTYPES),
+                    ("logical_and", torch.logical_and, INT_DTYPES + BOOL_DTYPES),
+                    ("logical_xor", torch.logical_xor, INT_DTYPES + BOOL_DTYPES),
+                    # Numerical Checks
+                    ("isclose", torch.isclose, FLOAT_DTYPES + INT_DTYPES),
+                    ("allclose", torch.allclose, FLOAT_DTYPES + INT_DTYPES),
+                ]
+                if flag_gems.device != "npu"
+                else []
+            ),
             # Comparison operations
             ("eq", torch.eq, FLOAT_DTYPES),
             ("ge", torch.ge, FLOAT_DTYPES),
@@ -76,9 +85,6 @@ class BinaryPointwiseBenchmark(Benchmark):
             ("bitwise_and", torch.bitwise_and, INT_DTYPES + BOOL_DTYPES),
             ("bitwise_or", torch.bitwise_or, INT_DTYPES + BOOL_DTYPES),
             ("or_", torch.bitwise_or, INT_DTYPES + BOOL_DTYPES),
-            # Numerical Checks
-            ("isclose", torch.isclose, FLOAT_DTYPES + INT_DTYPES),
-            ("allclose", torch.allclose, FLOAT_DTYPES + INT_DTYPES),
         ]
     ],
 )
