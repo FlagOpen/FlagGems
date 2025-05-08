44859c93c99a773e93c2b5ab181b21b82afee647
nmpress1 <1935298275@qq.com>
2025-05-08 16:40:30 +0800

    change fill&fill_

--- src/flag_gems/ops/fill.py
+++ src/flag_gems/ops/fill.py
@@ -1,50 +1,41 @@
 import logging
 
-import torch
 import triton
 
 from ..runtime import torch_device_fn
 from ..utils import pointwise_dynamic
 
 
-@pointwise_dynamic(is_tensor=[True, False], promotion_methods=[(0, "DEFAULT")])
+@pointwise_dynamic(
+    is_tensor=[True, False], promotion_methods=[(0, "DEFAULT")], num_outputs=1
+)
 @triton.jit
-def fill_scalar_kernel(
-    out,
-    value_scalar,
-):
-    out = value_scalar
-    return out
+def fill_scalar_kernel(out, value_scalar):
+    return value_scalar
 
 
-@pointwise_dynamic(is_tensor=[True, True], promotion_methods=[(0, "DEFAULT")])
+@pointwise_dynamic(
+    is_tensor=[True, True], promotion_methods=[(0, "DEFAULT")], num_outputs=1
+)
 @triton.jit
-def fill_tensor_kernel(
-    out,
-    value,
-):
-    out = value
-    return out
+def fill_tensor_kernel(out, value):
+    return value
+
+
+def fill_scalar(input, value):
+    logging.debug("GEMS FILL (Dynamic)")
+    with torch_device_fn.device(input.device):
+        return fill_scalar_kernel(input, value)
 
 
 def fill_tensor(input, value):
-    logging.debug("GEMS FILL")
+    logging.debug("GEMS FILL (Dynamic)")
     if value.ndim != 0:
         raise RuntimeError(
             f"fill_ only supports 0-dimension value tensor but got tensor with {value.ndim} dimensions."
         )
-    out = torch.empty_like(input)
     with torch_device_fn.device(input.device):
-        result = fill_tensor_kernel(out, value)
-    return result
-
-
-def fill_scalar(input, value):
-    logging.debug("GEMS FILL")
-    out = torch.empty_like(input)
-    with torch_device_fn.device(input.device):
-        result = fill_scalar_kernel(out, value)
-    return result
+        return fill_tensor_kernel(input, value)
 
 
 def fill_tensor_(self, value):
