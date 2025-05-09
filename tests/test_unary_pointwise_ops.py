17d4cdf1605347e3f37d1641c8a730a4e01349b4
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-09 17:28:51 +0800

    Update test_unary_pointwise_ops.py

--- tests/test_unary_pointwise_ops.py
+++ tests/test_unary_pointwise_ops.py
@@ -86,6 +86,7 @@ def test_accuracy_angle(shape, dtype):
     gems_assert_close(res_out, ref_out, dtype_out)
 
 
+@pytest.mark.skipif(flag_gems.vendor_name == "ascend", reason="TODO")
 @pytest.mark.bitwise_not
 @pytest.mark.parametrize("shape", POINTWISE_SHAPES)
 @pytest.mark.parametrize("dtype", INT_DTYPES + BOOL_TYPES)
