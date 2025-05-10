a1d321fc78732aa8f2c5e869c7e0ef586de5d49c
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-10 10:20:18 +0800

    Update test_unary_pointwise_ops.py

--- tests/test_unary_pointwise_ops.py
+++ tests/test_unary_pointwise_ops.py
@@ -544,6 +544,7 @@ def test_accuracy_sin_(shape, dtype):
     gems_assert_close(res_out, ref_out, dtype)
 
 
+@pytest.mark.skipif(flag_gems.vendor_name == "ascend", reason="TODO")
 @pytest.mark.tanh
 @pytest.mark.parametrize("shape", POINTWISE_SHAPES)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
