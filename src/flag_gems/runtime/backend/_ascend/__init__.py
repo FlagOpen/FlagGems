c3bcbc90ab437cd405f359fcb674bf65a7ef6f5c
Jiang_wj <62932620+Sans1J@users.noreply.github.com>
2025-05-10 09:47:10 +0800

    Update __init__.py

--- src/flag_gems/runtime/backend/_ascend/__init__.py
+++ src/flag_gems/runtime/backend/_ascend/__init__.py
@@ -3,7 +3,7 @@ from backend_utils import VendorInfoBase  # noqa: E402
 vendor_info = VendorInfoBase(
     vendor_name="ascend",
     device_name="npu",
-    device_query_cmd="npu-smi info"，
+    device_query_cmd="npu-smi info",
     dispatch_key="PrivateUse2",
 )
 
