43a35445357265513a9cccb023b5750857cdf6fa
zhzhcookie <zhengyang@baai.ac.cn>
2025-05-12 19:31:59 +0800

    [BACKEND] Init ascend backend 20250512 (#616)
    * [Huawei] Ascend code for FlagGems (#608)
    
    * Add files via upload
    
    * Update __init__.py
    
    * Update device.py
    
    * Update commom_utils.py
    
    * Update __init__.py
    
    * Update gelu_and_mul.py
    
    * Update angle.py
    
    * Update div.py
    
    * Update gelu.py
    
    * Update isinf.py
    
    * Update isnan.py
    
    * Update nan_to_num.py
    
    * Update pow.py
    
    * Update tanh.py
    
    * Update vector_norm.py
    
    * Update performance_utils.py
    
    * Update test_binary_pointwise_perf.py
    
    * Update test_reduction_perf.py
    
    * Update test_unary_pointwise_perf.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_reduction_ops.py
    
    * Update test_unary_pointwise_ops.py
    
    * Create __init__.py
    
    * Update pointwise_dynamic.py
    
    * Update test_blas_ops.py
    
    * Update test_blas_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_reduction_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_unary_pointwise_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_blas_ops.py
    
    * Update test_special_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_unary_pointwise_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_unary_pointwise_ops.py
    
    * Update test_norm_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_reduction_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_blas_ops.py
    
    * Update test_blas_ops.py
    
    * Update test_special_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_reduction_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_reduction_ops.py
    
    * Update test_special_ops.py
    
    * Update test_unary_pointwise_ops.py
    
    * Update pointwise_dynamic.py
    
    * Update __init__.py
    
    * Update test_binary_pointwise_perf.py
    
    * Update test_reduction_perf.py
    
    * Update test_unary_pointwise_perf.py
    
    * Update test_blas_perf.py
    
    * Update test_binary_pointwise_perf.py
    
    * Update test_reduction_perf.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_blas_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_norm_ops.py
    
    * Update test_reduction_ops.py
    
    * Update test_unary_pointwise_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_general_reduction_ops.py
    
    * Update test_unary_pointwise_ops.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update __init__.py
    
    * Update test_binary_pointwise_ops.py
    
    * Update test_unary_pointwise_ops.py
    
    * Update test_blas_perf.py
    
    * Delete src/flag_gems/runtime/backend/_ascend/ops directory
    
    * Update test_binary_pointwise_ops.py
    
    * [BACKEND] Init ascend backend
    
    ---------
    
    Co-authored-by: Jiang_wj <62932620+Sans1J@users.noreply.github.com>

--- src/flag_gems/ops/isinf.py
+++ src/flag_gems/ops/isinf.py
@@ -5,7 +5,10 @@ import triton.language as tl
 
 from ..utils import pointwise_dynamic, tl_extra_shim
 
-_isinf = tl_extra_shim.isinf
+try:
+    import torch_npu  # noqa: F401
+except:  # noqa: E722
+    _isinf = tl_extra_shim.isinf
 
 
 @pointwise_dynamic(promotion_methods=[(0, "ALWAYS_BOOL")])
