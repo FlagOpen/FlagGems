64402b4acae31adb33dff13bc9dcd35dd5cf17b8
Lianghong.Huang <739302661@qq.com>
2025-05-12 15:32:03 +0800

    [METAX] Improve metax ops performance

--- src/flag_gems/runtime/backend/_metax/ops/__init__.py
+++ src/flag_gems/runtime/backend/_metax/ops/__init__.py
@@ -7,15 +7,19 @@ from .groupnorm import group_norm
 from .index_select import index_select
 from .isin import isin
 from .log_softmax import log_softmax
+from .masked_fill import masked_fill, masked_fill_
 from .min import min, min_dim
 from .ones import ones
+from .ones_like import ones_like
 from .outer import outer
 from .prod import prod, prod_dim
 from .repeat_interleave import repeat_interleave_self_tensor
+from .resolve_conj import resolve_conj
 from .sigmoid import sigmoid
 from .tanh import tanh
 from .unique import _unique2
 from .zeros import zeros
+from .zeros_like import zeros_like
 
 __all__ = [
     "arange",
@@ -29,15 +33,20 @@ __all__ = [
     "index_select",
     "isin",
     "log_softmax",
+    "masked_fill",
+    "masked_fill_",
     "min_dim",
     "min",
     "ones",
+    "ones_like",
     "outer",
     "prod",
     "prod_dim",
     "repeat_interleave_self_tensor",
+    "resolve_conj",
     "sigmoid",
     "tanh",
     "_unique2",
     "zeros",
+    "zeros_like",
 ]
---
+++ src/flag_gems/runtime/backend/_metax/ops/zeros_like.py
@@ -0,0 +1,24 @@
+import logging
+
+import torch
+import triton
+
+from flag_gems.runtime import torch_device_fn
+
+from .zeros import zeros_kernel
+
+
+def zeros_like(
+    x, *, dtype=None, layout=None, device=None, pin_memory=None, memory_format=None
+):
+    logging.debug("METAX GEMS ZEROS_LIKE")
+    if device is None:
+        device = x.device
+    if dtype is None:
+        dtype = x.dtype
+    out = torch.empty_like(x, device=device, dtype=dtype)
+    N = x.numel()
+    grid_fn = lambda meta: (triton.cdiv(N, meta["BLOCK_SIZE"]),)
+    with torch_device_fn.device(x.device):
+        zeros_kernel[grid_fn](out, N, BLOCK_SIZE=1024)
+    return out
