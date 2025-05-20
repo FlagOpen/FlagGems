40f9734e2aaadd8fff681bfc2c7f24ea626c9993
kiddyjinjin <kiddyjinjin@gmail.com>
2025-05-20 05:41:25 +0000

    build modules for RMSNorm

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,5 +1,5 @@
-option(BUILD_C_EXTENSIONS "whether to build c extension" OFF)
+option(BUILD_C_EXTENSIONS "whether to build c extension" ON)
 
 option(USE_EXTERNAL_TRITON_JIT "whether to use external triton jit library" OFF)
 option(USE_EXTERNAL_PYBIND11 "whether to use external pybind11 library" ON)
-option(BUILD_CTESTS "Whether to build CPP unit tests" OFF)
+option(BUILD_CTESTS "Whether to build CPP unit tests" ON)
--- modules_tests/test_utils.py
+++ modules_tests/test_utils.py
@@ -1,4 +1,13 @@
 import torch
+import flag_gems
+
+def gems_assert_close(res, ref, dtype, equal_nan=False, reduce_dim=1):
+    flag_gems.testing.assert_close(
+        res, ref, dtype, equal_nan=equal_nan, reduce_dim=reduce_dim
+    )
+
+def gems_assert_equal(res, ref, equal_nan=False):
+    flag_gems.testing.assert_equal(res, ref, equal_nan=equal_nan)
 
 
 def assert_allclose(a, b, rtol=1e-5, atol=1e-8):
