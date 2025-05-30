13a9aed09cfed7dc20002cfb90348df096d0b262
meinie <929449726@qq.com>
2025-05-30 18:06:35 +0800

    add ops glu (#653)
    * add ops glu
    
    * add function test_glu_perf in test_unary_pointwise_perf.py

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -64,6 +64,7 @@ from .full_like import full_like
 from .gather import gather, gather_backward
 from .ge import ge, ge_scalar
 from .gelu import gelu, gelu_, gelu_backward
+from .glu import glu
 from .groupnorm import group_norm, group_norm_backward
 from .gt import gt, gt_scalar
 from .hstack import hstack
@@ -260,6 +261,7 @@ __all__ = [
     "gelu",
     "gelu_",
     "gelu_backward",
+    "glu",
     "group_norm",
     "group_norm_backward",
     "gt",
