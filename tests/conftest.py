d5c399858f8d328dc623160c26a3168c908646d4
pijiu1234 <53425763+pijiu1234@users.noreply.github.com>
2025-05-30 13:29:49 +0800

    modify conftest.py, add --ref

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,139 @@
+import json
+import logging
+import os
+from datetime import datetime
+
+import pytest
+
+import flag_gems
+
+device = flag_gems.device
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = f"test_detail_and_result_{timestamp}.json"
+
+
 def pytest_addoption(parser):
     parser.addoption(
-        "--device",
+        "--ref",
         action="store",
-        default="cuda",
+        default=device,
         required=False,
-        choices=["cuda", "cpu"],
+        choices=[device, "cpu"],
         help="device to run reference tests on",
+    )
+    parser.addoption(
+        "--mode",
+        #if flag_gems.vendor_name != "kunlunxin"
+        #else "--fg_mode",  # TODO: fix pytest-* common --mode args,
+        action="store",
+        default="normal",
+        required=False,
+        choices=["normal", "quick"],
+        help="run tests on normal or quick mode",
+    )
+    parser.addoption(
+        "--record",
+        action="store",
+        default="none",
+        required=False,
+        choices=["none", "log"],
+        help="tests function param recorded in log files or not",
     )
 
 
 def pytest_configure(config):
-    value = config.getoption("--device")
     global TO_CPU
-    TO_CPU = value == "cpu"
+    TO_CPU = config.getoption("--ref") == "cpu"
+
+    global QUICK_MODE
+    QUICK_MODE = config.getoption("--mode") == "quick"
+
+    global RECORD_LOG
+    RECORD_LOG = config.getoption("--record") == "log"
+    if RECORD_LOG:
+        global RUNTEST_INFO, BUILTIN_MARKS, REGISTERED_MARKERS
+        RUNTEST_INFO = {}
+        BUILTIN_MARKS = {
+            "parametrize",
+            "skip",
+            "skipif",
+            "xfail",
+            "usefixtures",
+            "filterwarnings",
+            "timeout",
+            "tryfirst",
+            "trylast",
+        }
+        REGISTERED_MARKERS = {
+            marker.split(":")[0].strip() for marker in config.getini("markers")
+        }
+        cmd_args = [
+            arg.replace(".py", "").replace("=", "_").replace("/", "_")
+            for arg in config.invocation_params.args
+        ]
+        logging.basicConfig(
+            filename="result_{}.log".format("_".join(cmd_args)).replace("_-", "-"),
+            filemode="w",
+            level=logging.INFO,
+            format="[%(levelname)s] %(message)s",
+        )
+
+
+def pytest_runtest_teardown(item, nextitem):
+    if not RECORD_LOG:
+        return
+    if hasattr(item, "callspec"):
+        all_marks = list(item.iter_markers())
+        op_marks = [
+            mark.name
+            for mark in all_marks
+            if mark.name not in BUILTIN_MARKS and mark.name not in REGISTERED_MARKERS
+        ]
+        if len(op_marks) > 0:
+            params = str(item.callspec.params)
+            for op_mark in op_marks:
+                if op_mark not in RUNTEST_INFO:
+                    RUNTEST_INFO[op_mark] = [params]
+                else:
+                    RUNTEST_INFO[op_mark].append(params)
+        else:
+            func_name = item.function.__name__
+            logging.warning("There is no mark at {}".format(func_name))
+
+
+def pytest_sessionfinish(session, exitstatus):
+    if RECORD_LOG:
+        logging.info(json.dumps(RUNTEST_INFO, indent=2))
+
+
+test_results = {}
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtest_protocol(item, nextitem):
+    test_results[item.nodeid] = {"params": None, "result": None}
+    param_values = {}
+    request = item._request
+    if hasattr(request, "node") and hasattr(request.node, "callspec"):
+        param_values = request.node.callspec.params
+
+    test_results[item.nodeid]["params"] = param_values
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtest_logreport(report):
+    if report.when == "call":
+        test_results[report.nodeid]["result"] = report.outcome
+
+
+def pytest_terminal_summary(terminalreporter):
+    if os.path.exists(filename):
+        with open(filename, "r") as json_file:
+            existing_data = json.load(json_file)
+        existing_data.update(test_results)
+    else:
+        existing_data = test_results
+
+    with open("result.json", "w") as json_file:
+        json.dump(existing_data, json_file, indent=4, default=str)
