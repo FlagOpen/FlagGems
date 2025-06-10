53a65e4190a873a1cd70b6e7f71831be08020008
tsingmicro-public <guohuijiang@tsingmicro.com>
2025-06-10 11:22:47 +0800

    to save the log

--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -1,3 +1,28 @@
+import json
+import logging
+import os
+
+import pytest
+import torch
+
+import flag_gems
+
+class BenchLevel(Enum):
+    COMPREHENSIVE = "comprehensive"
+    CORE = "core"
+    
+    
+class BenchConfig:
+    def __init__(self):
+        self.cpu_mode = False
+        self.bench_level = BenchLevel.COMPREHENSIVE
+        self.warm_up = 2
+        self.repetition = 2
+        self.record_log = False
+
+
+Config = BenchConfig()
+
 def pytest_addoption(parser):
     parser.addoption(
         "--mode",
@@ -7,9 +32,43 @@ def pytest_addoption(parser):
         choices=["cuda", "cpu"],
         help="record latency in cuda or cpu",
     )
-
+    parser.addoption(
+        "--record",
+        action="store",
+        default="none",
+        required=False,
+        choices=["none", "log"],
+        help="Benchmark info recorded in log files or not",
+    )
+    
+    parser.addoption(
+        "--level",
+        action="store",
+        default="comprehensive",
+        required=False,
+        choices=[level.value for level in BenchLevel],
+        help="Specify the benchmark level: comprehensive, or core.",
+    )
 
 def pytest_configure(config):
     value = config.getoption("--mode")
     global CPU_MODE
+    global Config
     CPU_MODE = value == "cpu"
+    
+    level_value = config.getoption("--level")
+    Config.bench_level = BenchLevel(level_value)
+    
+    Config.record_log = config.getoption("--record") == "log"
+    if Config.record_log:
+        cmd_args = [
+            arg.replace(".py", "").replace("=", "_").replace("/", "_")
+            for arg in config.invocation_params.args
+        ]
+
+        logging.basicConfig(
+            filename="result_{}.log".format("_".join(cmd_args)).replace("_-", "-"),
+            filemode="w",
+            level=logging.INFO,
+            format="[%(levelname)s] %(message)s",
+        )  
--- benchmark/performance_utils.py
+++ benchmark/performance_utils.py
@@ -7,6 +7,10 @@ import flag_gems
 import torch_xla.core.xla_model as xm
 
 from .conftest import CPU_MODE
+from dataclasses import asdict, dataclass, fields
+from typing import List, Optional, Tuple
+from .conftest import Config
+import logging
 
 WARMUP = 100
 REPETITION = 1000
@@ -17,6 +21,116 @@ if os.getenv("TRITON_TX8BE_E2E_BACKEND"):
 else:
     torch.backends.cuda.matmul.allow_tf32 = False
     device = "cuda"
+
+def custom_json_encoder(obj):
+    if isinstance(obj, torch.dtype):
+        return str(obj)
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+
+@dataclass
+class BenchmarkMetrics:
+    # Legacy shape information for backward compatibility
+    # This field corresponds to the 'size' field in the previous version's benchmark.
+    legacy_shape: Optional[int] = None
+    # Detailed size info
+    shape_detail: Optional[Tuple[int, ...]] = None
+    # Latency base in ms
+    latency_base: Optional[float] = None
+    # Latency in ms
+    latency: Optional[float] = None
+    # Speedup over baseline
+    speedup: Optional[float] = None
+
+@dataclass
+class BenchmarkResult:
+    """Record the benchmark result for each operator."""
+
+    # Unique name of the operator
+    op_name: str
+    dtype: str
+    mode: str
+    level: str
+    # Benchmark results
+    result: List[BenchmarkMetrics]
+
+    def __str__(self) -> str:
+        header_title = (
+            f"\nOperator: {self.op_name}  Performance Test (dtype={self.dtype}, mode={self.mode},"
+            f"level={self.level})\n"
+        )
+        col_names = [
+            f"{'Status':<10}",
+            f"{'Torch Latency (ms)':>20}",
+            f"{'Gems Latency (ms)':>20}",
+            f"{'Gems Speedup':>20}",
+        ]
+        col_names.append(f"{'Size Detail':>20}\n")
+        header_col_names = " ".join(col_names)
+        header_break = "-" * len(header_col_names) + "\n"
+        header = header_title + header_col_names + header_break
+
+        metrics_lines = "".join(self._format_metrics(ele) for ele in self.result)
+        return header + metrics_lines
+
+    def _format_metrics(self, metrics: BenchmarkMetrics) -> str:
+        # self.gen_legacy_shape(metrics)
+        # legacy_shape_str = (
+        #     metrics.legacy_shape if metrics.legacy_shape is not None else "N/A"
+        # )
+        latency_base_str = (
+            f"{metrics.latency_base:.6f}" if metrics.latency_base is not None else "N/A"
+        )
+        latency_str = f"{metrics.latency:.6f}" if metrics.latency is not None else "N/A"
+        speedup_str = f"{metrics.speedup:.3f}" if metrics.speedup is not None else "N/A"
+
+
+        shape_detail_str = (
+            metrics.shape_detail if metrics.shape_detail is not None else "N/A"
+        )
+        status = "SUCCESS" if metrics.error_msg is None else "FAILED"
+        data_line = (
+            f"{status:<10}"
+            f"{latency_base_str:>20}"
+            f"{latency_str:>20}"
+            f"{speedup_str:>20}"
+        )
+
+        data_line += " " * 10
+        data_line += f"{shape_detail_str}\n"
+        return data_line
+
+    def gen_legacy_shape(self, metrics: BenchmarkMetrics) -> Optional[int]:
+        first_shape = (
+            metrics.shape_detail[0] if isinstance(metrics.shape_detail, list) else None
+        )
+        to_record_shape = (
+            tuple(first_shape) if isinstance(first_shape, torch.Size) else None
+        )
+
+        if to_record_shape in LEGACY_NON_BLAS_SHAPES:
+            metrics.legacy_shape = to_record_shape[-1]
+        elif (
+            isinstance(to_record_shape, tuple)
+            and len(to_record_shape) == 2
+            and to_record_shape[0] == 1024
+        ):
+            metrics.legacy_shape = to_record_shape[-1]
+        else:
+            metrics.legacy_shape = None
+
+    def to_json(self) -> str:
+        import json
+
+        # Convert to dict and handle tuple serialization for shape_detail
+        result_dict = asdict(self)
+        return json.dumps(result_dict, default=custom_json_encoder)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+
+
 
 class Benchmark:
     def __init__(
@@ -44,7 +158,25 @@ class Benchmark:
 
     def set_gems(self, gems_op):
         self.gems_op = gems_op
+        
+    def record_shapes(self, *args, **kwargs):
+        def deep_parse(item):
+            if isinstance(item, torch.Tensor):
+                return item.size()
+            elif isinstance(item, (int, float, str, torch.dtype)):
+                return item
+            elif isinstance(item, (list, tuple)):
+                return [deep_parse(sub_item) for sub_item in item]
+            elif isinstance(item, dict):
+                return {key: deep_parse(value) for key, value in item.items()}
+            return None
 
+        parsed_args = [deep_parse(arg) for arg in args]
+        parsed_kwargs = {key: deep_parse(value) for key, value in kwargs.items()}
+        if parsed_args and parsed_kwargs:
+            return parsed_args, parsed_kwargs
+        return parsed_args if parsed_args else parsed_kwargs
+    
     def profile(self, op, *args, **kwargs):
         mode = 1
         if args[0].device.type == "cpu":
@@ -94,9 +226,11 @@ class Benchmark:
             print(f"Operator {self.op_name} Performance Test ({dtype})")
             print("Size        Torch Latency (ms)   Gems Latency (ms)")
             print("--------------------------------------------------")
+            metrics = []
             for size in self.sizes:
                 args = ()
                 args_xla = ()
+                metric = BenchmarkMetrics()
                 if self.arg_func is not None:
                     args = self.arg_func(dtype, self.batch, size)
                 if self.is_backward:
@@ -113,14 +247,32 @@ class Benchmark:
 
                 args_xla = tuple(xm.send_cpu_data_to_device(arg, xm.xla_device()) for arg in args)
                 torch_perf = self.profile(self.torch_op, *args_xla, **kwargs)
+                
                 if self.gems_op:
                     gems_perf = self.profile(self.gems_op, *args, **kwargs)
                 else:
                     with flag_gems.use_gems():
                         gems_perf = self.profile(self.torch_op, *args, **kwargs)
+                        
+                metric.shape_detail = self.record_shapes(*args, **kwargs)
+                metric.latency_base = torch_perf
+                metric.latency = gems_perf
+                metric.speedup = metric.latency_base / metric.latency
+                metrics.append(metric)
                 print(f"{size: <10}{torch_perf: >20.6}{gems_perf: >20.6}")
-
-
+                
+            result = BenchmarkResult(
+                level=Config.bench_level.value,
+                op_name=self.op_name,
+                dtype=str(dtype),
+                mode="Tx8",
+                result=metrics,
+            )
+            print(result)
+            logging.info(result.to_json())
+            
+            
+            
 FLOAT_DTYPES = [torch.float16, torch.float32, torch.bfloat16]
 INT_DTYPES = [torch.int16, torch.int32]
 
@@ -130,7 +282,7 @@ POINTWISE_BATCH = 1024
 REDUCTION_BATCH = 1024
 BLAS_BATCH = 16
 SIZES = [i * 1024 for i in range(1, 81, 5)]
-
+LEGACY_NON_BLAS_SHAPES = [(1024, shape) for shape in SIZES]
 
 def unary_arg(dtype, batch, size):
     inp = torch.randn([batch, size], dtype=dtype, device=device)
