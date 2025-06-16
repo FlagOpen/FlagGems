1694b7c90c339761565b2c79dcdeebd1d5c0abc0
xumy <xomingyan@gmail.com>
2025-06-16 17:32:00 +0800

    [kunlunxin3] fix mv test

--- src/flag_gems/runtime/backend/_kunlunxin/ops/mv.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/mv.py
@@ -8,7 +8,8 @@ import triton.language as tl
 from flag_gems.runtime import torch_device_fn
 from flag_gems.utils import libentry
 from flag_gems.utils import triton_lang_extension as tle
-
+from flag_gems.ops.mm import mm
+import os
 logger = logging.getLogger(__name__)
 
 
@@ -75,16 +76,23 @@ def mv(inp, vec):
     out = torch.empty((N,), device=inp.device, dtype=inp.dtype)
     grid = lambda META: (triton.cdiv(N, META["BLOCK_N"]),)
     with torch_device_fn.device(inp.device):
-        mv_kernel[grid](
-            inp,
-            vec,
-            out,
-            N,
-            M,
-            inp.stride(0),
-            inp.stride(1),
-            vec.stride(0),
-            out.stride(0),
-            buffer_size_limit=256,
-        )
+        if M == 1:
+            mv_kernel[grid](
+                inp,
+                vec,
+                out,
+                N,
+                M,
+                inp.stride(0),
+                inp.stride(1),
+                vec.stride(0),
+                out.stride(0),
+                buffer_size_limit=256,
+            )
+        else:
+            os.environ["XMLIR_MATMUL_FAST_MODE"] = "1"
+            vec = vec[:, None]
+            out = mm(inp, vec)
+            out = out.squeeze()
+            del os.environ["XMLIR_MATMUL_FAST_MODE"]
     return out
