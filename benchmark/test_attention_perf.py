5981c120192eb8d3d95540cd300964a4e8cf1818
junjian.zhan <junjian.zhan@iluvatar.com>
2025-06-30 11:10:59 +0800

    [Iluvatar] fix randn, randn_like, normal; Skip some ops..

--- benchmark/test_attention_perf.py
+++ benchmark/test_attention_perf.py
@@ -72,6 +72,7 @@ class FlashMLABenchmark(GenericBenchmark):
 
 
 @pytest.mark.skipif(vendor_name == "kunlunxin", reason="RESULT TODOFIX")
+@pytest.mark.skipif(vendor_name == "iluvatar", reason="RESULT TODOFIX")
 @pytest.mark.skipif(
     flag_gems.device == "musa" or vendor_name == "hygon", reason="RuntimeError"
 )
