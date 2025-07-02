9d321af491d18ce7dbe749354d50635339c799fe
you-and-you <1823382186@qq.com>
2025-07-02 18:24:50 +0800

    again

--- triton_src/zeros.py
+++ triton_src/zeros.py
@@ -1,6 +1,8 @@
 import triton
 import triton.language as tl
 
+from flag_gems.utils import triton_lang_extension as tle
+
 
 @triton.jit
 def zeros_kernel(
@@ -8,7 +10,7 @@ def zeros_kernel(
     n_elements,
     BLOCK_SIZE: tl.constexpr,
 ):
-    pid = tl.program_id(axis=0)
+    pid = tle.program_id(axis=0)
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0, BLOCK_SIZE)
     mask = offsets < n_elements
