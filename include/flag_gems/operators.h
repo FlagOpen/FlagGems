28acd7b8f048d5bdd2fbb65aee9dad86772f7fe2
AdvancedCompiler <Pikachu_Jun@outlook.com>
2025-07-07 14:44:05 +0800

    [AdvancedCompiler]bmm(cpp wrapper) (#735)
    * Update the C++ wrapper for bmm
    * Modify the BMM code according to the review comments
    
    ---------
    
    Co-authored-by: “ph0375” <“ph0375@163.com”>

--- include/flag_gems/operators.h
+++ include/flag_gems/operators.h
@@ -29,4 +29,6 @@ std::tuple<at::Tensor, at::Tensor> rotary_embedding(
     const at::Tensor &sin,
     const std::optional<at::Tensor> &position_ids = std::nullopt,
     bool rotary_interleaved = false);
+
+at::Tensor bmm(const at::Tensor &A, const at::Tensor &B);
 }  // namespace flag_gems
