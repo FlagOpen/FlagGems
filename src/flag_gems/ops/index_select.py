938052d71c92f611fe1307374e207c480fb141d3
meinie <929449726@qq.com>
2025-07-12 10:33:26 +0000

    update index_select

--- src/flag_gems/ops/index_select.py
+++ src/flag_gems/ops/index_select.py
@@ -4,63 +4,78 @@ import torch
 import triton
 import triton.language as tl
 
-from flag_gems import runtime
-from flag_gems.utils import dim_compress, libentry
-from flag_gems.utils import triton_lang_extension as tle
-
 logger = logging.getLogger(__name__)
 
 
-@libentry()
-@triton.heuristics(runtime.get_heuristic_config("index_select"))
 @triton.jit
 def index_select_kernel(
-    inp, out, M, N, index, index_len, BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr
+    inp_ptr,
+    out_ptr,
+    index_ptr,
+    dim_prod_post,
+    dim_size_in,
+    dim_size_out,
+    total_elements,
+    BLOCK_SIZE: tl.constexpr,
 ):
-    pid_x = tle.program_id(axis=0)
-    pid_y = tle.program_id(axis=1)
-    rows_offsets = pid_x * BLOCK_M + tl.arange(0, BLOCK_M)[:, None]
-    rows_mask = rows_offsets < M
-    cols_offsets = pid_y * BLOCK_N + tl.arange(0, BLOCK_N)
+    pid = tl.program_id(0)
+    block_start = pid * BLOCK_SIZE
+    offsets = tl.arange(0, BLOCK_SIZE)
+    mask = block_start + offsets < total_elements
 
-    out_mask = rows_mask and (cols_offsets < index_len)
+    idx = block_start + offsets
+    pre_idx = idx // (dim_size_out * dim_prod_post)
+    out_dim_idx = (idx // dim_prod_post) % dim_size_out
+    post_idx = idx % dim_prod_post
 
-    indices = tl.load(index + cols_offsets, mask=(cols_offsets < index_len), other=0)
-    inp_off = rows_offsets * N + indices[None, :]
-    out_off = rows_offsets * index_len + cols_offsets[None, :]
+    in_dim_idx = tl.load(index_ptr + out_dim_idx)
 
-    selected = tl.load(inp + inp_off, mask=rows_mask, other=0.0)
-    tl.store(out + out_off, selected, mask=out_mask)
+    inp_offset = (
+        pre_idx * dim_size_in * dim_prod_post + in_dim_idx * dim_prod_post + post_idx
+    )
+
+    inp = tl.load(inp_ptr + inp_offset, mask=mask)
+    tl.store(out_ptr + idx, inp, mask=mask)
 
 
 def index_select(inp, dim, index):
     logger.debug("GEMS INDEX SELECT")
     assert dim >= -inp.ndim and dim < inp.ndim, "Invalid dim"
     assert index.ndim <= 1, "Index should have dimension 1 or 0"
-    assert ((i >= 0 and i < inp.size(dim)) for i in index), "Index out of range"
+    # The check below is a generator, it needs to be consumed.
+    # Using all() to consume it and check all values.
+    assert all((i >= 0 and i < inp.size(dim)) for i in index), "Index out of range"
 
     if index.ndim == 0:
         index = index.unsqueeze(0)
+
     dim = dim % inp.ndim
-    inp_shape = list(inp.shape)
+    inp_shape = inp.shape
     index_len = index.numel()
 
-    # with dim_compress
-    inp = dim_compress(inp, dim)
-    N = inp_shape[dim]
-    M = inp.numel() // N
-    out_shape = list(inp.shape)
-    out_shape[inp.ndim - 1] = index_len
+    out_shape = list(inp_shape)
+    out_shape[dim] = index_len
     out = torch.empty(out_shape, dtype=inp.dtype, device=inp.device)
 
-    grid = lambda meta: (
-        triton.cdiv(M, meta["BLOCK_M"]),
-        triton.cdiv(index_len, meta["BLOCK_N"]),
-    )
-    index_select_kernel[grid](inp, out, M, N, index, index_len)
-    if dim != out.ndim - 1:
-        order = [i for i in range(out.ndim - 1)]
-        order.insert(dim, out.ndim - 1)
-        return out.permute(order).contiguous()
-    else:
+    if out.numel() == 0:
         return out
+
+    dim_prod_post = 1
+    for s in inp_shape[dim + 1 :]:
+        dim_prod_post *= s
+
+    total_elements = out.numel()
+    BLOCK_SIZE = 1024
+    grid = (triton.cdiv(total_elements, BLOCK_SIZE),)
+
+    index_select_kernel[grid](
+        inp,
+        out,
+        index,
+        dim_prod_post,
+        inp_shape[dim],
+        index_len,
+        total_elements,
+        BLOCK_SIZE=BLOCK_SIZE,
+    )
+    return out
