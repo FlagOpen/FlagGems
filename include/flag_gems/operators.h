0fe929bfa1636237332868c9b457b01896c58376
AdvancedCompiler <Pikachu_Jun@outlook.com>
2025-07-16 18:06:01 +0800

    [AdvancedCompiler]Topk(cpp wrapper) (#736)
    * merge
    
    * Change the format
    
    * Rename test_triton_special.cpp to test_triton_topk.cpp
    
    * change to test_triton_topk
    
    ---------
    
    Co-authored-by: nmpress1 <1935298275@qq.com>
    Co-authored-by: nmpress1 <162261334+nmpress1@users.noreply.github.com>
    Co-authored-by: StrongSpoon <35829812+StrongSpoon@users.noreply.github.com>

--- include/flag_gems/operators.h
+++ include/flag_gems/operators.h
@@ -40,6 +40,8 @@ std::tuple<at::Tensor, at::Tensor> rotary_embedding(
     const at::Tensor &sin,
     const std::optional<at::Tensor> &position_ids = std::nullopt,
     bool rotary_interleaved = false);
+std::tuple<at::Tensor, at::Tensor> topk(
+    const at::Tensor &x, int64_t k, int64_t dim = -1, bool largest = true, bool sorted = true);
 
 at::Tensor cat(const at::TensorList &tensors, int64_t dim = 0);
 at::Tensor bmm(const at::Tensor &A, const at::Tensor &B);
--- src/flag_gems/csrc/cstub.cpp
+++ src/flag_gems/csrc/cstub.cpp
@@ -36,6 +36,7 @@ TORCH_LIBRARY(flag_gems, m) {
   m.def(
       "rotary_embedding(Tensor q, Tensor k, Tensor cos, Tensor sin, Tensor? position_ids=None, "
       "bool rotary_interleaved=False) -> (Tensor, Tensor)");  // q and k may be view to other size
+  m.def("topk(Tensor x, SymInt k, int dim, bool largest, bool sorted) -> (Tensor, Tensor)");
   m.def("cat(Tensor[] tensors, int dim=0) -> Tensor");
   m.def("bmm(Tensor self, Tensor mat2) -> Tensor");
   m.def(
@@ -59,6 +60,7 @@ TORCH_LIBRARY_IMPL(flag_gems, CUDA, m) {
   // Rotary embedding
   m.impl("rotary_embedding", TORCH_FN(rotary_embedding));
   m.impl("rotary_embedding_inplace", TORCH_FN(rotary_embedding_inplace));
+  m.impl("topk", TORCH_FN(topk));
   m.impl("cat", TORCH_FN(cat));
   m.impl("bmm", TORCH_FN(bmm));
   m.impl("embedding", TORCH_FN(embedding));
