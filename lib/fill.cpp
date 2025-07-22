3f04053868cde408cabe7a4ed18fc3ad755a037b
Clement Chan <iclementine@outlook.com>
2025-07-22 09:52:40 +0800

    Revert "fix fill operator registration (#809)"
    This reverts commit f18b2f88320c2f48e67fa0a3ad01a6d57aebc221.

--- lib/fill.cpp
+++ lib/fill.cpp
@@ -47,9 +47,9 @@ at::Tensor fill_tensor(const at::Tensor& input, const at::Tensor& value) {
   return out;
 }
 
-at::Tensor& fill_scalar_(at::Tensor& input, const c10::Scalar& value) {
+void fill_scalar_(at::Tensor& input, const c10::Scalar& value) {
   int64_t numel = input.numel();
-  if (numel == 0) return input;
+  if (numel == 0) return;
 
   constexpr int BLOCK_SIZE = 1024;
   unsigned int grid_x = (numel + BLOCK_SIZE - 1) / BLOCK_SIZE;
@@ -62,13 +62,12 @@ at::Tensor& fill_scalar_(at::Tensor& input, const c10::Scalar& value) {
   cudaStream_t stream = c10::cuda::getCurrentCUDAStream();
 
   fill_kernel(stream, grid_x, 1, 1, 4, 0, input, value, numel, BLOCK_SIZE);
-  return input;
 }
 
-at::Tensor& fill_tensor_(at::Tensor& input, const at::Tensor& value) {
+void fill_tensor_(at::Tensor& input, const at::Tensor& value) {
   TORCH_CHECK(value.dim() == 0, "fill_tensor_ only supports 0-dim value tensor");
   int64_t numel = input.numel();
-  if (numel == 0) return input;
+  if (numel == 0) return;
 
   constexpr int BLOCK_SIZE = 1024;
   unsigned int grid_x = (numel + BLOCK_SIZE - 1) / BLOCK_SIZE;
@@ -81,7 +80,6 @@ at::Tensor& fill_tensor_(at::Tensor& input, const at::Tensor& value) {
   cudaStream_t stream = c10::cuda::getCurrentCUDAStream();
 
   fill_kernel(stream, grid_x, 1, 1, 4, 0, input, value, numel, BLOCK_SIZE);
-  return input;
 }
 
 }  // namespace flag_gems
