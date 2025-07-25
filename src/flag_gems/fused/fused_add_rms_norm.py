fd2a461a9441b78f0b1432a3b8ac7b696447fb7b
Clement Chan <iclementine@outlook.com>
2025-07-25 12:46:00 +0800

    Use triton jit functions in flag_gems for more cpp op wrappers (#826)
    * rms_norm and fused_add_rms_norm: use triton jit function s from flag_gems python package
    * embedding and rotary_embedding: use source from flag_gems python package
    * nonzero: use src from flag_gems
    * topk: use triton src from flag_gems
    * rename arguemnts
    * export FLAGGEMS_SOURCE_DIR for ctests

--- src/flag_gems/fused/fused_add_rms_norm.py
+++ src/flag_gems/fused/fused_add_rms_norm.py
@@ -14,37 +14,44 @@ logger = logging.getLogger(__name__)
 @libentry()
 @triton.jit(do_not_specialize=["eps"])
 def fused_add_rms_norm_kernel(
-    X,  # pointer to the input
-    R,  # pointer to the residual
-    W,  # pointer to the weights
-    x_stride_r,  # how much to increase the pointer when moving by 1 row
-    x_stride_c,  # how much to increase the pointer when moving by 1 col
+    input_ptr,  # pointer to the input
+    residual_ptr,  # pointer to the residual
+    w_ptr,  # pointer to the weights
+    in_stride_r,  # how much to increase the pointer when moving by 1 row
+    in_stride_c,  # how much to increase the pointer when moving by 1 col
     r_stride_r,  # how much to increase the pointer when moving by 1 row
     r_stride_c,  # how much to increase the pointer when moving by 1 col
-    N,  # number of columns in X
+    N,  # number of columns in in_ptr
     eps,  # epsilon to avoid division by zero
     BLOCK_SIZE: tl.constexpr,
 ):
+    if tl.constexpr(input_ptr.dtype.element_ty == tl.float16) or tl.constexpr(
+        input_ptr.dtype.element_ty == tl.bfloat16
+    ):
+        cdtype = tl.float32
+    else:
+        cdtype = input_ptr.dtype.element_ty
+
     pid = tle.program_id(0)
-    X += pid * x_stride_r
-    R += pid * r_stride_r
+    input_ptr += pid * in_stride_r
+    residual_ptr += pid * r_stride_r
 
     mask = tl.arange(0, BLOCK_SIZE) < N
     cols = tl.arange(0, BLOCK_SIZE)
-    x = tl.load(X + cols * x_stride_c, mask, other=0.0).to(tl.float32)
-    r = tl.load(R + cols * r_stride_c, mask, other=0.0).to(tl.float32)
+    x = tl.load(input_ptr + cols * in_stride_c, mask, other=0.0).to(cdtype)
+    r = tl.load(residual_ptr + cols * r_stride_c, mask, other=0.0).to(cdtype)
 
     x += r
     # write back to residual
-    tl.store(R + cols * r_stride_c, x, mask=mask)
+    tl.store(residual_ptr + cols * r_stride_c, x, mask=mask)
 
     var = tl.sum(x * x / N, axis=0)
     rrms = 1 / tl.sqrt(var + eps)
 
-    w = tl.load(W + tl.arange(0, BLOCK_SIZE), mask=mask, other=0.0)
-    y = (x * rrms).to(X.dtype.element_ty) * w
+    w = tl.load(w_ptr + tl.arange(0, BLOCK_SIZE), mask=mask, other=0.0)
+    y = (x * rrms * w).to(cdtype)
     # write back to input
-    tl.store(X + cols * x_stride_c, y, mask=mask)
+    tl.store(input_ptr + cols * in_stride_c, y, mask=mask)
 
 
 def fused_add_rms_norm(x, residual, normalized_shape, weight, eps=1e-5):
