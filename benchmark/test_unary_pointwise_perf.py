c444f708442613c149f400d7b824f56b5a77fa13
Blury233 <ingenue2333@gmail.com>
2025-08-07 05:58:13 +0000

    Fix: address review comments on GLU backward

--- benchmark/test_unary_pointwise_perf.py
+++ benchmark/test_unary_pointwise_perf.py
@@ -151,8 +151,11 @@ def test_to_dtype_perf():
 
 
 class GluBenchmark(UnaryPointwiseBenchmark):
+    # Glu test requires even numbers
     def set_more_shapes(self):
-        return
+        special_shapes_2d = [(1024, 2**i) for i in range(1, 20, 4)]
+        sp_shapes_3d = [(64, 64, 2**i) for i in range(1, 15, 4)]
+        return special_shapes_2d + sp_shapes_3d
 
 
 @pytest.mark.glu
@@ -161,5 +164,16 @@ def test_glu_perf():
         op_name="glu",
         torch_op=torch.nn.functional.glu,
         dtypes=FLOAT_DTYPES,
+    )
+    bench.run()
+
+
+@pytest.mark.glu_backward
+def test_glu_backward_perf():
+    bench = GluBenchmark(
+        op_name="glu",
+        torch_op=torch.nn.functional.glu,
+        dtypes=FLOAT_DTYPES,
+        is_backward=True,
     )
     bench.run()
