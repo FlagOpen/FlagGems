9475932930dc402df39e395e95d7e8b16ef7958f
meinie <929449726@qq.com>
2025-08-11 06:36:30 +0000

    test data init in cpu, and cvt to device for kernel

--- tests/test_blas_ops.py
+++ tests/test_blas_ops.py
@@ -10,7 +10,7 @@ from .accuracy_utils import (
     gems_assert_close,
     to_reference,
 )
-from .conftest import QUICK_MODE
+from .conftest import QUICK_MODE, TO_DEVICE
 
 MN_SHAPES = [(1, 32)] if QUICK_MODE else [(1, 32), (160, 1024), (5333, 497)]
 MNK_SHAPES = (
@@ -28,28 +28,32 @@ FLOAT_DTYPES = [torch.float32] if QUICK_MODE else FLOAT_DTYPES
 @pytest.mark.parametrize("scalar", SCALARS)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
 def test_accuracy_addmm(M, N, K, scalar, dtype):
-    mat1 = torch.randn((M, K), dtype=dtype, device="cpu").to(flag_gems.device)
-    mat2 = torch.randn((K, N), dtype=dtype, device="cpu").to(flag_gems.device)
-    bias1 = torch.randn((N,), dtype=dtype, device="cpu").to(flag_gems.device)
+    mat1 = torch.randn((M, K), dtype=dtype, device="cpu")
+    mat2 = torch.randn((K, N), dtype=dtype, device="cpu")
+    bias1 = torch.randn((N,), dtype=dtype, device="cpu")
     ref_mat1 = to_reference(mat1, True)
     ref_mat2 = to_reference(mat2, True)
     ref_bias1 = to_reference(bias1, True)
+    mat1 = mat1.to(flag_gems.device)
+    mat2 = mat2.to(flag_gems.device)
+    bias1 = bias1.to(flag_gems.device)
 
     alpha = beta = scalar
 
     ref_out1 = torch.addmm(ref_bias1, ref_mat1, ref_mat2, alpha=alpha, beta=beta)
     with flag_gems.use_gems():
         res_out1 = torch.addmm(bias1, mat1, mat2, alpha=alpha, beta=beta)
-
+    res_out1 = res_out1.to("cpu") if not TO_DEVICE else res_out1
     gems_assert_close(res_out1, ref_out1, dtype, reduce_dim=K)
 
-    bias2 = torch.randn((M, N), dtype=dtype, device="cpu").to(flag_gems.device)
+    bias2 = torch.randn((M, N), dtype=dtype, device="cpu")
     ref_bias2 = to_reference(bias2, True)
+    bias2 = bias2.to(flag_gems.device)
 
     ref_out2 = torch.addmm(ref_bias2, ref_mat1, ref_mat2, alpha=alpha, beta=beta)
     with flag_gems.use_gems():
         res_out2 = torch.addmm(bias2, mat1, mat2, alpha=alpha, beta=beta)
-
+    res_out2 = res_out2.to("cpu") if not TO_DEVICE else res_out2
     gems_assert_close(res_out2, ref_out2, dtype, reduce_dim=K)
 
 
@@ -62,15 +66,17 @@ def test_accuracy_addmm(M, N, K, scalar, dtype):
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
 def test_accuracy_bmm(M, N, K, dtype):
     batch = 4
-    mat1 = torch.randn((batch, M, K), dtype=dtype, device="cpu").to(flag_gems.device)
-    mat2 = torch.randn((batch, K, N), dtype=dtype, device="cpu").to(flag_gems.device)
+    mat1 = torch.randn((batch, M, K), dtype=dtype, device="cpu")
+    mat2 = torch.randn((batch, K, N), dtype=dtype, device="cpu")
     ref_mat1 = to_reference(mat1, True)
     ref_mat2 = to_reference(mat2, True)
+    mat1 = mat1.to(flag_gems.device)
+    mat2 = mat2.to(flag_gems.device)
 
     ref_out = torch.bmm(ref_mat1, ref_mat2)
     with flag_gems.use_gems():
         res_out = torch.bmm(mat1, mat2)
-
+    res_out = res_out.to("cpu") if not TO_DEVICE else res_out
     gems_assert_close(res_out, ref_out, dtype, reduce_dim=K)
 
 
@@ -83,15 +89,16 @@ def test_accuracy_bmm(M, N, K, dtype):
 @pytest.mark.parametrize("M, N, K", MNK_SHAPES)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
 def test_accuracy_mm(M, N, K, dtype):
-    mat1 = torch.randn((M, K), dtype=dtype, device="cpu").to(flag_gems.device)
-    mat2 = torch.randn((K, N), dtype=dtype, device="cpu").to(flag_gems.device)
+    mat1 = torch.randn((M, K), dtype=dtype, device="cpu")
+    mat2 = torch.randn((K, N), dtype=dtype, device="cpu")
     ref_mat1 = to_reference(mat1, True)
     ref_mat2 = to_reference(mat2, True)
-
+    mat1 = mat1.to(flag_gems.device)
+    mat2 = mat2.to(flag_gems.device)
     ref_out = torch.mm(ref_mat1, ref_mat2)
     with flag_gems.use_gems():
         res_out = torch.mm(mat1, mat2)
-
+    res_out = res_out.to("cpu") if not TO_DEVICE else res_out
     gems_assert_close(res_out, ref_out, dtype, reduce_dim=K)
 
 
@@ -103,15 +110,16 @@ def test_accuracy_mm(M, N, K, dtype):
 @pytest.mark.parametrize("M, N", MN_SHAPES)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
 def test_accuracy_mv(M, N, dtype):
-    matrix = torch.randn((N, M), dtype=dtype, device="cpu").to(flag_gems.device)
-    vector = torch.randn((M,), dtype=dtype, device="cpu").to(flag_gems.device)
+    matrix = torch.randn((N, M), dtype=dtype, device="cpu")
+    vector = torch.randn((M,), dtype=dtype, device="cpu")
     ref_matrix = to_reference(matrix, True)
     ref_vector = to_reference(vector, True)
-
+    matrix = matrix.to(flag_gems.device)
+    vector = vector.to(flag_gems.device)
     ref_out = torch.mv(ref_matrix, ref_vector)
     with flag_gems.use_gems():
         res_out = torch.mv(matrix, vector)
-
+    res_out = res_out.to("cpu") if not TO_DEVICE else res_out
     gems_assert_close(res_out, ref_out, dtype, reduce_dim=M)
 
 
@@ -121,26 +129,26 @@ def test_accuracy_mv(M, N, dtype):
 )
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
 def test_accuracy_outer(M, N, dtype):
-    inp1 = torch.randn(M, dtype=dtype, device="cpu", requires_grad=True).to(
-        flag_gems.device
-    )
-    inp2 = torch.randn(N, dtype=dtype, device="cpu", requires_grad=True).to(
-        flag_gems.device
-    )
+    inp1 = torch.randn(M, dtype=dtype, device="cpu", requires_grad=True)
+    inp2 = torch.randn(N, dtype=dtype, device="cpu", requires_grad=True)
     ref_inp1 = to_reference(inp1, True)
     ref_inp2 = to_reference(inp2, True)
-
+    inp1 = inp1.to(flag_gems.device)
+    inp2 = inp2.to(flag_gems.device)
     ref_out = torch.outer(ref_inp1, ref_inp2)
     res_out = flag_gems.outer(inp1, inp2)
+    res_out = res_out.to("cpu") if not TO_DEVICE else res_out
     gems_assert_close(res_out, ref_out, dtype)
 
-    out_grad = torch.randn_like(res_out)
+    out_grad = torch.randn_like(res_out, device="cpu")
     ref_grad = to_reference(out_grad, True)
-
+    out_grad = out_grad.to(flag_gems.device)
     ref_in1_grad, ref_in2_grad = torch.autograd.grad(
         ref_out, (ref_inp1, ref_inp2), ref_grad
     )
     res_in1_grad, res_in2_grad = torch.autograd.grad(res_out, (inp1, inp2), out_grad)
+    res_in1_grad = res_in1_grad.to("cpu") if not TO_DEVICE else res_in1_grad
+    res_in2_grad = res_in2_grad.to("cpu") if not TO_DEVICE else res_in2_grad
     gems_assert_close(res_in1_grad, ref_in1_grad, dtype, reduce_dim=N)
     gems_assert_close(res_in2_grad, ref_in2_grad, dtype, reduce_dim=M)
 
@@ -155,13 +163,8 @@ def test_accuracy_outer(M, N, dtype):
 @pytest.mark.parametrize("stride", [1, 2])
 def test_accuracy_vdot(M, is_conj, dtype, stride):
     inp1_is_conj, inp2_is_conj = is_conj
-
-    if flag_gems.device == "musa":
-        inp1 = torch.randn(M, dtype=dtype, device="cpu")
-        inp2 = torch.randn(M, dtype=dtype, device="cpu")
-    else:
-        inp1 = torch.randn(M, dtype=dtype, device="cpu").to(flag_gems.device)
-        inp2 = torch.randn(M, dtype=dtype, device="cpu").to(flag_gems.device)
+    inp1 = torch.randn(M, dtype=dtype, device="cpu")
+    inp2 = torch.randn(M, dtype=dtype, device="cpu")
 
     inp1 = inp1[::stride]
     inp2 = inp2[::stride]
@@ -173,15 +176,12 @@ def test_accuracy_vdot(M, is_conj, dtype, stride):
 
     ref_inp1 = to_reference(inp1, True)
     ref_inp2 = to_reference(inp2, True)
-
-    with flag_gems.use_gems():
-        if flag_gems.device == "musa":
-            res_out = torch.vdot(
-                inp1.to(device=flag_gems.device), inp2.to(device=flag_gems.device)
-            )
-        else:
-            res_out = torch.vdot(inp1, inp2)
+    ref_inp1 = ref_inp1.to(flag_gems.device)
+    ref_inp2 = ref_inp2.to(flag_gems.device)
     ref_out = torch.vdot(ref_inp1, ref_inp2)
+    with flag_gems.use_gems():
+        res_out = torch.vdot(inp1, inp2)
+    res_out = res_out.to("cpu") if not TO_DEVICE else res_out
     gems_assert_close(res_out, ref_out, dtype)
 
 
@@ -193,13 +193,14 @@ def test_accuracy_dot_tensor_tensor(shape, dtype):
         torch.manual_seed(0)
         torch.cuda.manual_seed_all(0)
 
-    inp1 = torch.randn(shape, dtype=dtype, device="cpu").to(flag_gems.device)
-    inp2 = torch.randn(shape, dtype=dtype, device="cpu").to(flag_gems.device)
+    inp1 = torch.randn(shape, dtype=dtype, device="cpu")
+    inp2 = torch.randn(shape, dtype=dtype, device="cpu")
     ref_inp1 = to_reference(inp1, True)
     ref_inp2 = to_reference(inp2, True)
-
+    inp1 = to_reference(inp1, True)
+    inp2 = to_reference(inp2, True)
     ref_out = torch.dot(ref_inp1, ref_inp2)
     with flag_gems.use_gems():
         res_out = torch.dot(inp1, inp2)
-
+    res_out = res_out.to("cpu") if not TO_DEVICE else res_out
     gems_assert_close(res_out, ref_out, dtype, equal_nan=True)
