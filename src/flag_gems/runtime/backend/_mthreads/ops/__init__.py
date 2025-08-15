be3e5988548eade940f4a180e5dfb13b2cec124d
Kylin1207 <13345006231@163.com>
2025-08-15 17:35:14 +0800

    [MTHREADS] fix illegal address access error in perf stage

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -5,11 +5,30 @@ from .attention import (
     flash_attn_varlen_func,
     scaled_dot_product_attention,
 )
+from .dropout import dropout, dropout_backward
+from .ones import ones
+from .ones_like import ones_like
+from .rand import rand
+from .rand_like import rand_like
+from .randn import randn
+from .randn_like import randn_like
+from .zeros import zeros
+from .zeros_like import zeros_like
 
 __all__ = [
     "flash_attn_varlen_func",
     "scaled_dot_product_attention",
     "flash_attention_forward",
+    "rand",
+    "rand_like",
+    "dropout",
+    "dropout_backward",
+    "ones",
+    "ones_like",
+    "randn",
+    "randn_like",
+    "zeros",
+    "zeros_like",
 ]
 
 if get_device_capability(current_device())[0] >= 3:
