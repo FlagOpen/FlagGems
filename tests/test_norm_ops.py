1debdc41b985c4a8606ed66284f83f438623bd40
Kylin1207 <13345006231@163.com>
2025-08-20 13:43:49 +0800

    [MTHREADS] temp disable layer_norm op

--- tests/test_norm_ops.py
+++ tests/test_norm_ops.py
@@ -146,6 +146,7 @@ def test_accuracy_groupnorm_backward(N, C, H, W, num_groups, dtype, wb_none):
         gems_assert_close(res_bias_grad, ref_bias_grad, dtype, reduce_dim=N * HxW)
 
 
+@pytest.mark.skipif(flag_gems.device == "musa", reason="temp disable for updating")
 @pytest.mark.layer_norm
 @pytest.mark.parametrize(
     "shape",
@@ -200,6 +201,7 @@ def test_accuracy_layernorm(shape, dtype, wb_none):
     gems_assert_close(res_out, ref_out, dtype)
 
 
+@pytest.mark.skipif(flag_gems.device == "musa", reason="temp disable for updating")
 @pytest.mark.layer_norm
 @pytest.mark.parametrize(
     "shape",
