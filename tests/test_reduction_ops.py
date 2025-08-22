664b24ddbc5555b98807c5923859c6c9985da0e7
StrongSpoon <35829812+StrongSpoon@users.noreply.github.com>
2025-08-22 09:26:22 +0800

    add pytest mark

--- tests/test_reduction_ops.py
+++ tests/test_reduction_ops.py
@@ -1477,6 +1477,7 @@ def topk_softmax_torch_reference(gating_output: torch.Tensor, topk: int):
     return topk_values, topk_indices, source_rows
 
 
+@pytest.mark.topk_softmax
 @pytest.mark.parametrize("index_dtype", [torch.int32, torch.int64, torch.uint32])
 @pytest.mark.parametrize(
     "num_tokens, num_experts, topk",
