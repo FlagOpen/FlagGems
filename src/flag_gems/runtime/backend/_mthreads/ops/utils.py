219a604166d3643c7eae727efd8039aeacff1ba9
Qilin Gai <qilin.gai@mthreads.com>
2025-09-11 17:42:43 +0800

    [MTHREADS] fix type error

--- src/flag_gems/runtime/backend/_mthreads/ops/utils.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/utils.py
@@ -35,6 +35,6 @@ def get_triton_dtype(dtype):
 def should_enable_sqmma(a_dtype, b_dtype, M, N, K):
     return (
         (os.getenv("MUSA_ENABLE_SQMMA", "0") == "1")
-        and (a_dtype.itemsize == 2)
+        and (a_dtype in [torch.float16, torch.bfloat16] and a_dtype.itemsize == 2)
         and ((M, N, K) not in [(1, 1, 32), (15, 160, 1024), (495, 5333, 71)])
     )
