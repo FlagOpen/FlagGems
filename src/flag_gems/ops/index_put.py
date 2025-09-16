65b04a5adab3678dd03eea3aa84367d137a25d2b
0x45f <wangzhen@baai.ac.cn>
2025-09-16 17:34:54 +0800

    Fix

--- src/flag_gems/ops/index_put.py
+++ src/flag_gems/ops/index_put.py
@@ -262,6 +262,8 @@ def index_put(inp, indices, values, accumulate=False):
     target_shape = get_max_rank_shape(indices)
     broadcast_indices(indices, target_shape)
     target_shape += inp.shape[len(indices) :]
+    if values.device != inp.device:
+        values = values.to(inp.device)
     values = torch.broadcast_to(values, target_shape)
 
     out = inp.clone()
@@ -280,6 +282,8 @@ def index_put_(inp, indices, values, accumulate=False):
     target_shape = get_max_rank_shape(indices)
     broadcast_indices(indices, target_shape)
     target_shape += inp.shape[len(indices) :]
+    if values.device != inp.device:
+        values = values.to(inp.device)
     values = torch.broadcast_to(values, target_shape)
 
     _index_put_func(inp, indices, values, accumulate)
