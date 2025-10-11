baf63a6df4452fb1903572b4ae7ccf9c0d58c8ab
root <qjk595391@gmail.com>
2025-10-11 07:22:59 +0000

    Fix(ops): Corrected formatting issues in the code

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -64,7 +64,7 @@ def enable(
             ("arange.start_step", arange_start),
             ("argmax", argmax),
             ("argmin", argmin),
-            ("atan",atan),
+            ("atan", atan),
             ("atan_", atan_),
             ("bitwise_and.Scalar", bitwise_and_scalar),
             ("bitwise_and.Scalar_Tensor", bitwise_and_scalar_tensor),
