b8f7110560cbc58a02bc580775da3464c5156f92
SabYic <sunwenjia04@163.com>
2025-10-31 15:02:54 +0800

    modified:   src/flag_gems/runtime/backend/_amd/__init__.py

--- src/flag_gems/runtime/backend/_amd/__init__.py
+++ src/flag_gems/runtime/backend/_amd/__init__.py
@@ -1,7 +1,7 @@
 from backend_utils import VendorInfoBase  # noqa: E402
 
 vendor_info = VendorInfoBase(
-    vendor_name="amd", device_name="rocm", device_query_cmd="rocm-smi"
+    vendor_name="amd", device_name="cuda", device_query_cmd="rocm-smi"
 )
 
 CUSTOMIZED_UNUSED_OPS = ("cumsum", "cos", "add")
