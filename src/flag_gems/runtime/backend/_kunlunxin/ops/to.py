983081700aa2f1632d96e76ef4cc755825be624c
Ason93 <18817617225@163.com>
2025-10-31 13:15:49 +0800

    [KUNLUNXIN] Speed Up to (#1026)

--- src/flag_gems/runtime/backend/_kunlunxin/ops/to.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/to.py
@@ -15,7 +15,6 @@ config_ = CodeGenConfig(
     32,
     True,
     prefer_1d_tile=True,
-    isCloseVectorization=True,
 )
 
 
@@ -31,9 +30,34 @@ def to_dtype_func(x):
     return x
 
 
+config_bf16_ = CodeGenConfig(
+    512,
+    (65536, 65536, 65536),
+    32,
+    True,
+    prefer_1d_tile=True,
+    isCloseVectorization=True,
+)
+
+
+@pointwise_dynamic(
+    is_tensor=[
+        True,
+    ],
+    promotion_methods=[(0, "DEFAULT")],
+    config=config_bf16_,
+)
+@triton.jit
+def to_dtype_func_bf16(x):
+    return x
+
+
 def to_dtype(x, dtype, non_blocking=False, copy=False, memory_format=None):
     logger.debug("GEMS TO.DTYPE")
     if not copy and x.dtype == dtype:
         return x
     out = torch.empty_like(x, dtype=dtype, memory_format=memory_format)
-    return to_dtype_func(x, out0=out)
+    if dtype == torch.bfloat16:
+        return to_dtype_func_bf16(x, out0=out)
+    else:
+        return to_dtype_func(x, out0=out)
