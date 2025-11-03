7fdb1f8e941328adc6831ca69fd9c77d280c4b2d
Ason93 <18817617225@163.com>
2025-11-03 15:16:22 +0800

    [KUNLUNXIN] Speed Up ge/gt/le/lt/ne with MemoryAsync

--- src/flag_gems/runtime/backend/_kunlunxin/ops/le.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/le.py
@@ -2,13 +2,27 @@ import logging
 
 import triton
 import triton.language as tl
+from _kunlunxin.utils.codegen_config_utils import CodeGenConfig
 
 from ..utils.pointwise_dynamic import pointwise_dynamic
 
 logger = logging.getLogger("flag_gems").getChild(__name__.lstrip("."))
 
 
-@pointwise_dynamic(promotion_methods=[(0, 1, "ALWAYS_BOOL")])
+config_ = CodeGenConfig(
+    512,
+    (65536, 65536, 65536),
+    32,
+    True,
+    prefer_1d_tile=True,
+    isCloseMemoryAsync=False,
+)
+
+
+@pointwise_dynamic(
+    promotion_methods=[(0, 1, "ALWAYS_BOOL")],
+    config=config_,
+)
 @triton.jit
 def le_func(x, y):
     return x.to(tl.float32) <= y
@@ -20,7 +34,11 @@ def le(A, B):
     return res
 
 
-@pointwise_dynamic(is_tensor=[True, False], promotion_methods=[(0, 1, "ALWAYS_BOOL")])
+@pointwise_dynamic(
+    is_tensor=[True, False],
+    promotion_methods=[(0, 1, "ALWAYS_BOOL")],
+    config=config_,
+)
 @triton.jit
 def le_func_scalar(x, y):
     return x.to(tl.float32) <= y
--- src/flag_gems/runtime/backend/_kunlunxin/ops/lt.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/lt.py
@@ -2,13 +2,26 @@ import logging
 
 import triton
 import triton.language as tl
+from _kunlunxin.utils.codegen_config_utils import CodeGenConfig
 
 from ..utils.pointwise_dynamic import pointwise_dynamic
 
 logger = logging.getLogger("flag_gems").getChild(__name__.lstrip("."))
 
+config_ = CodeGenConfig(
+    512,
+    (65536, 65536, 65536),
+    32,
+    True,
+    prefer_1d_tile=True,
+    isCloseMemoryAsync=False,
+)
 
-@pointwise_dynamic(promotion_methods=[(0, 1, "ALWAYS_BOOL")])
+
+@pointwise_dynamic(
+    promotion_methods=[(0, 1, "ALWAYS_BOOL")],
+    config=config_,
+)
 @triton.jit
 def lt_func(x, y):
     return x.to(tl.float32) < y
@@ -20,7 +33,10 @@ def lt(A, B):
     return res
 
 
-@pointwise_dynamic(is_tensor=[True, False], promotion_methods=[(0, 1, "ALWAYS_BOOL")])
+@pointwise_dynamic(
+    promotion_methods=[(0, 1, "ALWAYS_BOOL")],
+    config=config_,
+)
 @triton.jit
 def lt_func_scalar(x, y):
     return x.to(tl.float32) < y
