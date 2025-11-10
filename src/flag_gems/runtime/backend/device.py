a3b6816ab47dcb6ac34c7f97ef69abf86405aee7
Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>
2025-11-10 10:44:00 +0800

    Remove vendor name check from environment variable
    Removed the check for vendor name in environment variable.
    
    Signed-off-by: Galaxy1458 <55453380+Galaxy1458@users.noreply.github.com>

--- src/flag_gems/runtime/backend/device.py
+++ src/flag_gems/runtime/backend/device.py
@@ -67,10 +67,6 @@ class DeviceDetector(object):
         vendor_name = self._get_vendor_from_quick_cmd()
         if vendor_name is not None:
             return backend.get_vendor_info(vendor_name)
-        # Check whether the vendor name is set in the environment variable.
-        vendor_from_env = self._get_vendor_from_env()
-        if vendor_from_env is not None:
-            return backend.get_vendor_info(vendor_from_env)
         try:
             # Obtaining a vendor_info from the methods provided by torch or triton, but is not currently implemented.
             return self._get_vendor_from_lib()
