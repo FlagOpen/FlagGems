9913954b6aa3e70ff4a6e5d6f31a73e13d23ef6b
QiLin Gai <qilin.gai@mthreads.com>
2025-11-11 07:52:58 +0000

    [MTHREADS] enable flash_mla op

--- benchmark/test_attention_perf.py
+++ benchmark/test_attention_perf.py
@@ -75,7 +75,6 @@ class FlashMLABenchmark(GenericBenchmark):
 @pytest.mark.skipif(vendor_name == "metax", reason="TODOFIX")
 @pytest.mark.skipif(vendor_name == "kunlunxin", reason="RESULT TODOFIX")
 @pytest.mark.skipif(vendor_name == "hygon", reason="RuntimeError")
-# @pytest.mark.skipif(vendor_name == "mthreads", reason="RESULT TODOFIX")
 @pytest.mark.skipif(flag_gems.vendor_name == "cambricon", reason="TypeError")
 @pytest.mark.flash_mla
 def test_perf_flash_mla():
