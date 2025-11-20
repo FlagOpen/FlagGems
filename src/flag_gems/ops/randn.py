12966a68d02fe3995bb1591983697382dc50681f
songdejun <jude.songd@gmail.com>
2025-11-20 19:21:31 +0800

    optim randn & randn_like with inline ptx and mem reordering

--- src/flag_gems/ops/randn.py
+++ src/flag_gems/ops/randn.py
@@ -12,21 +12,42 @@ from flag_gems.utils.random_utils import (
 )
 from flag_gems.utils.shape_utils import volume
 
-try:
-    pair_uniform_to_normal = tl.pair_uniform_to_normal
-except AttributeError:
-
-    @triton.jit
-    def pair_uniform_to_normal(u1, u2):
-        """Box-Muller transform"""
-        u1 = tl.maximum(1.0e-7, u1)
-        th = 6.283185307179586 * u2
-        r = tl.sqrt(-2.0 * tl.log(u1))
-        return r * tl.cos(th), r * tl.sin(th)
-
-
 device_ = device
 logger = logging.getLogger(__name__)
+
+
+@triton.jit
+def fast_cos_ptx(x):
+    return tl.inline_asm_elementwise(
+        "cos.approx.ftz.f32 $0, $1;",
+        "=f,f",
+        [x],
+        dtype=tl.float32,
+        is_pure=True,
+        pack=1,
+    )
+
+
+@triton.jit
+def fast_sin_ptx(x):
+    return tl.inline_asm_elementwise(
+        "sin.approx.ftz.f32 $0, $1;",
+        "=f,f",
+        [x],
+        dtype=tl.float32,
+        is_pure=True,
+        pack=1,
+    )
+
+
+@triton.jit
+def fast_box_muller_ptx(u1, u2):
+    u1 = tl.maximum(1.0e-7, u1)
+    th = 6.283185307179586 * u2
+
+    r = tl.sqrt(-2.0 * tl.log(u1))
+
+    return r * fast_cos_ptx(th), r * fast_sin_ptx(th)
 
 
 @triton.heuristics(runtime.get_heuristic_config("randn"))
@@ -42,24 +63,29 @@ def randn_kernel(
     philox_offset = philox_offset.to(tl.int64)
     c0 = (philox_offset & 0xFFFFFFFF).to(tl.uint32)
     c1 = ((philox_offset >> 32) & 0xFFFFFFFF).to(tl.uint32)
+
     i4 = tl.program_id(0) * BLOCK + tl.arange(0, BLOCK)
     c0 += i4
     _O = c0 * 0
+
     r0, r1, r2, r3 = tl.philox(philox_seed, c0, c1, _O, _O)
+
     r0 = uint_to_uniform_float(r0)
     r1 = uint_to_uniform_float(r1)
     r2 = uint_to_uniform_float(r2)
     r3 = uint_to_uniform_float(r3)
-    n0, n1 = pair_uniform_to_normal(r0, r1)
-    n2, n3 = pair_uniform_to_normal(r2, r3)
-    off_0 = tl.program_id(0) * BLOCK * 4 + tl.arange(0, BLOCK)
-    off_1 = off_0 + BLOCK
-    off_2 = off_1 + BLOCK
-    off_3 = off_2 + BLOCK
-    tl.store(out_ptr + off_0, n0, mask=off_0 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_1, n1, mask=off_1 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_2, n2, mask=off_2 < N, eviction_policy="evict_first")
-    tl.store(out_ptr + off_3, n3, mask=off_3 < N, eviction_policy="evict_first")
+
+    n0, n1 = fast_box_muller_ptx(r0, r1)
+    n2, n3 = fast_box_muller_ptx(r2, r3)
+
+    n_01 = tl.cat(n0, n1, can_reorder=True)
+    n_23 = tl.cat(n2, n3, can_reorder=True)
+    n_all = tl.cat(n_01, n_23, can_reorder=True)
+
+    base_offset = tl.program_id(0) * BLOCK * 4
+    off_all = base_offset + tl.arange(0, BLOCK * 4)
+
+    tl.store(out_ptr + off_all, n_all, mask=off_all < N, eviction_policy="evict_first")
 
 
 UNROLL = 4
@@ -73,11 +99,13 @@ def randn(size, *, dtype=None, layout=None, device=None, pin_memory=None):
         device = torch.device(device_.name)
     out = torch.empty(size, device=device, dtype=dtype)
     N = volume(size)
+
     grid_fn = lambda meta: (triton.cdiv(N, meta["BLOCK"] * UNROLL),)
-    # (TODO) Using Triton autotuner makes kernel parameters opaque to the caller,
-    # hence we cannot obtain the per thread offset as in Pytorch.
+
     increment = triton.cdiv(N, UNROLL)
     philox_seed, philox_offset = philox_backend_seed_offset(increment)
+
     with torch_device_fn.device(device):
         randn_kernel[grid_fn](out, N, philox_seed, philox_offset)
+
     return out
